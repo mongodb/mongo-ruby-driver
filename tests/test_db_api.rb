2d90d45c74f6fa94fbc06ad39de275e8173940f3
Adrian Madrid <aem@aemtower.(none)>
2008-12-08 13:35:49 -0700

    Fixed $in test

--- tests/test_db_api.rb
+++ tests/test_db_api.rb
@@ -96,15 +96,10 @@ class DBAPITest < Test::Unit::TestCase
     assert_equal 1, docs.size
     assert docs.detect { |row| row['a'] == 2 }
     # Find by advanced query (in clause)
-    begin
-      docs = @coll.find('a' => {'$in' => [1,2]}).map
-      assert_equal 2, docs.size
-      assert docs.detect { |row| row['a'] == 1 }
-      assert docs.detect { |row| row['a'] == 2 }
-    rescue Exception => ex
-      # this currently breaks
-      assert false
-    end
+    docs = @coll.find('a' => {'$in' => [1,2]}).map
+    assert_equal 2, docs.size
+    assert docs.detect { |row| row['a'] == 1 }
+    assert docs.detect { |row| row['a'] == 2 }
   end
   
   def test_close
