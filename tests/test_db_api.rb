1669e79241bc3a344ad48719e9497b79c8306548
Jim Menard <jim@10gen.com>
2009-02-03 12:16:21 -0500

    new test for Undefined type, commented out due to Mongo bug

--- tests/test_db_api.rb
+++ tests/test_db_api.rb
@@ -405,4 +405,17 @@ class DBAPITest < Test::Unit::TestCase
     end
   end
 
+# TODO this test fails with error message "Undefed Before end of object"
+# That is a database error. The undefined type may go away.
+
+#   def test_insert_undefined
+#     doc = {'undef' => Undefined.new}
+#     @coll.clear
+#     @coll.insert(doc)
+#     p @db.error                 # DEBUG
+#     assert_equal 1, @coll.count
+#     row = @coll.find().next_object
+#     assert_not_nil row
+#   end
+
 end
