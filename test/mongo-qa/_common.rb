990c00b261d5e1358acdf6087881184f907c89a9
Kyle Banker <kylebanker@gmail.com>
2010-01-05 18:00:34 -0500

    removed old mongo-qa tests

--- test/mongo-qa/_common.rb
+++
@@ -1,8 +0,0 @@
-$LOAD_PATH[0,0] = File.join(File.dirname(__FILE__), '../..', 'lib')
-require 'mongo'
-
-DEFAULT_HOST = '127.0.0.1'
-DEFAULT_PORT = 27017
-DEFAULT_DB = 'driver_test_framework'
-
-include Mongo
