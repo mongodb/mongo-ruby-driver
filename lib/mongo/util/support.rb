5dd263663455a5d6111dca1a0ddfac200d85ddcd
Kyle Banker <kylebanker@gmail.com>
2010-02-19 17:41:36 -0500

    minor: copyright update

--- lib/mongo/util/support.rb
+++ lib/mongo/util/support.rb
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2008-2009 10gen Inc.
+# Copyright (C) 2008-2010 10gen Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
