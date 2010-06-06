0335937b3ca7faeefd555973e3c453e25b6da1f0
Kyle Banker <kylebanker@gmail.com>
2010-06-05 22:45:37 -0400

    minor: docs

--- lib/mongo/collection.rb
+++ lib/mongo/collection.rb
@@ -434,8 +434,8 @@ module Mongo
 
     # Atomically update and return a document using MongoDB's findAndModify command. (MongoDB > 1.3.0)
     #
-    # @option opts [Hash] :update (nil) the update operation to perform on the matched document.
     # @option opts [Hash] :query ({}) a query selector document for matching the desired document.
+    # @option opts [Hash] :update (nil) the update operation to perform on the matched document.
     # @option opts [Array, String, OrderedHash] :sort ({}) specify a sort option for the query using any
     #   of the sort options available for Cursor#sort. Sort order is important if the query will be matching
     #   multiple documents since only the first matching document will be updated and returned.
