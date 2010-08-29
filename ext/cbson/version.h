c2955239a40f5cc3a91722c2627b5529dbdbe574
Kyle Banker <kylebanker@gmail.com>
2010-08-28 20:08:50 -0400

    BUMP BSON 1.0.7

--- ext/cbson/version.h
+++ ext/cbson/version.h
@@ -14,4 +14,4 @@
  * limitations under the License.
  */
 
-#define VERSION "1.0.6"
+#define VERSION "1.0.7"
--- lib/bson.rb
+++ lib/bson.rb
@@ -2,10 +2,10 @@
 
 $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-MINIMUM_BSON_EXT_VERSION = "1.0.6"
+MINIMUM_BSON_EXT_VERSION = "1.0.7"
 
 module BSON
-  VERSION = "1.0.6"
+  VERSION = "1.0.7"
   def self.serialize(obj, check_keys=false, move_id=false)
     BSON_CODER.serialize(obj, check_keys, move_id)
   end
