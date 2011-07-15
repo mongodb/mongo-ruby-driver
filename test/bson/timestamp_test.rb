5950f6850ff59c08a35f544a9ed311c84c7fa0ef
Kyle Banker <kylebanker@gmail.com>
2011-07-15 11:09:16 -0400

    minor: timestamp test

--- test/bson/timestamp_test.rb
+++ test/bson/timestamp_test.rb
@@ -9,6 +9,17 @@ class TiumestampTest < Test::Unit::TestCase
     assert_equal t2, t1
   end
 
+  def test_timestamp_range
+    t = 1;
+    while(t < 1_000_000_000 ) do
+      ts = Timestamp.new(t, 0)
+      doc = {:ts => ts}
+      bson = BSON::BSON_CODER.serialize(doc)
+      assert_equal doc[:ts], BSON::BSON_CODER.deserialize(bson)['ts']
+      t = t * 10
+    end
+  end
+
   def test_implements_array_for_backward_compatibility
     silently do
       ts = Timestamp.new(5000, 200)
