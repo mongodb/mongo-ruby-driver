5c2b4bb4e8dbd7f628cbc0b2a9774427437a0930
Nelson Elhage <nelhage@nelhage.com>
2012-11-12 15:27:44 -0800

    If sending data on a socket fails, close the socket.
    This mirrors the behavior on receive, and can prevent a situation
    where we keep trying to use the same socket to send data after a
    connection is dropped.

--- lib/mongo/networking.rb
+++ lib/mongo/networking.rb
@@ -282,6 +282,7 @@ module Mongo
       end
       total_bytes_sent
       rescue => ex
+        socket.close
         raise ConnectionFailure, "Operation failed with the following exception: #{ex}:#{ex.message}"
       end
     end
