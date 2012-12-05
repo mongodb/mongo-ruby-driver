680460e3f9b42acd988d1c87aab692a282dce71c
Tyler Brock <tyler.brock@gmail.com>
2012-12-05 11:31:31 -0500

    RUBY-463 refactor/renable threading tests

--- test/threading/threading_with_large_pool_test.rb
+++ test/threading/threading_with_large_pool_test.rb
@@ -1,53 +1,50 @@
 require 'test_helper'
 
-# Essentialy the same as test_threading.rb but with an expanded pool for
-# testing multiple connections.
 class TestThreadingLargePool < Test::Unit::TestCase
 
   include Mongo
 
-  @@client = standard_connection(:pool_size => 50, :pool_timeout => 60)
-  @@db = @@client.db(MONGO_TEST_DB)
-  @@coll = @@db.collection('thread-test-collection')
+  def setup
+    @client = standard_connection(:pool_size => 50, :pool_timeout => 60)
+    @db = @client.db(MONGO_TEST_DB)
+    @coll = @db.collection('thread-test-collection')
+    @coll.drop
 
-  def set_up_safe_data
-    @@db.drop_collection('duplicate')
-    @@db.drop_collection('unique')
-    @duplicate = @@db.collection('duplicate')
-    @unique    = @@db.collection('unique')
+    collections = ['duplicate', 'unique']
 
-    @duplicate.insert("test" => "insert")
-    @duplicate.insert("test" => "update")
-    @unique.insert("test" => "insert")
-    @unique.insert("test" => "update")
+    collections.each do |coll_name|
+      coll = @db.collection(coll_name)
+      coll.drop
+      coll.insert("test" => "insert")
+      coll.insert("test" => "update")
+      instance_variable_set("@#{coll_name}", coll)
+    end
+
     @unique.create_index("test", :unique => true)
   end
 
   def test_safe_update
-    set_up_safe_data
     threads = []
     300.times do |i|
-      threads[i] = Thread.new do
+      threads << Thread.new do
         if i % 2 == 0
           assert_raise Mongo::OperationFailure do
             @unique.update({"test" => "insert"}, {"$set" => {"test" => "update"}})
           end
         else
           @duplicate.update({"test" => "insert"}, {"$set" => {"test" => "update"}})
+          @duplicate.update({"test" => "update"}, {"$set" => {"test" => "insert"}})
         end
       end
     end
 
-    300.times do |i|
-      threads[i].join
-    end
+    threads.each {|thread| thread.join}
   end
 
   def test_safe_insert
-    set_up_safe_data
     threads = []
     300.times do |i|
-      threads[i] = Thread.new do
+      threads << Thread.new do
         if i % 2 == 0
           assert_raise Mongo::OperationFailure do
             @unique.insert({"test" => "insert"})
@@ -58,33 +55,25 @@ class TestThreadingLargePool < Test::Unit::TestCase
       end
     end
 
-    300.times do |i|
-      threads[i].join
-    end
+    threads.each {|thread| thread.join}
   end
 
-  def test_threading
-    @@coll.drop
-    @@coll = @@db.collection('thread-test-collection')
-
+  def test_count
     1000.times do |i|
-      @@coll.insert({ "x" => i })
+      @coll.insert({ "x" => i })
     end
 
     threads = []
-
     10.times do |i|
-      threads[i] = Thread.new do
+      threads << Thread.new do
         sum = 0
-        @@coll.find().each do |document|
+        @coll.find().each do |document|
           sum += document["x"]
         end
         assert_equal 499500, sum
       end
     end
 
-    10.times do |i|
-      threads[i].join
-    end
+    threads.each {|thread| thread.join}
   end
 end
