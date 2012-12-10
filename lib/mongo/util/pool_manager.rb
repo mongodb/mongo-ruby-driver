8a90fb0fd4406b96c05f7bf63dfdb6eabdf923b6
Tyler Brock <tyler.brock@gmail.com>
2012-12-10 17:24:44 -0500

    RUBY-472 move #near_pool and #select_pool into module

--- lib/mongo/util/pool_manager.rb
+++ lib/mongo/util/pool_manager.rb
@@ -114,13 +114,13 @@ module Mongo
         when :primary
           @primary_pool
         when :primary_preferred
-          @primary_pool || select_pool(@secondary_pools, tags, acceptable_latency)
+          @primary_pool || select_read_pool(@secondary_pools, tags, acceptable_latency)
         when :secondary
-          select_pool(@secondary_pools, tags, acceptable_latency)
+          select_read_pool(@secondary_pools, tags, acceptable_latency)
         when :secondary_preferred
-          select_pool(@secondary_pools, tags, acceptable_latency) || @primary_pool
+          select_read_pool(@secondary_pools, tags, acceptable_latency) || @primary_pool
         when :nearest
-          select_pool(pools, tags, acceptable_latency)
+          select_read_pool(pools, tags, acceptable_latency)
         end
       end
 
@@ -231,33 +231,6 @@ module Mongo
         :node => member
       )
       @secondary_pools << pool
-    end
-
-    def select_pool(candidates, tag_sets, acceptable_latency)
-      tag_sets = [tag_sets] unless tag_sets.is_a?(Array)
-
-      if !tag_sets.empty?
-        matches = []
-        tag_sets.detect do |tag_set|
-          matches = candidates.select do |candidate|
-            tag_set.none? { |k,v| candidate.tags[k.to_s] != v } &&
-            candidate.ping_time
-          end
-          !matches.empty?
-        end
-      else
-        matches = candidates
-      end
-
-      matches.empty? ? nil : near_pool(matches, acceptable_latency)
-    end
-
-    def near_pool(pool_set, acceptable_latency)
-      nearest_pool = pool_set.min_by { |pool| pool.ping_time }
-      near_pools = pool_set.select do |pool|
-        (pool.ping_time - nearest_pool.ping_time) <= acceptable_latency
-      end
-      near_pools[ rand(near_pools.length) ]
     end
 
     # Iterate through the list of provided seed
--- lib/mongo/util/read_preference.rb
+++ lib/mongo/util/read_preference.rb
@@ -16,5 +16,32 @@ module Mongo
           "Please specify one of the following read preferences as a symbol: #{READ_PREFERENCES}"
       end
     end
+
+    def select_read_pool(candidates, tag_sets, acceptable_latency)
+      tag_sets = [tag_sets] unless tag_sets.is_a?(Array)
+
+      if !tag_sets.empty?
+        matches = []
+        tag_sets.detect do |tag_set|
+          matches = candidates.select do |candidate|
+            tag_set.none? { |k,v| candidate.tags[k.to_s] != v } &&
+            candidate.ping_time
+          end
+          !matches.empty?
+        end
+      else
+        matches = candidates
+      end
+
+      matches.empty? ? nil : select_near_pool(matches, acceptable_latency)
+    end
+
+    def select_near_pool(pool_set, acceptable_latency)
+      nearest_pool = pool_set.min_by { |pool| pool.ping_time }
+      near_pools = pool_set.select do |pool|
+        (pool.ping_time - nearest_pool.ping_time) <= acceptable_latency
+      end
+      near_pools[ rand(near_pools.length) ]
+    end
   end
 end
