c8ce44df25220a62fa0c30e51a2a7d0aaeb64b71
Chris Heald <cheald@gmail.com>
2012-12-29 16:24:09 -0700

    Minor fixes to pool reuse tests (refactor, cleanup used connections)

--- test/auxillary/pool_reuse_test.rb
+++ test/auxillary/pool_reuse_test.rb
@@ -8,14 +8,22 @@ class PoolReuseTest < Test::Unit::TestCase
     @conn["admin"].command(:serverStatus => 1)["connections"]["current"]
   end
 
+  def setup
+    ensure_cluster(:rs, :replicas => 2, :arbiters => 1)
+    connect
+  end
+
   def teardown
     @@cluster.stop if @@cluster
+    @conn.close if @conn
+  end
+
+  def connect
+    @conn.close if @conn
+    @conn = MongoReplicaSetClient.new(["%s:%s" % [@rs.primary.host, @rs.primary.port]])
   end
 
   def test_pool_resources_are_reused
-    ensure_cluster(:rs)
-    @conn = MongoReplicaSetClient.new(["%s:%s" % [@rs.primary.host, @rs.primary.port]])
-
     handles_before_refresh = count_open_file_handles
     10.times do
       @conn.hard_refresh!
@@ -24,12 +32,12 @@ class PoolReuseTest < Test::Unit::TestCase
   end
 
   def test_pool_connectability_after_cycling_members
-    ensure_cluster(:rs, :replicas => 2, :arbiters => 1)
-    conn = MongoReplicaSetClient.new(["%s:%s" % [@rs.primary.host, @rs.primary.port]])
-    db   = conn['sample-db']
+    db   = @conn['sample-db']
 
     assert_equal db.collection_names, []
-    @@cluster.primary.stop
+    old_primary = @@cluster.primary
+    @conn["admin"].command step_down_command rescue nil
+    old_primary.stop
 
     rescue_connection_failure do
       db.collection_names
@@ -39,10 +47,12 @@ class PoolReuseTest < Test::Unit::TestCase
     assert_equal db.collection_names, []
 
     # Start up the old primary
-    @@cluster.restart
+    old_primary.start
 
     # Stop the new primary
-    @@cluster.primary.stop
+    primary = @@cluster.primary
+    @conn["admin"].command step_down_command rescue nil
+    primary.stop
 
     # Wait for primary failover
     rescue_connection_failure do
