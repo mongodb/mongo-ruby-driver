55d16970712e0d90397580397a191fa126cedde8
Brandon Black <brandonmblack@gmail.com>
2013-01-17 02:23:49 -0800

    RUBY-532 fix for threading issue in socket pool

--- test/functional/pool_test.rb
+++ test/functional/pool_test.rb
@@ -5,51 +5,43 @@ class PoolTest < Test::Unit::TestCase
   include Mongo
 
   def setup
-    @connection = standard_connection
+    @client    ||= standard_connection({:pool_size => 500, :pool_timeout => 5})
+    @db         = @client.db(MONGO_TEST_DB)
+    @collection = @db.collection("pool_test")
   end
 
   def test_pool_affinity
-    @pool = Pool.new(@connection, TEST_HOST, TEST_PORT, :size => 5)
+    pool = Pool.new(@client, TEST_HOST, TEST_PORT, :size => 5)
 
-    @threads    = []
-
+    threads = []
     10.times do
-      @threads << Thread.new do
-        original_socket = @pool.checkout
-        @pool.checkin(original_socket)
+      threads << Thread.new do
+        original_socket = pool.checkout
+        pool.checkin(original_socket)
         5000.times do
-          socket = @pool.checkout
+          socket = pool.checkout
           assert_equal original_socket, socket
-          @pool.checkin(socket)
+          pool.checkin(socket)
         end
       end
     end
 
-    @threads.each { |t| t.join }
+    threads.each { |t| t.join }
   end
 
-  def test_pool_thread_pruning
-    @pool = Pool.new(@connection, TEST_HOST, TEST_PORT, :size => 5)
-
-    @threads = []
-
-    10.times do
-      @threads << Thread.new do
-        50.times do
-          socket = @pool.checkout
-          @pool.checkin(socket)
-        end
-      end
+  def test_pool_affinity_max_size
+    8000.times {|x| @collection.insert({:value => x})}
+    threads = []
+    threads << Thread.new do
+      @collection.find({"value" => {"$lt" => 100}}).each {|e| e}
+      Thread.pass
+      sleep(5)
+      @collection.find({"value" => {"$gt" => 100}}).each {|e| e}
     end
-
-    @threads.each { |t| t.join }
-    assert_equal 10, @pool.instance_variable_get(:@threads_to_sockets).size
-
-    # Thread-socket pool
-    10000.times do
-      @pool.checkin(@pool.checkout)
+    sleep(1)
+    threads << Thread.new do
+      @collection.find({'$where' => "function() {for(i=0;i<8000;i++) {this.value};}"}).each {|e| e}
     end
-
-    assert_equal 1, @pool.instance_variable_get(:@threads_to_sockets).size
+    threads.each(&:join)
   end
 end
