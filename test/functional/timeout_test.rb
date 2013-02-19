a96bc6e98d0c38408850b2317de36030bac2464f
Brandon Black <brandonmblack@gmail.com>
2013-02-19 10:31:51 -0800

    minor: fixing db names in tests, standardizing on MONGO_TEST_DB

--- test/functional/timeout_test.rb
+++ test/functional/timeout_test.rb
@@ -3,24 +3,24 @@ require 'test_helper'
 class TestTimeout < Test::Unit::TestCase
   def test_op_timeout
     connection = standard_connection(:op_timeout => 1)
-    
+
     admin = connection.db('admin')
 
     command = {:eval => "sleep(500)"}
     # Should not timeout
     assert admin.command(command)
-   
+
     # Should timeout
     command = {:eval => "sleep(1500)"}
     assert_raise Mongo::OperationTimeout do
-      admin.command(command) 
+      admin.command(command)
     end
   end
 
   def test_external_timeout_does_not_leave_socket_in_bad_state
     client = Mongo::MongoClient.new
-    db = client['testdb']
-    coll = db['testcoll']
+    db = client[MONGO_TEST_DB]
+    coll = db['timeout-tests']
 
     # prepare the database
     coll.drop
@@ -43,14 +43,13 @@ class TestTimeout < Test::Unit::TestCase
 =begin
   def test_ssl_op_timeout
     connection = standard_connection(:op_timeout => 1, :ssl => true)
-    
+
     coll = connection.db(MONGO_TEST_DB).collection("test")
     coll.insert({:a => 1})
-   
     # Should not timeout
     assert coll.find_one({"$where" => "sleep(100); return true;"})
 
-    # Should timeout 
+    # Should timeout
     assert_raise Mongo::OperationTimeout do
       coll.find_one({"$where" => "sleep(5 * 1000); return true;"})
     end
