9f9b2ba86477714e4ee50a036418ec64fa1552ff
eranb <barakviz@gmail.com>
2013-03-19 11:21:42 +0200

    Patch only ruby-2.0.0-p0

--- ext/cbson/extconf.rb
+++ ext/cbson/extconf.rb
@@ -8,7 +8,7 @@ have_header("ruby/encoding.h")
 
 dir_config('cbson')
 
-if RUBY_VERSION >= "2.0.0"
+if "#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}".eql? '2.0.0-p0'
   FileUtils.mkdir "../bson_ext/bson_ext"
   create_makefile('bson_ext/cbson')  
 else
