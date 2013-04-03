31dd7cda7e2251cc114ae71eb01319043337f448
Emily <emily@10gen.com>
2013-04-02 23:40:06 -0400

    RUBY-568-max-message-size updates after code review

--- lib/mongo.rb
+++ lib/mongo.rb
@@ -16,7 +16,7 @@ module Mongo
                 ]
 
   DEFAULT_MAX_BSON_SIZE = 4 * 1024 * 1024
-  DEFAULT_MAX_MESSAGE_SIZE = DEFAULT_MAX_BSON_SIZE * 2
+  MESSAGE_SIZE_FACTOR = 2
 
   module Constants
     OP_REPLY        = 1
