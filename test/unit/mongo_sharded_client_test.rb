d2afc9b1d2fbb6fcc54393a64c624b5b5e1b5d94
Brandon Black <brandonmblack@gmail.com>
2013-04-21 16:21:10 -0700

    RUBY-584 fixing unit tests that require a mongodb connection

--- test/unit/mongo_sharded_client_test.rb
+++ test/unit/mongo_sharded_client_test.rb
@@ -9,24 +9,24 @@ class MongoShardedClientTest < Test::Unit::TestCase
 
   def test_initialize_with_single_mongos_uri
     ENV["MONGODB_URI"] = "mongodb://localhost:27017"
-    client = MongoShardedClient.new
+    client = MongoShardedClient.new(:connect => false)
     assert_equal [[ "localhost", 27017 ]], client.seeds
   end
 
   def test_initialize_with_multiple_mongos_uris
     ENV["MONGODB_URI"] = "mongodb://localhost:27017,localhost:27018"
-    client = MongoShardedClient.new
+    client = MongoShardedClient.new(:connect => false)
     assert_equal [[ "localhost", 27017 ], [ "localhost", 27018 ]], client.seeds
   end
 
   def test_from_uri_with_string
-    client = MongoShardedClient.from_uri("mongodb://localhost:27017,localhost:27018")
+    client = MongoShardedClient.from_uri("mongodb://localhost:27017,localhost:27018", :connect => false)
     assert_equal [[ "localhost", 27017 ], [ "localhost", 27018 ]], client.seeds
   end
 
   def test_from_uri_with_env_variable
     ENV["MONGODB_URI"] = "mongodb://localhost:27017,localhost:27018"
-    client = MongoShardedClient.from_uri
+    client = MongoShardedClient.from_uri(nil, :connect => false)
     assert_equal [[ "localhost", 27017 ], [ "localhost", 27018 ]], client.seeds
   end
 end
