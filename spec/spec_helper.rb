2fe3bf7eeaff241df3583fcc9fbe97b9f42310fc
Tyler Brock <tyler.brock@gmail.com>
2013-04-23 15:33:43 -0400

    RUBY-590: remove RSpec as part of cleanup

--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'mongo'
-require 'rspec/autorun'
-
-RSpec.configure do |config|
-  config.color     = true
-  config.fail_fast = true
-  config.formatter = 'documentation'
-  config.alias_it_should_behave_like_to :it_can_be, "it can be"
-end
