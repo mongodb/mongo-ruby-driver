671feccb38193fab92fb4b98456703f7883928bd
estolfo <emily@10gen.com>
2013-05-14 18:31:59 -0400

    RUBY-578 Set max sizes as an attribute on a node

--- test/replica_set/max_values_test.rb
+++ test/replica_set/max_values_test.rb
@@ -7,6 +7,12 @@ class MaxValuesTest < Test::Unit::TestCase
   def setup
     ensure_cluster(:rs)
     @client = MongoReplicaSetClient.new(@rs.repl_set_seeds, :name => @rs.repl_set_name)
+    @db = new_mock_db
+    @client.stubs(:[]).returns(@db)
+    @ismaster = {
+      'hosts' => @client.local_manager.hosts.to_a,
+      'arbiters' => @client.local_manager.arbiters
+    }
   end
 
   def test_initial_max_sizes
@@ -15,43 +21,52 @@ class MaxValuesTest < Test::Unit::TestCase
   end
 
   def test_updated_max_sizes_after_node_config_change
-    # stub max sizes for one member
-    @client.local_manager.members.each_with_index do |m, i|
-      if i % 2 == 0
-        m.stubs(:config).returns({'maxMessageSizeBytes' => 1024 * MESSAGE_SIZE_FACTOR, 'maxBsonObjectSize' => 1024})
-        m.set_config
-      end
-    end
+    @db.stubs(:command).returns(
+      @ismaster.merge({'ismaster' => true}),
+      @ismaster.merge({'secondary' => true, 'maxMessageSizeBytes' => 1024 * MESSAGE_SIZE_FACTOR}),
+      @ismaster.merge({'secondary' => true, 'maxBsonObjectSize' => 1024})
+    )
+    @client.local_manager.stubs(:refresh_required?).returns(true)
+    @client.refresh
 
     assert_equal 1024, @client.max_bson_size
     assert_equal 1024 * MESSAGE_SIZE_FACTOR, @client.max_message_size
   end
 
   def test_neither_max_sizes_in_config
-    @client.local_manager.members.each do |m|
-      m.stubs(:config).returns({})
-      m.set_config
-    end
+    @db.stubs(:command).returns(
+      @ismaster.merge({'ismaster' => true}),
+      @ismaster.merge({'secondary' => true}),
+      @ismaster.merge({'secondary' => true})
+    )
+    @client.local_manager.stubs(:refresh_required?).returns(true)
+    @client.refresh
 
     assert_equal DEFAULT_MAX_BSON_SIZE, @client.max_bson_size
     assert_equal DEFAULT_MAX_BSON_SIZE * MESSAGE_SIZE_FACTOR, @client.max_message_size
   end
 
   def test_only_bson_size_in_config
-    @client.local_manager.members.each do |m|
-      m.stubs(:config).returns({'maxBsonObjectSize' => 1024})
-      m.set_config
-    end
+    @db.stubs(:command).returns(
+      @ismaster.merge({'ismaster' => true}),
+      @ismaster.merge({'secondary' => true}),
+      @ismaster.merge({'secondary' => true, 'maxBsonObjectSize' => 1024})
+    )
+    @client.local_manager.stubs(:refresh_required?).returns(true)
+    @client.refresh
+
     assert_equal 1024, @client.max_bson_size
     assert_equal 1024 * MESSAGE_SIZE_FACTOR, @client.max_message_size
   end
 
   def test_both_sizes_in_config
-    @client.local_manager.members.each do |m|
-      m.stubs(:config).returns({'maxMessageSizeBytes' => 1024 * 2 * MESSAGE_SIZE_FACTOR,
-                                'maxBsonObjectSize' => 1024})
-      m.set_config
-    end
+    @db.stubs(:command).returns(
+      @ismaster.merge({'ismaster' => true, 'maxMessageSizeBytes' => 1024 * 2 * MESSAGE_SIZE_FACTOR, 'maxBsonObjectSize' => 1024}),
+      @ismaster.merge({'secondary' => true, 'maxMessageSizeBytes' => 1024 * 2 * MESSAGE_SIZE_FACTOR, 'maxBsonObjectSize' => 1024}),
+      @ismaster.merge({'secondary' => true, 'maxMessageSizeBytes' => 1024 * 2 * MESSAGE_SIZE_FACTOR, 'maxBsonObjectSize' => 1024})
+    )
+    @client.local_manager.stubs(:refresh_required?).returns(true)
+    @client.refresh
 
     assert_equal 1024, @client.max_bson_size
     assert_equal 1024 * 2 * MESSAGE_SIZE_FACTOR, @client.max_message_size
