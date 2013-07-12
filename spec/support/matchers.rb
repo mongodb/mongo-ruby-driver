760f63e46a8308cccff4b33aa0440243ec26dbd7
estolfo <emily@10gen.com>
2013-07-12 10:54:23 -0400

    minor: remove be_String from spec matchers

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -35,9 +35,3 @@ RSpec::Matchers.define :be_bson_sequence do |array|
     actual == array.map(&:to_bson).join
   end
 end
-
-RSpec::Matchers.define :be_String do
-  match do |object|
-    object.is_a?(String)
-  end
-end
