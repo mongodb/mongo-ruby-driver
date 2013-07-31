0d9bcc82282de5b2afcc271c67dd5e4d9b964606
Tyler Brock <tyler.brock@gmail.com>
2013-07-31 10:19:50 -0400

    minor: only allow expect mocking

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,10 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.mock_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
 
 TEST_DB      = 'ruby-driver'
