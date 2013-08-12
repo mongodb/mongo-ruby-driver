f2866ed5019b439805733828edc66a75ed773aba
Durran Jordan <durran@gmail.com>
2013-08-12 14:41:10 +0200

    Initial commit of Cluster and spec

--- lib/mongo/cluster.rb
+++ lib/mongo/cluster.rb
@@ -14,16 +14,42 @@
 
 module Mongo
 
+  # Represents a group of nodes on the server side, either as a single node, a
+  # replica set, or a single or multiple mongos.
+  #
+  # @since 2.0.0
   class Cluster
 
-    attr_reader :addresses
+    attr_reader :addresses, :nodes
 
+    # Determine if this cluster of nodes is equal to another object. Checks the
+    # nodes currently in the cluster, not what was configured.
+    #
+    # @example Is the cluster equal to the object?
+    #   cluster == other
+    #
+    # @param [ Object ] other The object to compare to.
+    #
+    # @return [ true, false ] If the objects are equal.
+    #
+    # @since 2.0.0
     def ==(other)
-      addresses == other.addresses
+      return false unless other.is_a?(Cluster)
+      nodes == other.nodes
     end
 
-    def initialize(addresses)
+    # Instantiate the new cluster.
+    #
+    # @example Instantiate the cluster.
+    #   Mongo::Cluster.new(["127.0.0.1:27017"])
+    #
+    # @param [ Array<String> ] addresses The addresses of the configured nodes.
+    # @param [ Hash ] options The options.
+    #
+    # @since 2.0.0
+    def initialize(addresses, options = {})
       @addresses = addresses
+      @nodes = addresses.map { |address| Node.new(address, options) }
     end
   end
 end
