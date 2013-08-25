9cf7264949dc52a8d2b998d6abf1352672e7f13f
Durran Jordan <durran@gmail.com>
2013-08-25 20:31:57 +0200

    Adding node refresher

--- lib/mongo/node.rb
+++ lib/mongo/node.rb
@@ -63,12 +63,18 @@ module Mongo
       @cluster = cluster
       @address = address
       @options = options
+      @refresher = Refresher.new(self, refresh_interval)
+      @refresher.run
     end
 
     # @todo This should be synchronized. I envison this checks if the node is
     # alive and a primary or secondary. (no arbiters)
     def operable?
       true
+    end
+
+    def refresh
+      p 'Refreshing node...'
     end
 
     # Get the refresh interval for the node. This will be defined via an option
@@ -82,6 +88,50 @@ module Mongo
     # @since 2.0.0
     def refresh_interval
       @refresh_interval ||= options[:refresh_interval] || REFRESH_INTERVAL
+    end
+
+    # This object is responsible for keeping node status up to date, running in
+    # a separate thread as to not disrupt other operations.
+    #
+    # @since 2.0.0
+    class Refresher
+
+      # @return [ Mongo::Node ] The node the refresher refreshes.
+      attr_reader :node
+      # @return [ Integer ] The interval the refresh happens on, in seconds.
+      attr_reader :interval
+
+      # Create the new node refresher.
+      #
+      # @example Create the node refresher.
+      #   Mongo::Node::Refresher.new(node, 5)
+      #
+      # @param [ Mongo::Node ] node The node to refresh.
+      # @param [ Integer ] interval The refresh interval in seconds.
+      #
+      # @since 2.0.0
+      def initialize(node, interval)
+        @node = node
+        @interval = interval
+      end
+
+      # Runs the node refresher. Refreshing happens on a separate thread per
+      # node.
+      #
+      # @example Run the refresher.
+      #   refresher.run
+      #
+      # @return [ Thread ] The thread the refresher runs on.
+      #
+      # @since 2.0.0
+      def run
+        Thread.new(interval, node) do |i, n|
+          loop do
+            n.refresh
+            sleep(i)
+          end
+        end
+      end
     end
 
     private
