7d8f1fd322138dcca34e5174a8cc4b7dadf669e0
Emily Stolfo <emily@10gen.com>
2013-09-24 17:59:32 -0400

    RUBY-661 adding server_op_timeout to find_one

--- lib/mongo/collection.rb
+++ lib/mongo/collection.rb
@@ -356,7 +356,9 @@ module Mongo
              else
                raise TypeError, "spec_or_object_id must be an instance of ObjectId or Hash, or nil"
              end
-      find(spec, opts.merge(:limit => -1)).next_document
+      timeout = opts.delete(:server_op_timeout)
+      cursor = find(spec, opts.merge(:limit => -1))
+      timeout ? cursor.server_op_timeout(timeout).next_document : cursor.next_document
     end
 
     # Save a document to this collection.
