d7892be8c5d1e9c1d00e8193eb87aa88998b11e5
Durran Jordan <durran@gmail.com>
2014-02-10 10:33:45 -0500

    Update pool checkin/out

--- lib/mongo/pool.rb
+++ lib/mongo/pool.rb
@@ -19,14 +19,86 @@ module Mongo
     # The default timeout for getting connections from the queue.
     TIMEOUT = 0.5
 
-    def checkin(connection)
+    # @return [ Hash ] options The pool options.
+    attr_reader :options
+
+    # Check a connection back into the pool. Will pull the connection from a
+    # thread local stack that should contain it after it was checked out.
+    #
+    # @example Checkin the thread's connection to the pool.
+    #   pool.checkin
+    #
+    # @since 3.0.0
+    def checkin
+      connection = pinned_connections.pop
+      queue.enqueue(connection) if pinned_connections.empty?
     end
 
+    # Check a connection out from the pool.
+    #
+    # @example Check a connection out from the pool.
+    #   pool.checkout
+    #
+    # @return [ Mongo::Pool::Connection ] The checked out connection.
+    #
+    # @since 3.0.0
     def checkout
+      if pinned_connections.empty?
+        connection = queue.dequeue(timeout)
+      else
+        connection = pinned_connections.pop
+      end
+      stack.push(connection) and connection
     end
 
+    # Create the new connection pool.
+    #
+    # @example Create the new connection pool.
+    #   Pool.new(timeout: 0.5) do
+    #     Connection.new
+    #   end
+    #
+    # @note A block must be passed to set up the connections on initialization.
+    #
+    # @param [ Hash ] options The connection pool options.
+    #
+    # @since 3.0.0
     def initialize(options = {}, &block)
+      @options = options
+      @queue = Queue.new(pool_size, &block)
+      @identifier = :"mongodb-pool-#{queue.object_id}"
+    end
 
+    # Get the default size of the connection pool.
+    #
+    # @example Get the pool size.
+    #   pool.pool_size
+    #
+    # @return [ Integer ] The size of the pool.
+    #
+    # @since 3.0.0
+    def pool_size
+      @pool_size ||= options[:pool_size] || POOL_SIZE
+    end
+
+    # Get the timeout for checking connections out from the pool.
+    #
+    # @example Get the pool timeout.
+    #   pool.timeout
+    #
+    # @return [ Float ] The pool timeout.
+    #
+    # @since 3.0.0
+    def timeout
+      @timeout ||= options[:timeout] || TIMEOUT
+    end
+
+    private
+
+    attr_reader :queue, :identifier
+
+    def pinned_connections
+      ::Thread.current[identifier] ||= []
     end
 
     class << self
