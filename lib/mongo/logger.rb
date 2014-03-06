936676330ab160202e2196f4cc294eebd2d714dc
Brandon Black <brandonmblack@gmail.com>
2014-03-05 20:12:22 -0800

    minor: fixing tests, stdlib logger require was missing

--- lib/mongo/logger.rb
+++ lib/mongo/logger.rb
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'logger'
+
 module Mongo
 
   # Provides ability to log messages.
