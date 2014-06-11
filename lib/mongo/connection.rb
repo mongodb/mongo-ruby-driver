5b516882ca914affcc5679f058e59cca02483070
Durran Jordan <durran@gmail.com>
2014-06-11 16:56:28 -0400

    Fix connection on jruby

--- lib/mongo/connection.rb
+++ lib/mongo/connection.rb
@@ -145,8 +145,7 @@ module Mongo
     attr_reader :socket, :ssl_opts
 
     def ensure_connected
-      connect! if socket.nil?
-      socket.connect! if !socket.alive?
+      connect! if socket.nil? || !socket.alive?
       yield socket
     end
   end
