b0894189c218b56d602d72e45f09672c884219c3
Samantha Ritter <samantha.ritter@10gen.com>
2014-06-12 17:29:23 -0400

    Mocked-up classes for write responses.

---
+++ lib/mongo/response/write/insert.rb
@@ -0,0 +1,28 @@
+# Copyright (C) 2009-2014 MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module Mongo
+  module Response
+    module Write
+
+      # A response object for insert operations, OP_INSERT.
+      #
+      # @since 2.0.0
+      class Insert
+        include Responsive
+        include Writable
+      end
+    end
+  end
+end
