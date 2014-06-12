9dc51a85995e4a1b83bc279134facad68fa813b0
Samantha Ritter <samantha.ritter@10gen.com>
2014-06-12 17:10:05 -0400

    Mock of response-related error classes

---
+++ lib/mongo/response/error.rb
@@ -0,0 +1,6 @@
+# Errorable mix-in
+require 'mongo/response/error/errorable.rb'
+
+# Response error types
+require 'mongo/response/error/write_error.rb'
+require 'mongo/response/error/write_concern_error.rb'
---
+++ lib/mongo/response/error/write_concern_error.rb
@@ -0,0 +1,36 @@
+# Copyright (C) 2009-2014 MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module Mongo
+  module Response
+    module Error
+
+      # An object representing a write concern error.
+      #
+      # @since 2.0.0
+      class WriteConcernError
+        include Errorable
+
+        # A document identifying the write concern setting related the error.
+        #
+        # @return [ Hash ] information about this error.
+        #
+        # @since 2.0.0
+        def info
+          {}
+        end
+      end
+    end
+  end
+end
