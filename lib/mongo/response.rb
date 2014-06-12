d303f2b04a9cc091e0e914253930b8ed199756f8
Samantha Ritter <samantha.ritter@10gen.com>
2014-06-12 17:30:24 -0400

    Responsive module.

---
+++ lib/mongo/response.rb
@@ -0,0 +1,11 @@
+# Responsive mix-in
+require 'mongo/response/responsive'
+
+# Response types
+require 'mongo/response/read'
+require 'mongo/response/write'
+require 'mongo/response/insert'
+require 'mongo/response/bulk'
+
+# Errors
+require 'mongo/response/error'
