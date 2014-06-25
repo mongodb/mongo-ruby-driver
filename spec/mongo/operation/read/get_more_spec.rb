36d62157be4f8b3ad96145311f93d2080d4ce797
Emily Stolfo <emily@mongodb.com>
2014-06-25 10:54:09 -0400

    Refactor get more operation

--- spec/mongo/operation/read/get_more_spec.rb
+++ spec/mongo/operation/read/get_more_spec.rb
@@ -7,34 +7,64 @@ describe Mongo::Operation::Read::GetMore do
   let(:cursor_id) { 1 }
 
   let(:spec) do
-    { :db_name   => db_name,
-      :coll_name => coll_name,
-      :to_return => to_return,
+    { :to_return => to_return,
       :cursor_id => cursor_id }
   end
 
-  let(:op) { described_class.new(spec) }
+  let(:op) { described_class.new(collection, spec) }
 
   describe '#initialize' do
 
     it 'sets the spec' do
       expect(op.spec).to be(spec)
     end
+
+    it 'sets the collection' do
+      expect(op.collection).to be(collection)
+    end
   end
 
   describe '#==' do
 
-    context ' when two ops have different specs' do
-      let(:other_spec) do
-        { :db_name   => 'test_db',
-          :coll_name => 'test_coll',
-          :to_return => 50,
-          :cursor_id => 2 }
-      end
-      let(:other) { described_class.new(other_spec) }
+    context 'spec' do
 
-      it 'returns false' do
-        expect(op).not_to eq(other)
+      context 'when two ops have the same specs' do
+        let(:other) { described_class.new(collection, spec) }
+
+        it 'returns true' do
+          expect(op).to eq(other)
+        end
+      end
+
+      context ' when two ops have different specs' do
+        let(:other_spec) do
+          { :to_return => 50,
+            :cursor_id => 2 }
+        end
+        let(:other) { described_class.new(collection, other_spec) }
+  
+        it 'returns false' do
+          expect(op).not_to eq(other)
+        end
+      end
+    end
+
+    context 'collection' do
+      context 'when two ops have the same collection' do
+        let(:other) { described_class.new(collection, spec) }
+
+        it 'returns true' do
+          expect(op).to eq(other)
+        end
+      end
+
+      context 'when two ops have different collections' do
+        let(:other_collection) { double('collection') }
+        let(:other) { described_class.new(other_collection, spec) }
+
+        it 'returns false' do
+          expect(op).not_to eq(other)
+        end
       end
     end
   end
@@ -45,8 +75,8 @@ describe Mongo::Operation::Read::GetMore do
 
       it 'creates a get more wire protocol message with correct specs' do
         expect(Mongo::Protocol::GetMore).to receive(:new) do |db, coll, to_ret, id|
-          expect(db).to eq(db_name)
-          expect(coll).to eq(coll_name)
+          expect(db).to eq(collection.database.name)
+          expect(coll).to eq(collection.name)
           expect(to_ret).to eq(to_return)
           expect(id).to eq(cursor_id)
         end
