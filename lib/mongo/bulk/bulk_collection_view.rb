e3dbd3364f5abaa2ad0ab3f3616fedc0d9d82325
Emily Stolfo <emily@10gen.com>
2014-07-03 15:03:07 -0400

    Documentation for BulkCollectionView

--- lib/mongo/bulk/bulk_collection_view.rb
+++ lib/mongo/bulk/bulk_collection_view.rb
@@ -13,54 +13,123 @@
 # limitations under the License.
 
 module Mongo
+
   module Bulk
+
     class BulkCollectionView
 
+      # Initialize the BulkCollectionView object.
+      # Encapsulates any kind of write operation that requires a selector.
+      #
+      # @params [ Mongo::Bulk::BulkWrite ] bulk_write The +BulkWrite+ object
+      #   that will handle the actual execution of the batch operations.
+      # @params [ Hash ] selector The selector associated with the write
+      #   operation.
+      #
+      # @since 2.0.0
       def initialize(bulk_write, selector)
         @bulk_write = bulk_write
         @selector   = selector
         @upsert     = false
       end
 
+      # Set the upsert flag for the write operation.
+      #
+      # @return [ self ] This object so that methods can be chained.
+      #
+      # @since 2.0.0
       def upsert
         @upsert = true
         self
       end
 
-      # op creators
+      # Update one document that matches the selector.
+      #
+      # @params [ Hash ] update_doc The document representing the update.
+      #
+      # @return [ self ] This object so that methods can be chained.
+      #
+      # @since 2.0.0
       def update_one(update_doc)
         raise Exception unless update_doc?(update_doc)
         multi_or_single_update(false, update_doc)
       end
 
+      # Update all documents matching the selector.
+      #
+      # @params [ Hash ] update_doc The document representing the update.
+      #
+      # @return [ self ] This object so that methods can be chained.
+      #
+      # @since 2.0.0
       def update(update_doc)
         raise Exception unless update_doc?(update_doc)
         multi_or_single_update(true, update_doc)
       end
 
+      # Replace one document matching the selector.
+      #
+      # @params [ Hash ] replace_doc The document representing the
+      #   replacement.
+      #
+      # @return [ self ] This object so that methods can be chained.
+      #
+      # @since 2.0.0
       def replace_one(replace_doc)
         raise Exception unless replace_doc?(replace_doc)
         multi_or_single_update(false, replace_doc)
       end
 
+      # Remove one document matching the selector.
+      #
+      # @return [ self ] This object so that methods can be chained.
+      #
+      # @since 2.0.0
       def remove_one
         multi_or_single_remove(false)
       end
 
+      # Remove all documents matching the selector.
+      #
+      # @return [ self ] This object so that methods can be chained.
+      #
+      # @since 2.0.0
       def remove
         multi_or_single_remove(true)
       end
 
       private
 
+      # Is the document a valid update document?
+      # The first key must start with '$' and the document cannot be empty.
+      #
+      # @return [ true, false ] Whether the doc is a valid update doc.
+      #
+      # @since 2.0.0
       def update_doc?(doc)
         !doc.empty? && doc.keys.first.to_s =~ /^\$/
       end
 
+      # Is the document a valid replacement document?
+      # No top-level keys may start with '$'.
+      #
+      # @return [ true, false ] Whether the doc is a valid replacement doc.
+      #
+      # @since 2.0.0
       def replace_doc?(doc)
         doc.keys.all?{|key| key !~ /^\$/}
       end
 
+      # Either do a multi update or a single update.
+      #
+      # @params [ true, false ] multi Whether the operation is a multi or single
+      #   update.
+      # @params [ Hash ] update_doc The document representing the update. Either
+      #   a replacement or update document.
+      #
+      # @return [ Mongo::Bulk::BulkWrite ] The bulk write object.
+      #
+      # @since 2.0.0
       def multi_or_single_update(multi = false, update_doc)
         raise Exception unless @selector
 
@@ -78,6 +147,14 @@ module Mongo
         end
       end
 
+      # Either do a multi remove or a single remove.
+      #
+      # @params [ true, false ] multi Whether the remove is a multi or single
+      #   remove.
+      #
+      # @return [ Mongo::Bulk::BulkWrite ] The bulk write object.
+      #
+      # @since 2.0.0
       def multi_or_single_remove(multi = false)
         raise Exception unless @selector
         
