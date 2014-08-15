a2300f98339fd05519abab60a03270ad4bb0a03c
Emily Stolfo <emily@10gen.com>
2014-08-15 17:06:04 +0200

    Logic mistake

--- test/helpers/test_unit.rb
+++ test/helpers/test_unit.rb
@@ -392,7 +392,7 @@ class Test::Unit::TestCase
   def self.cleanup_admin_user
     not_cluster = TEST_BASE.class_eval { class_variables }.none? { |v| v =~ /@@cluster_/ }
 
-    if @@connected_single_mongod || not_cluster
+    if @@connected_single_mongod && not_cluster
       client = Mongo::MongoClient.from_uri(TEST_URI)
       db = client[TEST_DB]
       begin
