6e2a60cf57c780abf97d7486922f697accf2a29a
Durran Jordan <durran@gmail.com>
2014-08-26 11:11:05 +0200

    RUBY-596 Ensure limit is used during remove from collection

--- spec/mongo/collection_view/modifiable_spec.rb
+++ spec/mongo/collection_view/modifiable_spec.rb
@@ -41,5 +41,46 @@ describe Mongo::CollectionView::Modifiable do
         expect(response.n).to eq(2)
       end
     end
+
+    context 'when limiting the number removed' do
+
+      context 'when a selector was provided' do
+
+        before do
+          collection.insert([{ field: 'test1' }, { field: 'test1' }])
+        end
+
+        after do
+          collection.find.remove
+        end
+
+        let(:response) do
+          collection.find(field: 'test1').limit(1).remove
+        end
+
+        it 'deletes the first matching document in the collection' do
+          expect(response.n).to eq(1)
+        end
+      end
+
+      context 'when no selector was provided' do
+
+        before do
+          collection.insert([{ field: 'test1' }, { field: 'test2' }])
+        end
+
+        let(:response) do
+          collection.find.limit(1).remove
+        end
+
+        after do
+          collection.find.remove
+        end
+
+        it 'deletes the first document in the collection' do
+          expect(response.n).to eq(1)
+        end
+      end
+    end
   end
 end
