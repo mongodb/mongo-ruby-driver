18f1e0368401ed31ef8b5b744549cb2867d09832
Durran Jordan <durran@gmail.com>
2014-08-26 11:07:31 +0200

    RUBY-596 Implement remove for collection

---
+++ spec/mongo/collection_view/modifiable_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Mongo::CollectionView::Modifiable do
+
+  describe '#remove' do
+
+    let(:collection) do
+      authorized_client[TEST_COLL]
+    end
+
+    context 'when a selector was provided' do
+
+      before do
+        collection.insert([{ field: 'test1' }, { field: 'test2' }])
+      end
+
+      after do
+        collection.find.remove
+      end
+
+      let(:response) do
+        collection.find(field: 'test1').remove
+      end
+
+      it 'deletes the matching documents in the collection' do
+        expect(response.n).to eq(1)
+      end
+    end
+
+    context 'when no selector was provided' do
+
+      before do
+        collection.insert([{ field: 'test1' }, { field: 'test2' }])
+      end
+
+      let(:response) do
+        collection.find.remove
+      end
+
+      it 'deletes all the documents in the collection' do
+        expect(response.n).to eq(2)
+      end
+    end
+  end
+end
