b37c1635b48c9a676cccf5df7d7b358ca5ca3ae3
Emily Stolfo <emily@10gen.com>
2014-09-04 10:48:19 +0200

    RUBY-788 Check pool accounting after socket error

--- test/functional/pool_test.rb
+++ test/functional/pool_test.rb
@@ -95,4 +95,41 @@ class PoolTest < Test::Unit::TestCase
     # # No semaphore leak, the pool is allowed to make a new socket.
     assert_equal 1, pool.instance_variable_get(:@sockets).size
   end
+
+  def test_socket_cleanup
+    # Get a client with one socket so we detect if it's leaked.
+    client = MongoClient.new(TEST_HOST, TEST_PORT, :pool_size => 1, :pool_timeout => 1)
+    assert_equal 1, client.pool_size
+
+    # Set up the client with a pool
+    client[TEST_DB].command(:ping => 1)
+
+    # Simulate an authenticate() call on a different socket.
+    # Cache the creds on the client.
+    creds = {
+        :db_name   => TEST_DB,
+        :username  => TEST_USER,
+        :password  => TEST_USER_PWD,
+        :source    => TEST_DB,
+        :mechanism => Mongo::Authentication::DEFAULT_MECHANISM,
+        :extra     => {}
+    }
+    client.auths << creds
+
+    # Mock the socket to raise a ConnectionFailure when applying auths
+    pool   = client.primary_pool
+    socket = pool.instance_variable_get(:@sockets).first
+    socket.expects(:send).raises(ConnectionFailure)
+
+    # Checkout a socket from the pool to force it to get a new socket
+    pool.checkout
+    new_socket = pool.instance_variable_get(:@sockets).first
+
+    # Make sure the pool is cleaned up properly
+    assert_not_equal socket, new_socket
+    assert_equal 1, pool.instance_variable_get(:@sockets).size
+    assert_equal 1, pool.instance_variable_get(:@thread_ids_to_sockets).size
+    assert !pool.instance_variable_get(:@checked_out).include?(socket)
+    assert !pool.instance_variable_get(:@sockets).include?(socket)
+  end
 end
