3778512c8cda953516e2996301ad443b696a2494
Durran Jordan <durran@gmail.com>
2014-09-11 18:01:06 +0200

    RUBY-596 Aggregate operation uses Specifiable

--- lib/mongo/operation/aggregate.rb
+++ lib/mongo/operation/aggregate.rb
@@ -18,51 +18,30 @@ module Mongo
   module Operation
 
     # A MongoDB aggregate operation.
-    # Note that an aggregate operation can behave like a read and
-    # return a result set, or can behave like a write operation and
-    # output results to a user-specified collection.
+    #
+    # @note An aggregate operation can behave like a read and return a 
+    #   result set, or can behave like a write operation and
+    #   output results to a user-specified collection.
+    #
+    # @example Create the aggregate operation.
+    #   Aggregate.new({
+    #     :selector => {
+    #       :aggregate => 'test_coll', :pipeline => [{ '$out' => 'test-out' }]
+    #     },
+    #     :db_name => 'test_db'
+    #   })
+    #
+    # @param [ Hash ] spec The specifications for the operation.
+    #
+    # @option spec :selector [ Hash ] The aggregate selector.
+    # @option spec :db_name [ String ] The name of the database on which
+    #   the operation should be executed.
+    # @option spec :options [ Hash ] Options for the aggregate command.
     #
     # @since 2.0.0
     class Aggregate
       include Executable
-
-      # Check equality of two aggregate operations.
-      #
-      # @example Check operation equality.
-      #   operation == other
-      #
-      # @param [ Object ] other The other operation.
-      #
-      # @return [ true, false ] Whether the objects are equal.
-      #
-      # @since 2.0.0
-      def ==(other)
-        # @todo: compare db name as well?
-        spec[:selector][:aggregate] == other.spec[:selector][:aggregate] &&
-            spec[:selector][:pipeline] == other.spec[:selector][:pipeline]
-      end
-      alias_method :eql?, :==
-
-      # Initialize an aggregate operation.
-      #
-      # @example
-      #   include Mongo
-      #   include Operation
-      #   Aggregate.new({ :selector => { :aggregate => 'test_coll',
-      #                                  :pipeline => [{ '$out' => 'test-out' }] },
-      #                   :db_name  => 'test_db' })
-      #
-      # @param [ Hash ] spec The specifications for the operation.
-      #
-      # @option spec :selector [ Hash ] The aggregate selector.
-      # @option spec :db_name [ String ] The name of the database on which
-      #   the operation should be executed.
-      # @option spec :options [ Hash ] Options for the aggregate command.
-      #
-      # @since 2.0.0
-      def initialize(spec)
-        @spec = spec
-      end
+      include Specifiable
 
       # Execute the operation.
       # The context gets a connection on which the operation
@@ -77,26 +56,20 @@ module Mongo
       #
       # @since 2.0.0
       def execute(context)
+        # @todo: Should we respect tag sets and options here?
         if context.server.secondary? && !secondary_ok?
           warn "Database command '#{selector.keys.first}' rerouted to primary server"
-          # @todo: Should we respect tag sets and options here?
           context = Mongo::ServerPreference.get(:mode => :primary).server.context
         end
-        context.with_connection do |connection|
-          # @todo: Return an aggregation result here.
-          Result.new(connection.dispatch([ message(context) ]))
-        end
+        execute_message(context)
       end
 
       private
 
-      # The selector for this aggregate command operation.
-      #
-      # @return [ Hash ] The selector describing this aggregate operation.
-      #
-      # @since 2.0.0
-      def selector
-        @spec[:selector]
+      def execute_message(context)
+        context.with_connection do |connection|
+          Result.new(connection.dispatch([ message(context) ]))
+        end
       end
 
       # Whether this operation can be executed on a replica set secondary server.
@@ -122,11 +95,6 @@ module Mongo
         selector.reject{ |option, value| option.to_s == 'cursor' }
       end
 
-      # The wire protocol message for this operation.
-      #
-      # @return [ Mongo::Protocol::Query ] Wire protocol message.
-      #
-      # @since 2.0.0
       def message(context)
         Protocol::Query.new(db_name, Database::COMMAND, filter(context), options)
       end
