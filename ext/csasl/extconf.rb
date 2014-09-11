65c24f2d0711ba44ce8a880c0fcab20eae63b0f0
Durran Jordan <durran@gmail.com>
2014-09-11 11:56:08 +0200

    RUBY-802: Create dummy Makefile when sasl not installed

--- ext/csasl/extconf.rb
+++ ext/csasl/extconf.rb
@@ -1,5 +1,8 @@
 require 'mkmf'
 find_header('sasl/sasl.h')
-have_library('sasl2', 'sasl_version')
 
-create_makefile('csasl/csasl')
+if have_library('sasl2', 'sasl_version')
+  create_makefile('csasl/csasl')
+else
+  dummy_makefile('csasl/csasl')
+end
