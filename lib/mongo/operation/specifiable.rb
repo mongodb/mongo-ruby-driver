ba2f431df52086176627520046029499d7cf46cb
Durran Jordan <durran@gmail.com>
2014-09-11 09:28:32 +0200

    RUBY-596 Add specifiable module

---
+++ lib/mongo/operation/specifiable.rb
@@ -0,0 +1,59 @@
+# Copyright (C) 2009-2014 MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module Mongo
+  module Operation
+
+    # This module contains common functionality for convenience methods getting
+    # various values from the spec.
+    #
+    # @since 2.0.0
+    module Specifiable
+
+      # @return [ Hash ] spec The specification for the operation.
+      attr_reader :spec
+
+      # Check equality of two specifiable operations.
+      #
+      # @example Are the operations equal?
+      #   operation == other
+      #
+      # @param [ Object ] other The other operation.
+      #
+      # @return [ true, false ] Whether the objects are equal.
+      #
+      # @since 2.0.0
+      def ==(other)
+        return false unless other.is_a?(Specifiable)
+        spec == other.spec
+      end
+      alias_method :eql?, :==
+
+      # Create the new specifiable operation.
+      #
+      # @example Create the new specifiable operation.
+      #   Specifiable.new(spec)
+      #
+      # @param [ Hash ] spec The operation specification.
+      #
+      # @see The individual operations for the values they require in their
+      #   specs.
+      #
+      # @since 2.0.0
+      def initialize(spec)
+        @spec = spec
+      end
+    end
+  end
+end
