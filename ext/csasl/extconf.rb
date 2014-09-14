fb8afdad931bcd171aae08e9bd352215f36b2e21
Durran Jordan <durran@gmail.com>
2014-09-15 01:16:35 +0200

    Add note on cyrus sasl in extconf

--- ext/csasl/extconf.rb
+++ ext/csasl/extconf.rb
@@ -4,5 +4,5 @@ find_header('sasl/sasl.h')
 if have_library('sasl2', 'sasl_version')
   create_makefile('csasl/csasl')
 else
-  abort "libsasl is required in the system to install the mongo-kerberos gem."
+  abort "libsasl (cyrus sasl) is required in the system to install the mongo-kerberos gem."
 end
