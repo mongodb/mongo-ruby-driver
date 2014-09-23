d90312d3efb87dadf34cedd560047f5d5b6ea726
Emily Stolfo <emily@mongodb.com>
2014-09-23 14:14:10 +0200

    RUBY-787 Specs for insert portion of Bulk API

---
+++ spec/mongo/bulk/bulk_write_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Mongo::Bulk::BulkWrite do
+
+  context 'ordered' do
+
+    let(:bulk) do
+      described_class.new(authorized_collection, ordered: true)
+    end
+
+    it_behaves_like 'a bulk write object'
+  end
+
+  context 'unordered' do
+
+    let(:bulk) do
+      described_class.new(authorized_collection, ordered: false)
+    end
+
+    it_behaves_like 'a bulk write object'
+  end
+end
