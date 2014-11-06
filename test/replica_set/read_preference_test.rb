614bda5351d47b5a34d35a3bba63ae300fccf85b
Emily Stolfo <emily@10gen.com>
2014-11-06 13:50:21 -0500

    RUBY-800 Fix read preference tests

--- test/replica_set/read_preference_test.rb
+++ test/replica_set/read_preference_test.rb
@@ -18,65 +18,62 @@ class ReadPreferenceTest < Test::Unit::TestCase
 
   def setup
     ensure_cluster(:rs, :replicas => 2, :arbiters => 0)
-
-    # Insert data
-    primary = @rs.primary
-    conn = Connection.new(primary.host, primary.port)
-    authenticate_client(conn)
-    db = conn.db(TEST_DB)
-    coll = db.collection("test-sets")
+    client = make_connection
+    db = client.db(TEST_DB)
+    coll = db.collection('test-sets')
     coll.save({:a => 20}, {:w => 2})
   end
 
   def test_read_primary
-    conn = make_connection
+    client = make_connection
     rescue_connection_failure do
-      assert conn.read_primary?
-      assert conn.primary?
+      assert client.read_primary?
+      assert client.primary?
     end
 
-    conn = make_connection(:primary_preferred)
+    client = make_connection(:primary_preferred)
     rescue_connection_failure do
-      assert conn.read_primary?
-      assert conn.primary?
+      assert client.read_primary?
+      assert client.primary?
     end
 
-    conn = make_connection(:secondary)
+    client = make_connection(:secondary)
     rescue_connection_failure do
-      assert !conn.read_primary?
-      assert !conn.primary?
+      assert !client.read_primary?
+      assert !client.primary?
     end
 
-    conn = make_connection(:secondary_preferred)
+    client = make_connection(:secondary_preferred)
     rescue_connection_failure do
-      assert !conn.read_primary?
-      assert !conn.primary?
+      assert !client.read_primary?
+      assert !client.primary?
     end
   end
 
   def test_connection_pools
-    conn = make_connection
-    assert conn.primary_pool, "No primary pool!"
-    assert conn.read_pool, "No read pool!"
-    assert conn.primary_pool.port == conn.read_pool.port,
+    client = make_connection
+    assert client.primary_pool, "No primary pool!"
+    assert client.read_pool, "No read pool!"
+    assert client.primary_pool.port == client.read_pool.port,
       "Primary port and read port are not the same!"
 
-    conn = make_connection(:primary_preferred)
-    assert conn.primary_pool, "No primary pool!"
-    assert conn.read_pool, "No read pool!"
-    assert conn.primary_pool.port == conn.read_pool.port,
+
+    client = make_connection(:primary_preferred)
+    assert client.primary_pool, "No primary pool!"
+    assert client.read_pool, "No read pool!"
+    assert client.primary_pool.port == client.read_pool.port,
       "Primary port and read port are not the same!"
 
-    conn = make_connection(:secondary)
-    assert conn.primary_pool, "No primary pool!"
-    assert conn.read_pool, "No read pool!"
-    assert conn.primary_pool.port != conn.read_pool.port,
+    client = make_connection(:secondary)
+    assert client.primary_pool, "No primary pool!"
+    assert client.read_pool, "No read pool!"
+    assert client.primary_pool.port != client.read_pool.port,
       "Primary port and read port are the same!"
 
-    conn = make_connection(:secondary_preferred)
-    assert conn.primary_pool, "No primary pool!"
-    assert conn.read_pool, "No read pool!"
-    assert conn.primary_pool.port != conn.read_pool.port,
+    client = make_connection(:secondary_preferred)
+    assert client.primary_pool, "No primary pool!"
+    assert client.read_pool, "No read pool!"
+    assert client.primary_pool.port != client.read_pool.port,
       "Primary port and read port are the same!"
   end
 
@@ -101,28 +98,12 @@ class ReadPreferenceTest < Test::Unit::TestCase
 
     # Kill the primary so only a single secondary exists
     @rs.primary.kill
-
+    sleep(2)
     # Test that reads are going to the right members
     assert_raise_error ConnectionFailure do
       @primary[TEST_DB]['test-sets'].find_one
     end
     assert_query_route(@primary_preferred, @secondary_direct)
-    assert_query_route(@secondary, @secondary_direct)
-    assert_query_route(@secondary_preferred, @secondary_direct)
-
-    # Restore set
-    @rs.restart
-    sleep(1)
-    @repl_cons.each { |con| con.refresh }
-    sleep(1)
-    @primary_direct = Connection.new(
-      @rs.config['host'],
-      @primary.read_pool.port
-    )
-
-    # Test that reads are going to the right members
-    assert_query_route(@primary, @primary_direct)
-    assert_query_route(@primary_preferred, @primary_direct)
     assert_query_route(@secondary, @secondary_direct)
     assert_query_route(@secondary_preferred, @secondary_direct)
   end
@@ -135,15 +116,15 @@ class ReadPreferenceTest < Test::Unit::TestCase
     assert_query_route(@primary_preferred, @primary_direct)
     assert_query_route(@secondary, @secondary_direct)
     assert_query_route(@secondary_preferred, @secondary_direct)
-
-    # Kill the secondary so that only primary exists
     @rs.secondaries.first.kill
 
-    # Test that reads are going to the right members
-    assert_query_route(@primary, @primary_direct)
     assert_query_route(@primary_preferred, @primary_direct)
-    assert_raise_error ConnectionFailure do
-      @secondary[TEST_DB]['test-sets'].find_one
+
+    primary_read_only = false
+    until primary_read_only
+      config = @primary_direct['admin'].command(:isMaster => 1)
+      primary_read_only = config["secondary"]
+      sleep(1)
     end
     assert_query_route(@secondary_preferred, @primary_direct)
 
@@ -152,29 +133,34 @@ class ReadPreferenceTest < Test::Unit::TestCase
     sleep(1)
     @repl_cons.each { |con| con.refresh }
     sleep(1)
-    @secondary_direct = Connection.new(
-      @rs.config['host'],
-      @secondary.read_pool.port,
-      :slave_ok => true
-    )
+
+    true_secondary = [@primary_direct, @secondary_direct].find do |client|
+      client['admin'].command(:isMaster => 1)['secondary']
+    end
+
+    true_primary = [@primary_direct, @secondary_direct].find do |client|
+      client['admin'].command(:isMaster => 1)['primary']
+    end
+
+    @secondary_direct, @primary_direct = true_secondary, true_primary
 
     # Test that reads are going to the right members
     assert_query_route(@primary, @primary_direct)
     assert_query_route(@primary_preferred, @primary_direct)
     assert_query_route(@secondary, @secondary_direct)
-    assert_query_route(@secondary_preferred, @secondary_direct)
+    assert_query_route(@secondary_preferred, @primary_direct) # primary pool is pinned
   end
 
   def test_write_lots_of_data
-    @conn = make_connection(:secondary_preferred)
-    @db = @conn[TEST_DB]
-    @coll = @db.collection("test-sets", {:w => 2})
+    client = make_connection(:secondary_preferred)
+    db = client[TEST_DB]
+    coll = db.collection("test-sets", {:w => 2})
 
     6000.times do |n|
-      @coll.save({:a => n})
+      coll.save({:a => n})
     end
 
-    cursor = @coll.find()
+    cursor = coll.find()
     cursor.next
     cursor.close
   end
@@ -190,9 +176,9 @@ class ReadPreferenceTest < Test::Unit::TestCase
     @repl_cons = [@primary, @primary_preferred, @secondary, @secondary_preferred]
 
     # Setup direct connections
-    @primary_direct = Connection.new(@rs.config['host'], @primary.read_pool.port)
+    @primary_direct = MongoClient.new(@rs.config['host'], @primary.read_pool.port, :slave_ok => true)
     authenticate_client(@primary_direct)
-    @secondary_direct = Connection.new(@rs.config['host'], @secondary.read_pool.port, :slave_ok => true)
+    @secondary_direct = MongoClient.new(@rs.config['host'], @secondary.read_pool.port, :slave_ok => true)
     authenticate_client(@secondary_direct)
   end
 
@@ -207,7 +193,6 @@ class ReadPreferenceTest < Test::Unit::TestCase
   end
 
   def assert_query_route(test_connection, expected_target)
-    #puts "#{test_connection.read_pool.port} #{expected_target.read_pool.port}"
     authenticate_client(test_connection)
     authenticate_client(expected_target)
     queries_before = query_count(expected_target)
