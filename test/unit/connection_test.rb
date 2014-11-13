029f6ea0a70e731e3aaf94ef49ff923ff48546e3
Durran Jordan <durran@gmail.com>
2014-11-13 16:38:02 +0100

    Use scram as default on wire version 3 or higher

--- test/unit/connection_test.rb
+++ test/unit/connection_test.rb
@@ -145,7 +145,7 @@ class ConnectionUnitTest < Test::Unit::TestCase
           :username  => 'hyphen-user_name',
           :password  => 'p-s_s',
           :source    => 'db',
-          :mechanism => Authentication::DEFAULT_MECHANISM
+          :mechanism => nil
         }
         assert_equal auth_hash, @connection.auths.first
       end
@@ -264,7 +264,7 @@ class ConnectionUnitTest < Test::Unit::TestCase
             :username  => 'hyphen-user_name',
             :password  => 'p-s_s',
             :source    => 'db',
-            :mechanism => Authentication::DEFAULT_MECHANISM
+            :mechanism => nil
           }
           assert_equal auth_hash, @connection.auths.first
         end
