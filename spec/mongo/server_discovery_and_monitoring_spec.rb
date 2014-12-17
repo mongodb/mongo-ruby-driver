740e74f5fea8763af12ee3de463cccaae4eeace2
Durran Jordan <durran@gmail.com>
2014-12-17 17:48:42 +0100

    RUBY-818: Add matchers for checking cluster and server types

--- spec/mongo/server_discovery_and_monitoring_spec.rb
+++ spec/mongo/server_discovery_and_monitoring_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
+# Matcher for determining if the server is of the expected type according to
+# the test.
+RSpec::Matchers.define :be_server_type do |expected|
+
+  match do |actual|
+    case expected
+      when 'Standalone' then actual.standalone?
+      when 'RSPrimary' then actual.primary?
+      when 'RSSecondary' then actual.secondary?
+      when 'RSArbiter' then actual.arbiter?
+      when 'Mongos' then actual.mongos?
+      when 'Unknown' then actual.ghost?
+    end
+  end
+end
+
+# Matcher for determining if the cluster topology is the expected type.
+RSpec::Matchers.define :be_topology do |expected|
+
+  match do |actual|
+    case expected
+      when 'ReplicaSetWithPrimary' then actual.replica_set?
+      when 'ReplicaSetNoPrimary' then actual.replica_set?
+      when 'Sharded' then actual.sharded?
+      when 'Single' then actual.standalone?
+      when 'Unknown' then actual.unknown?
+    end
+  end
+end
+
 describe 'Server Discovery and Monitoring' do
 
   SERVER_DISCOVERY_TESTS.each do |file|
