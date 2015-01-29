20a5ed9d366bb6cb226bb69f6366c16e14aba125
Durran Jordan <durran@gmail.com>
2015-01-29 10:46:41 +0100

    RUBY-846: No need to require weakref

--- lib/mongo/server/monitor.rb
+++ lib/mongo/server/monitor.rb
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'weakref'
 require 'mongo/server/monitor/connection'
 
 module Mongo
