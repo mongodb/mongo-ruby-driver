3f9e6bbd9a1cde2778ab95d713e6bdc9c071d205
Durran Jordan <durran@gmail.com>
2015-02-23 11:10:53 +0100

    Move travis helper into support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,22 +19,7 @@ end
 
 require 'mongo'
 
-# Give MongoDB time to start up on the travis ci environment.
-if (ENV['CI'] == 'travis')
-  starting = true
-  client = Mongo::Client.new(['127.0.0.1:27017'])
-  while starting
-    begin
-      client.command(Mongo::Server::Monitor::STATUS)
-      break
-    rescue Mongo::Error::OperationFailure => e
-      sleep(2)
-      client.cluster.scan!
-    end
-  end
-end
-
-require 'support/matchers'
+require 'support/travis'
 require 'support/authorization'
 require 'support/mongo_orchestration'
 require 'support/server_discovery_and_monitoring'
---
+++ spec/support/travis.rb
@@ -0,0 +1,14 @@
+# start up on the travis ci environment.
+if (ENV['CI'] == 'travis')
+  starting = true
+  client = Mongo::Client.new(['127.0.0.1:27017'])
+  while starting
+    begin
+      client.command(Mongo::Server::Monitor::STATUS)
+      break
+    rescue Mongo::Error::OperationFailure => e
+      sleep(2)
+      client.cluster.scan!
+    end
+  end
+end
