2d3787e3f3860f77f9a1395eb5872a54a1eba483
Emily Stolfo <emily@mongodb.com>
2015-02-27 12:45:32 +0100

    Get correct write concern

--- spec/support/authorization.rb
+++ spec/support/authorization.rb
@@ -95,7 +95,7 @@ TEST_READ_WRITE_USER = Mongo::Auth::User.new(
 # The write concern to use in the tests.
 #
 # @since 2.0.0
-WRITE_CONCERN = CONNECT == :direct || :sharded ? { w: 1 } : { w: 'majority' }
+WRITE_CONCERN = (CONNECT == :replica_set) ? { w: ADDRESSES.size } : { w: 1 }
 
 # Provides an authorized mongo client on the default test database for the
 # default test user.
