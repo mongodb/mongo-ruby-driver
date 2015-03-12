78166396a93e38b89098bee8778757b9ef5312c2
Emily Stolfo <emily@10gen.com>
2015-03-12 16:01:38 +0100

    Create example

---
+++ examples/create.rb
@@ -0,0 +1,30 @@
+# Insert a document
+require 'date'
+
+result = client[:restaurants].insert_one({
+    address: {
+              street: '2 Avenue',
+              zipcode: 10075,
+              building: 1480,
+              coord: [-73.9557413, 40.7720266]
+             },
+    borough: 'Manhattan',
+    cuisine: 'Italian',
+    grades: [
+             {
+               date: DateTime.strptime('2014-10-01', '%Y-%m-%d'),
+               grade: 'A',
+               score: 11
+             },
+             {
+               date: DateTime.strptime('2014-01-16', '%Y-%m-%d'),
+               grade: 'B',
+               score: 17
+             }
+            ],
+    name: 'Vella',
+    restaurant_id: '41704620'
+  })
+
+result.n #=> returns 1, because 1 document was inserted.
+
