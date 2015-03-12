5ba63b632f2070065ee7dee46858a8aca380df95
Emily Stolfo <emily@10gen.com>
2015-03-12 15:28:23 +0100

    Aggregation example

---
+++ examples/aggregate.rb
@@ -0,0 +1,25 @@
+# Group documents by field and calculate count.
+
+coll = client[:restaurants]
+
+results = coll.find.aggregate([ { '$group' => { '_id' => '$borough',
+                                                'count' => { '$sum' => 1 }
+                                              }
+                                }
+                              ])
+
+results.each do |result|
+  puts result
+end
+
+# Filter and group documents
+
+results = coll.find.aggregate([ { '$match' => { 'borough' => 'Queens',
+                                                'cuisine' => 'Brazilian' } },
+                                { '$group' => { '_id' => '$address.zipcode', 
+                                                'count' => { '$sum' => 1 } } }
+                              ])
+
+results.each do |result|
+  puts result
+end
