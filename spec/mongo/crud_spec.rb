e0edafd91c1dc9728264d1be9015c8abd5fcb0f5
Emily Stolfo <emily@mongodb.com>
2015-03-24 13:58:33 +0100

    Require >= 2.6 for some CRUD tests

--- spec/mongo/crud_spec.rb
+++ spec/mongo/crud_spec.rb
@@ -16,19 +16,23 @@ describe 'CRUD' do
             authorized_collection.find.delete_many
           end
 
-          let(:results) do
-            test.run(authorized_collection)
-          end
-
           after(:each) do
             authorized_collection.find.delete_many
           end
 
+          let(:results) do
+            test.run(authorized_collection)
+          end
+
           it "returns the correct result" do
-            expect(results).to match_results(test)
+            skip 'Test results only match with server version >= 2.6' if test.requires_2_6?(write_command_enabled?,
+                                                                                            authorized_collection)
+            expect(results).to eq(test.result)
           end
 
           it 'has the correct data in the collection' do
+            skip 'Test results only match with server version >= 2.6' if test.requires_2_6?(write_command_enabled?,
+                                                                                            authorized_collection)
             expect(test.run(authorized_collection)).to match_collection_data(test)
           end
         end
