f2cdfa4b5344b09c441eb3a8e288c2ea074df045
Durran Jordan <durran@gmail.com>
2015-04-20 09:19:23 +0200

    RUBY-885: Add DBRef Support

---
+++ spec/mongo/dbref_spec.rb
@@ -0,0 +1,100 @@
+require 'spec_helper'
+require 'json'
+
+describe Mongo::DBRef do
+
+  let(:object_id) do
+    BSON::ObjectId.new
+  end
+
+  describe '#as_json' do
+
+    context 'when the database is not provided' do
+
+      let(:dbref) do
+        described_class.new('users', object_id)
+      end
+
+      it 'returns the json document without database' do
+        expect(dbref.as_json).to eq({ '$ref' => 'users', '$id' => object_id })
+      end
+    end
+
+    context 'when the database is provided' do
+
+      let(:dbref) do
+        described_class.new('users', object_id, 'database')
+      end
+
+      it 'returns the json document with database' do
+        expect(dbref.as_json).to eq({
+          '$ref' => 'users',
+          '$id' => object_id,
+          '$db' => 'database'
+        })
+      end
+    end
+  end
+
+  describe '#initialize' do
+
+    let(:dbref) do
+      described_class.new('users', object_id)
+    end
+
+    it 'sets the collection' do
+      expect(dbref.collection).to eq('users')
+    end
+
+    it 'sets the id' do
+      expect(dbref.id).to eq(object_id)
+    end
+
+    context 'when a database is provided' do
+
+      let(:dbref) do
+        described_class.new('users', object_id, 'db')
+      end
+
+      it 'sets the database' do
+        expect(dbref.database).to eq('db')
+      end
+    end
+  end
+
+  describe '#to_bson' do
+
+    let(:dbref) do
+      described_class.new('users', object_id, 'database')
+    end
+
+    it 'converts the underlying document to bson' do
+      expect(dbref.to_bson).to eq(dbref.as_json.to_bson)
+    end
+  end
+
+  describe '#to_json' do
+
+    context 'when the database is not provided' do
+
+      let(:dbref) do
+        described_class.new('users', object_id)
+      end
+
+      it 'returns the json document without database' do
+        expect(dbref.to_json).to eq("{\"$ref\":\"users\",\"$id\":#{object_id.to_json}}")
+      end
+    end
+
+    context 'when the database is provided' do
+
+      let(:dbref) do
+        described_class.new('users', object_id, 'database')
+      end
+
+      it 'returns the json document with database' do
+        expect(dbref.to_json).to eq("{\"$ref\":\"users\",\"$id\":#{object_id.to_json},\"$db\":\"database\"}")
+      end
+    end
+  end
+end
