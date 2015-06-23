bc98ba95fb38af6ef4402a43e0256b9ef521c179
Emily Stolfo <emily@10gen.com>
2015-06-23 17:29:07 +0200

    RUBY-949 update topology specs

--- spec/mongo/cluster/topology_spec.rb
+++ spec/mongo/cluster/topology_spec.rb
@@ -68,18 +68,18 @@ describe Mongo::Cluster::Topology do
       context 'when provided a single mongos', if: single_mongos? do
 
         let(:topology) do
-          described_class.initial(ADDRESSES, {})
+          described_class.initial(ADDRESSES, TEST_OPTIONS)
         end
 
-        it 'returns a single topology' do
-          expect(topology).to be_a(Mongo::Cluster::Topology::Single)
+        it 'returns a sharded topology' do
+          expect(topology).to be_a(Mongo::Cluster::Topology::Sharded)
         end
       end
 
       context 'when provided a single replica set member', if: single_rs_member? do
 
         let(:topology) do
-          described_class.initial(ADDRESSES, {})
+          described_class.initial(ADDRESSES, TEST_OPTIONS)
         end
 
         it 'returns a single topology' do
