ae6705c0e590b3d7351a19a78c8d14187cebb161
Durran Jordan <durran@gmail.com>
2015-07-22 18:33:37 +0200

    INT-159: Writing spec for command monitoring and expecations

--- spec/mongo/command_monitoring_spec.rb
+++ spec/mongo/command_monitoring_spec.rb
@@ -10,8 +10,38 @@ describe 'Command Monitoring Events' do
 
       context(test.description) do
 
-        it '' do
-          p test.expectations
+        let(:subscriber) do
+          Mongo::CommandMonitoring::TestSubscriber.new
+        end
+
+        let(:monitoring) do
+          authorized_client.instance_variable_get(:@monitoring)
+        end
+
+        before do
+          authorized_collection.find.delete_many
+          authorized_client.subscribe(Mongo::Monitoring::COMMAND, subscriber)
+        end
+
+        after do
+          monitoring.subscribers[Mongo::Monitoring::COMMAND].delete(subscriber)
+          authorized_collection.find.delete_many
+        end
+
+        test.expectations.each do |expectation|
+
+          it "generates a #{expectation.event_name} for #{expectation.command_name}" do
+            begin
+              results = test.run(authorized_collection)
+              event = subscriber.send(expectation.event_type)[expectation.command_name]
+              expect(event).to_not be_nil
+              expect(event.command_name.to_s).to eq(expectation.command_name)
+              expect(event.database_name.to_s).to eq(expectation.database_name)
+              # expect(event.send(expectation.payload_name)).to
+              # include(expectation.data)
+            rescue Mongo::Error::OperationFailure
+            end
+          end
         end
       end
     end
--- spec/support/command_monitoring.rb
+++ spec/support/command_monitoring.rb
@@ -29,13 +29,61 @@ module Mongo
         @operation = Mongo::CRUD::Operation.get(test['operation'])
         @expectations = test['expectations'].map{ |e| Expectation.new(e) }
       end
+
+      def run(collection)
+        collection.insert_many(@data)
+        @operation.execute(collection)
+      end
     end
 
     class Expectation
 
+      attr_reader :event_type
+
+      def command_name
+        @event_data['command_name']
+      end
+
+      def database_name
+        @event_data['database_name']
+      end
+
+      def event_name
+        event_type.gsub('_', ' ')
+      end
+
       def initialize(expectation)
         @event_type = expectation.keys.first
         @event_data = expectation[@event_type]
+      end
+    end
+
+    class TestSubscriber
+
+      def started(event)
+        command_started_event[event.command_name] = event
+      end
+
+      def succeeded(event)
+        command_succeeded_event[event.command_name] = event
+      end
+
+      def failed(event)
+        command_failed_event[event.command_name] = event
+      end
+
+      private
+
+      def command_started_event
+        @started_events ||= BSON::Document.new
+      end
+
+      def command_succeeded_event
+        @succeeded_events ||= BSON::Document.new
+      end
+
+      def command_failed_event
+        @failed_events ||= BSON::Document.new
       end
     end
   end
