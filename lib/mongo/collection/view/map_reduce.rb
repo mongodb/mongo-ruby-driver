e313940938e217b3e55e0f313db39af1dba6eef6
Emily Stolfo <emily@10gen.com>
2015-08-22 23:29:14 +0200

    RUBY-1006 Account for the view setting sort in the modifiers document when doing mp

--- lib/mongo/collection/view/map_reduce.rb
+++ lib/mongo/collection/view/map_reduce.rb
@@ -161,8 +161,12 @@ module Mongo
               :reduce => reduce,
               :query => view.modifiers[:$query] || view.selector,
               :out => { inline: 1 }
-            }.merge(options).merge(view.options)
+            }.merge(options).merge(view_options)
           }
+        end
+
+        def view_options
+          view.sort ? view.options.merge(:sort => view.sort) : view.options
         end
 
         def new(options)
