8bd98eefeaad3ab277edc48ed6e0256fb1e661c1
Ralf Kistner <ralf@journeyapps.com>
2015-08-28 17:01:50 +0200

    Add failing specs for SSL socket #alive?

--- spec/mongo/socket/ssl_spec.rb
+++ spec/mongo/socket/ssl_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe Mongo::Socket::SSL do
+  let(:socket) do
+    described_class.new(*DEFAULT_ADDRESS.split(":"), DEFAULT_ADDRESS.split(":")[0], 5, Socket::PF_INET, options)
+  end
 
   describe '#connect!', if: running_ssl? do
-
-    let(:socket) do
-      described_class.new(*DEFAULT_ADDRESS.split(":"), DEFAULT_ADDRESS.split(":")[0], 5, Socket::PF_INET, options)
-    end
 
     context 'when a certificate is provided' do
 
@@ -149,5 +148,77 @@ describe Mongo::Socket::SSL do
         expect(socket).to be_alive
       end
     end
+
   end
+
+
+  describe '#alive?', if: running_ssl? do
+
+    context 'when connected' do
+
+      let(:options) do
+        {
+            :ssl => true,
+            :ssl_cert => CLIENT_PEM,
+            :ssl_key => CLIENT_PEM,
+            :ssl_verify => false
+        }
+      end
+
+      before do
+        socket.connect!
+      end
+
+      it 'is alive' do
+        expect(socket).to be_alive
+      end
+    end
+
+    context 'when not connected' do
+
+      let(:options) do
+        {
+            :ssl => true,
+            :ssl_cert => CLIENT_PEM,
+            :ssl_key => CLIENT_PEM,
+            :ssl_verify => false
+        }
+      end
+
+      before do
+        # Don't connect. Same behaviour as a timeout.
+        # Alternatively, mock the Timeout call to immediate timeout before
+        # being able to connect?
+      end
+
+      it 'is not alive' do
+        expect(socket).to_not be_alive
+      end
+    end
+
+
+    context 'when disconnected' do
+
+      let(:options) do
+        {
+            :ssl => true,
+            :ssl_cert => CLIENT_PEM,
+            :ssl_key => CLIENT_PEM,
+            :ssl_verify => false
+        }
+      end
+
+      before do
+        socket.connect!
+        socket.close
+      end
+
+      it 'is not alive' do
+        expect(socket).to_not be_alive
+      end
+    end
+
+
+  end
+
 end
