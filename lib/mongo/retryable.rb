2862170460a3740d6613d24afd04393736286518
Jonathan Hyman <jonathan.hyman@post.harvard.edu>
2015-10-06 10:14:56 -0400

    RUBY-1049: reauthenticate and retry when receiving a 'not authorized' or 'unauthorized' response from MongoDB.

--- lib/mongo/retryable.rb
+++ lib/mongo/retryable.rb
@@ -47,7 +47,10 @@ module Mongo
       rescue Error::SocketError, Error::SocketTimeoutError
         retry_operation(&block)
       rescue Error::OperationFailure => e
-        if cluster.sharded? && e.retryable?
+        if cluster.sharded? && (e.retryable? || e.unauthorized?)
+          if e.unauthorized?
+            cluster.servers.each {|server| server.context.with_connection {|conn| conn.authenticate! } }
+          end
           if attempt < cluster.max_read_retries
             # We don't scan the cluster in this case as Mongos always returns
             # ready after a ping no matter what the state behind it is.
