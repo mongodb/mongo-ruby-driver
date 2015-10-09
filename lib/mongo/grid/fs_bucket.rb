b526c04b843800d79d1a68aef5a17ec0c2712f40
Emily Stolfo <emily@10gen.com>
2015-10-09 11:39:15 -0400

    Must find one on files collection to comply with GridFS spec

--- lib/mongo/grid/fs_bucket.rb
+++ lib/mongo/grid/fs_bucket.rb
@@ -438,7 +438,7 @@ module Mongo
       end
 
       def ensure_indexes!
-        if files_collection.find({}).count == 0
+        if files_collection.find({}, limit: -1).to_a.empty?
           chunks_collection.indexes.create_one(FSBucket::CHUNKS_INDEX, :unique => true)
           files_collection.indexes.create_one(FSBucket::FILES_INDEX)
         end
