f9c1a61342bd00d1b5b79ef7b4eff9e315284bd2
Durran Jordan <durran@gmail.com>
2015-10-28 16:13:18 +0100

    RUBY-1019: Add small/large doc bench

--- profile/benchmark_21.rb
+++ profile/benchmark_21.rb
@@ -13,14 +13,35 @@ client = Mongo::Client.new(
 
 collection = client[:test]
 
-documents = 50000.times.map do |i|
-  { name: 'user', index: i, embedded: [{ n: i }] }
-end
-
 Benchmark.bm do |bm|
 
+  small_documents = 50000.times.map do |i|
+    { name: 'user', index: i, embedded: [{ n: i }] }
+  end
+
   bm.report('Mongo::Collection#insert_many') do
-    collection.insert_many(documents)
+    collection.insert_many(small_documents)
+  end
+
+  bm.report('Mongo::Cursor#each') do
+    collection.find.each do |document|
+    end
+  end
+
+  bm.report('Mongo::Collection::View#update_many') do
+    collection.find(name: 'user').update_many({ '$set' => { name: 'user_modified' }})
+  end
+
+  bm.report('Mongo::Collection::View#delete_many') do
+    collection.find(name: 'user_modified').delete_many
+  end
+
+  large_documents = 50000.times.map do |i|
+    { name: 'user', index: i, embedded: 100.times.map{ |x| { n: x } }}
+  end
+
+  bm.report('Mongo::Collection#insert_many') do
+    collection.insert_many(large_documents)
   end
 
   bm.report('Mongo::Cursor#each') do
