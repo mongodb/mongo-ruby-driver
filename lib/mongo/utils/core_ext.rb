aca663b24573d896591f79f5ce268a9be5890a43
Marion Duprey <marion.duprey.pro@gmail.com>
2015-10-28 11:18:27 +0100

    fix rails compat on Hash

--- lib/mongo/utils/core_ext.rb
+++ lib/mongo/utils/core_ext.rb
@@ -30,7 +30,7 @@ class Hash
   def assert_valid_keys(*valid_keys)
     unknown_keys = keys - [valid_keys].flatten
     raise(ArgumentError, "Unknown key(s): #{unknown_keys.join(", ")}") unless unknown_keys.empty?
-  end
+  end unless instance_methods.include?(:assert_valid_keys)
 
 end
 
