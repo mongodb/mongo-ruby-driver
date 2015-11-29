14f9b14b337e3da7b36a566132d2f716c3540560
Durran Jordan <durran@gmail.com>
2015-11-29 11:07:27 +0100

    Add rdoc

--- lib/mongo/address.rb
+++ lib/mongo/address.rb
@@ -63,6 +63,16 @@ module Mongo
       host == other.host && port == other.port
     end
 
+    # Check equality for hashing.
+    #
+    # @example Check hashing equality.
+    #   address.eql?(other)
+    #
+    # @param [ Object ] other The other object.
+    #
+    # @return [ true, false ] If the objects are equal.
+    #
+    # @since 2.2.0
     def eql?(other)
       self == other
     end
