b783b82a889dd2f9aa2f6577dbf8c10dd690c024
Emily <emily@mongodb.com>
2016-09-06 12:59:37 +0200

    RUBY-1126 Update combiner intialize method in spec

--- spec/mongo/bulk_write/ordered_combiner_spec.rb
+++ spec/mongo/bulk_write/ordered_combiner_spec.rb
@@ -5,7 +5,7 @@ describe Mongo::BulkWrite::OrderedCombiner do
   describe '#combine' do
 
     let(:combiner) do
-      described_class.new(requests, double('server'))
+      described_class.new(requests)
     end
 
     context 'when provided a series of delete one' do
