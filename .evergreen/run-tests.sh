27a2f7216102196c1521ad2d9f5be179f7a5fef8
Emily <emily@mongodb.com>
2017-05-03 17:04:19 -0400

    Update evergreen testing for ruby toolchain

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -35,7 +35,6 @@ export DRIVER_TOOLS_CA_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/ca.pem"
 export DRIVER_TOOLS_CLIENT_KEY_ENCRYPTED_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/password_protected.pem"
 
 source ~/.rvm/scripts/rvm
-rvm install $RVM_RUBY
 rvm use $RVM_RUBY
 gem install bundler
 bundle install
