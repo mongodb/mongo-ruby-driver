03a8f15ee50dccf87cf0c0563d4c199b80c18f6f
Emily <emily@mongodb.com>
2018-02-13 13:02:50 +0100

    Use rspec around to filter out CRUD spec tests

--- spec/mongo/crud_spec.rb
+++ spec/mongo/crud_spec.rb
@@ -6,34 +6,37 @@ describe 'CRUD' do
 
     spec = Mongo::CRUD::Spec.new(file)
 
-    if spec.server_version_satisfied?(AUTHORIZED_CLIENT)
+    context(spec.description) do
 
-      context(spec.description) do
+      spec.tests.each do |test|
 
-        spec.tests.each do |test|
+        around do |example|
+          if spec.server_version_satisfied?(authorized_client)
+            example.run
+          end
+        end
 
-          context(test.description) do
+        context(test.description) do
 
-            before(:each) do
-              test.setup_test(authorized_collection)
-            end
+          before(:each) do
+            test.setup_test(authorized_collection)
+          end
 
-            after(:each) do
-              authorized_collection.delete_many
-            end
+          after(:each) do
+            authorized_collection.delete_many
+          end
 
-            let(:results) do
-              test.run(authorized_collection)
-            end
+          let(:results) do
+            test.run(authorized_collection)
+          end
 
-            it 'returns the correct result' do
-              expect(results).to match_operation_result(test)
-            end
+          it 'returns the correct result' do
+            expect(results).to match_operation_result(test)
+          end
 
-            it 'has the correct data in the collection', if: test.outcome_collection_data do
-              results
-              expect(authorized_collection.find.to_a).to match_collection_data(test)
-            end
+          it 'has the correct data in the collection', if: test.outcome_collection_data do
+            results
+            expect(authorized_collection.find.to_a).to match_collection_data(test)
           end
         end
       end
