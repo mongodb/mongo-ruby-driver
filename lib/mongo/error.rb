f4e907865fba2696c7a5480a5dfa40410998c3e2
Emily <emily@mongodb.com>
2018-02-13 18:04:37 +0100

    RUBY-1284 Require forwardable in Mongo::Error

--- lib/mongo/error.rb
+++ lib/mongo/error.rb
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'forwardable'
+
 module Mongo
   # Base error class for all Mongo related errors.
   #
