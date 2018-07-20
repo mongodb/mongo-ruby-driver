c58d529c3ae0805128cda39dc26e234ecef809fd
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-20 19:10:47 -0400

    Test monitor thread resurrection

--- spec/integration/reconnect_spec.rb
+++ spec/integration/reconnect_spec.rb
@@ -12,4 +12,20 @@ describe 'Client after reconnect' do
     expect(doc).not_to be_nil
     expect(doc['testk']).to eq('testv')
   end
+
+  it 'recreates monitor thread' do
+    thread = client.cluster.servers.first.monitor.instance_variable_get('@thread')
+    expect(thread).to be_alive
+
+    thread.kill
+    # context switch to let the thread get killed
+    sleep 0.1
+    expect(thread).not_to be_alive
+
+    client.reconnect
+
+    new_thread = client.cluster.servers.first.monitor.instance_variable_get('@thread')
+    expect(new_thread).not_to eq(thread)
+    expect(new_thread).to be_alive
+  end
 end
