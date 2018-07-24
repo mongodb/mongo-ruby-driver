3d8acf0347aeb0d7961e4aa9902ac6bc57b3df30
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-24 13:35:42 -0400

    RUBY-1403 Build against ruby-head in Evergreen

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -34,33 +34,44 @@ export DRIVER_TOOLS_CA_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/ca.pem"
 export DRIVER_TOOLS_CLIENT_KEY_ENCRYPTED_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/password_protected.pem"
 
 
-source ~/.rvm/scripts/rvm
-
 # Necessary for jruby
 export JAVACMD=/opt/java/jdk8/bin/java
 export PATH=$PATH:/opt/java/jdk8/bin
 
 if [ "$RVM_RUBY" == "ruby-head" ]; then
-  rvm reinstall $RVM_RUBY
-fi
+  # 12.04, 14.04 and 16.04 are good
+  wget -O ruby-head.tar.bz2 http://rubies.travis-ci.org/ubuntu/`lsb_release -rs`/x86_64/ruby-head.tar.bz2
+  tar xf ruby-head.tar.bz2
+  export PATH=`pwd`/ruby-head/bin:`pwd`/ruby-head/lib/ruby/gems/2.6.0/bin:$PATH
+  ruby --version
+  ruby --version |grep dev
+  
+  #rvm reinstall $RVM_RUBY
+else
+  . ~/.rvm/scripts/rvm
 
-# Don't errexit because this may call scripts which error
-set +o errexit
-rvm use $RVM_RUBY
-set -o errexit
+  # Don't errexit because this may call scripts which error
+  set +o errexit
+  rvm use $RVM_RUBY
+  set -o errexit
 
-# Ensure we're using the right ruby
-python - <<EOH
+  # Ensure we're using the right ruby
+  python - <<EOH
 ruby = "${RVM_RUBY}".split("-")[0]
 version = "${RVM_RUBY}".split("-")[1]
 assert(ruby in "`ruby --version`")
 assert(version in "`ruby --version`")
 EOH
+fi
 
-echo 'updating rubygems'
-gem update --system
+if [ "$RVM_RUBY" != "ruby-head" ]; then
+  echo 'updating rubygems'
+  gem update --system
 
-gem install bundler
+  # ruby-head comes with bundler and gem complains
+  # because installing bundler would overwrite the bundler binary
+  gem install bundler
+fi
 
 echo "Installing all gem dependencies"
 bundle install
