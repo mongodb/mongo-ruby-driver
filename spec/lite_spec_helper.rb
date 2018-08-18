0e6fa3df9048f171e9f23cdba4e2f446ac622c9a
Oleg Pudeyev <oleg@bsdpower.com>
2018-08-17 22:03:58 -0400

    Comment

--- spec/lite_spec_helper.rb
+++ spec/lite_spec_helper.rb
@@ -79,7 +79,9 @@ RSpec.configure do |config|
   config.extend(LiteConstraints)
 
   unless SpecConfig.instance.client_debug?
-    # allow a max of 10 seconds per test
+    # Allow a max of 30 seconds per test.
+    # Tests should take under 10 seconds ideally but it seems
+    # we have some that run for more than 10 seconds in CI.
     config.around(:each) do |example|
       TimeoutInterrupt.timeout(30) do
         example.run
