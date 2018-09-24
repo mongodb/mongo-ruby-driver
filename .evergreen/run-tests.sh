28fec0c0538deb07a9e87d894b3ac509a05281c5
Oleg Pudeyev <oleg@bsdpower.com>
2018-09-24 16:12:04 -0400

    Update evergreen config to master

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -13,80 +13,25 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #                               For example: "ruby-2.3" or "jruby-9.1"
 #       DRIVER_TOOLS            Path to driver tools.
 
-AUTH=${AUTH:-noauth}
-SSL=${SSL:-nossl}
-MONGODB_URI=${MONGODB_URI:-}
-TOPOLOGY=${TOPOLOGY:-server}
-DRIVERS_TOOLS=${DRIVERS_TOOLS:-}
+. `dirname "$0"`/functions.sh
 
+set_env_vars
 
-if [ "$AUTH" != "noauth" ]; then
-  export ROOT_USER_NAME="bob"
-  export ROOT_USER_PWD="pwd123"
-fi
-if [ "$COMPRESSOR" == "zlib" ]; then
-  export COMPRESSOR="zlib"
-fi
-export CI=true
 export DRIVER_TOOLS_CLIENT_CERT_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/client-public.pem"
 export DRIVER_TOOLS_CLIENT_KEY_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/client-private.pem"
 export DRIVER_TOOLS_CA_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/ca.pem"
 export DRIVER_TOOLS_CLIENT_KEY_ENCRYPTED_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/password_protected.pem"
 
+setup_ruby
 
-# Necessary for jruby
-export JAVACMD=/opt/java/jdk8/bin/java
-export PATH=$PATH:/opt/java/jdk8/bin
-
-if [ "$RVM_RUBY" == "ruby-head" ]; then
-  # 12.04, 14.04 and 16.04 are good
-  wget -O ruby-head.tar.bz2 http://rubies.travis-ci.org/ubuntu/`lsb_release -rs`/x86_64/ruby-head.tar.bz2
-  tar xf ruby-head.tar.bz2
-  export PATH=`pwd`/ruby-head/bin:`pwd`/ruby-head/lib/ruby/gems/2.6.0/bin:$PATH
-  ruby --version
-  ruby --version |grep dev
-  
-  #rvm reinstall $RVM_RUBY
-else
-  . ~/.rvm/scripts/rvm
-
-  # Don't errexit because this may call scripts which error
-  set +o errexit
-  rvm use $RVM_RUBY
-  set -o errexit
-
-  # Ensure we're using the right ruby
-  python - <<EOH
-ruby = "${RVM_RUBY}".split("-")[0]
-version = "${RVM_RUBY}".split("-")[1]
-assert(ruby in "`ruby --version`")
-assert(version in "`ruby --version`")
-EOH
-fi
-
-if [ "$RVM_RUBY" != "ruby-head" ]; then
-  echo 'updating rubygems'
-  gem update --system
-
-  # ruby-head comes with bundler and gem complains
-  # because installing bundler would overwrite the bundler binary
-  gem install bundler
-fi
-
-echo "Installing all gem dependencies"
-bundle install
-bundle exec rake clean
+install_deps
 
 echo "Running specs"
-bundle exec rake spec
+bundle exec rake spec:ci
 test_status=$?
 echo "TEST STATUS"
 echo ${test_status}
 
-jruby_running=`ps -ef | grep 'jruby' | grep -v grep | awk '{print $2}'`
-if [ -n "$jruby_running" ];then
-  echo "terminating remaining jruby processes"
-  for pid in $(ps -ef | grep "jruby" | grep -v grep | awk '{print $2}'); do kill -9 $pid; done
-fi
+kill_jruby
 
 exit ${test_status}
