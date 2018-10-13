536b82c6565490917a59fc019e98aad7e6ba35c0
Oleg Pudeyev <oleg@bsdpower.com>
2018-10-12 22:03:57 -0400

    Do not heartbeat when monitoring is off

--- lib/mongo/monitoring.rb
+++ lib/mongo/monitoring.rb
@@ -205,6 +205,7 @@ module Mongo
     #
     # @since 2.1.0
     def initialize(options = {})
+      @options = options
       if options[:monitoring] != false
         Global.subscribers.each do |topic, subscribers|
           subscribers.each do |subscriber|
@@ -219,6 +220,14 @@ module Mongo
         subscribe(TOPOLOGY_CHANGED, TopologyChangedLogSubscriber.new(options))
         subscribe(TOPOLOGY_CLOSED, TopologyClosedLogSubscriber.new(options))
       end
+    end
+
+    # @api private
+    attr_reader :options
+
+    # @api private
+    def monitoring?
+      options[:monitoring] != false
     end
 
     # Publish a started event.
--- spec/integration/heartbeat_events_spec.rb
+++ spec/integration/heartbeat_events_spec.rb
@@ -82,4 +82,35 @@ describe 'Heartbeat events' do
     expect(failed_event.address).to be_a(Mongo::Address)
     expect(failed_event.address.seed).to eq(SpecConfig.instance.addresses.first)
   end
+
+  context 'when monitoring option is false' do
+    let(:client) { new_local_client([SpecConfig.instance.addresses.first],
+      authorized_client.options.merge(server_selection_timeout: 0.1, connect: :direct,
+        monitoring: false)) }
+
+    shared_examples_for 'does not notify on heartbeats' do
+      it 'does not notify on heartbeats' do
+        client.database.command(ismaster: 1)
+
+        started_event = subscriber.started_events.first
+        expect(started_event).to be nil
+      end
+    end
+
+    it_behaves_like 'does not notify on heartbeats'
+
+    context 'when a subscriber is added manually' do
+      let(:client) do
+        sdam_proc = Proc.new do |client|
+          client.subscribe(Mongo::Monitoring::SERVER_HEARTBEAT, subscriber)
+        end
+
+        new_local_client([SpecConfig.instance.addresses.first],
+          authorized_client.options.merge(server_selection_timeout: 0.1, connect: :direct,
+            monitoring: false, sdam_proc: sdam_proc))
+      end
+
+      it_behaves_like 'does not notify on heartbeats'
+    end
+  end
 end
