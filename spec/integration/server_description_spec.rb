1e00a98b7588d183750924829d6e41c3b1e28dc8
Oleg Pudeyev <oleg@bsdpower.com>
2018-11-06 16:55:51 -0500

    1566

---
+++ spec/integration/server_description_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'Server description' do
+  describe '#op_time' do
+    require_topology :replica_set
+    min_server_version '3.4'
+
+    let(:client) { ClientRegistry.instance.global_client('authorized') }
+    let(:desc) { client.cluster.servers.first.description }
+
+    it 'is set' do
+      client.database.command(ismaster: 1)
+
+      expect(desc.op_time).to be_a(BSON::Timestamp)
+    end
+  end
+end
