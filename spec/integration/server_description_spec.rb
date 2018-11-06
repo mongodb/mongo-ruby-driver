b37c2af3ea01c3a24267dae9a18e05799c95bcab
Oleg Pudeyev <oleg@bsdpower.com>
2018-11-06 16:57:26 -0500

    for good measure

--- spec/integration/server_description_spec.rb
+++ spec/integration/server_description_spec.rb
@@ -14,4 +14,18 @@ describe 'Server description' do
       expect(desc.op_time).to be_a(BSON::Timestamp)
     end
   end
+
+  describe '#last_write_date' do
+    require_topology :replica_set
+    min_server_version '3.4'
+
+    let(:client) { ClientRegistry.instance.global_client('authorized') }
+    let(:desc) { client.cluster.servers.first.description }
+
+    it 'is set' do
+      client.database.command(ismaster: 1)
+
+      expect(desc.last_write_date).to be_a(Time)
+    end
+  end
 end
