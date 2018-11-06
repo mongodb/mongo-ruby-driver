9e84df6530586fec255a0df15a9e9fe012c834ae
Oleg Pudeyev <oleg@bsdpower.com>
2018-11-06 17:01:55 -0500

    1567

--- lib/mongo/server/description.rb
+++ lib/mongo/server/description.rb
@@ -193,6 +193,7 @@ module Mongo
         @config = config
         @features = Features.new(wire_versions, me || @address.to_s)
         @average_round_trip_time = average_round_trip_time
+        @last_update_time = Time.now.freeze
       end
 
       # @return [ Address ] address The server's address.
@@ -651,6 +652,13 @@ module Mongo
           config['lastWrite']['opTime']['ts']
         end
       end
+
+      # Time when the server was last checked.
+      #
+      # @return [ Time ] Last check time.
+      #
+      # @since 2.7.0
+      attr_reader :last_update_time
 
       # Check equality of two descriptions.
       #
