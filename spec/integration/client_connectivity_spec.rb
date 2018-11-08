c98e7291c3c6fea50d4dbccc6064d57e35df402e
Oleg Pudeyev <oleg@bsdpower.com>
2018-11-07 23:11:59 -0500

    more tests

--- spec/integration/client_connectivity_spec.rb
+++ spec/integration/client_connectivity_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
 describe 'Client connectivity' do
+  shared_examples_for 'is correctly configured' do
+    it 'is configured with the correct database' do
+      expect(client.options[:database]).to eq(SpecConfig.instance.test_db)
+    end
+
+    it 'has correct database in the cluster' do
+      expect(client.cluster.options[:database]).to eq(SpecConfig.instance.test_db)
+    end
+  end
+
   context 'no auth' do
     let(:client) { ClientRegistry.instance.global_client('basic') }
+
+    it_behaves_like 'is correctly configured'
 
     it 'connects and is usable' do
       resp = client.database.command(ismaster: 1)
@@ -12,6 +24,8 @@ describe 'Client connectivity' do
 
   context 'with auth' do
     let(:client) { ClientRegistry.instance.global_client('authorized') }
+
+    it_behaves_like 'is correctly configured'
 
     it 'connects and is usable' do
       client['connectivity_spec'].insert_one(foo: 1)
