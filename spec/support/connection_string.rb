849a3e6e8b71c0fe4dcdc8552ad46cba0213a6b1
Oleg Pudeyev <oleg@bsdpower.com>
2018-11-10 19:37:29 -0500

    Omit monitoring io when creating clients for connection string spec tests.
    This speeds up the test suite by about 20 seconds.

--- spec/support/connection_string.rb
+++ spec/support/connection_string.rb
@@ -152,7 +152,7 @@ module Mongo
       end
 
       def client
-        @client ||= Mongo::Client.new(@spec['uri'])
+        @client ||= Mongo::Client.new(@spec['uri'], monitoring_io: false)
       end
 
       def uri
