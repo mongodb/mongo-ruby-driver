e27a1ba19e158f1b99317434057986d214cef30e
Saghm Rossi <saghmrossi@gmail.com>
2019-01-04 15:18:30 -0500

    minor: update copyright year (#1199)

--- lib/mongo/operation/collections_info/result.rb
+++ lib/mongo/operation/collections_info/result.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2014-2018MongoDB, Inc.
+# Copyright (C) 2014-2019MongoDB, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
