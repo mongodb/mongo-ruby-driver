958ba33e4c00932f1b4b7d65f242335d016719a5
Oleg Pudeyev <oleg@bsdpower.com>
2019-01-07 12:41:31 -0500

    extract

--- spec/support/crud.rb
+++ spec/support/crud.rb
@@ -14,6 +14,7 @@
 
 require 'support/crud/read'
 require 'support/crud/write'
+require 'support/crud/verifier'
 
 module Mongo
   module CRUD
@@ -199,88 +200,6 @@ module Mongo
         if @outcome['collection']
           collection_name = @outcome['collection']['name'] || @collection.name
           @collection.database[collection_name].find.to_a
-        end
-      end
-    end
-
-    class Verifier
-      include RSpec::Matchers
-
-      def initialize(test_instance)
-        @test_instance = test_instance
-      end
-
-      attr_reader :test_instance
-
-      # Compare the existing collection data and the expected collection data.
-      #
-      # Uses RSpec matchers and raises expectation failures if there is a
-      # mismatch.
-      def verify_collection_data(actual_collection_data)
-        expected_collection_data = test_instance.outcome_collection_data
-        if expected_collection_data.nil?
-          expect(actual_collection_data).to be nil
-        elsif expected_collection_data.empty?
-          expect(actual_collection_data).to be_empty
-        else
-          expect(actual_collection_data).not_to be nil
-          expected_collection_data.each do |doc|
-            expect(actual_collection_data).to include(doc)
-          end
-          actual_collection_data.each do |doc|
-            expect(expected_collection_data).to include(doc)
-          end
-        end
-      end
-
-      # Compare the actual operation result to the expected operation result.
-      #
-      # Uses RSpec matchers and raises expectation failures if there is a
-      # mismatch.
-      def verify_operation_result(actual)
-        expected = test_instance.outcome['result']
-        if expected.is_a?(Array)
-          if expected.empty?
-            expect(actual).to be_empty
-          else
-            expected.each_with_index do |expected_elt, i|
-              verify_result(expected_elt, actual[i])
-            end
-          end
-        else
-          verify_result(expected, actual)
-        end
-      end
-
-      private
-
-      def verify_result(expected, actual)
-        case expected
-        when nil
-          expect(actual).to be nil
-        when Hash
-          results = actual.instance_variable_get(:@results)
-          (results || actual).each do |k, v|
-            ok = expected[k] == v || handle_upserted_id(k, expected[k], v) || handle_inserted_ids(k, expected[k], v)
-            expect(ok).to be true
-          end
-        when Integer
-          expect(actual).to eq(expected)
-        end
-      end
-
-      def handle_upserted_id(field, expected_id, actual_id)
-        return true if expected_id.nil?
-        if field == 'upsertedId'
-          if expected_id.is_a?(Integer)
-            actual_id.is_a?(BSON::ObjectId) || actual_id.nil?
-          end
-        end
-      end
-
-      def handle_inserted_ids(field, expected, actual)
-        if field == 'insertedIds'
-          expected.values == actual
         end
       end
     end
