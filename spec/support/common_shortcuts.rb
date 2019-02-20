9dd599711a2c830f6428f4ff037007f3234c3e3d
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2019-02-20 14:34:46 -0500

    Fix RUBY-1719 Unknown servers are not marked as such in summaries (#1248)

--- spec/support/common_shortcuts.rb
+++ spec/support/common_shortcuts.rb
@@ -28,5 +28,38 @@ module CommonShortcuts
         sleep 0.25
       end
     end
+
+    def make_server(mode, options = {})
+      tags = options[:tags] || {}
+      average_round_trip_time = options[:average_round_trip_time] || 0
+
+      ismaster = {
+        'ismaster' => mode == :primary,
+        'secondary' => mode == :secondary,
+        'arbiterOnly' => mode == :arbiter,
+        'isreplicaset' => mode == :ghost,
+        'hidden' => mode == :other,
+        'tags' => tags,
+        'ok' => 1,
+        'minWireVersion' => 2, 'maxWireVersion' => 8,
+      }
+      if [:primary, :secondary, :arbiter, :other].include?(mode)
+        ismaster['setName'] = 'mongodb_set'
+      end
+
+      listeners = Mongo::Event::Listeners.new
+      monitoring = Mongo::Monitoring.new
+      address = options[:address]
+
+      cluster = double('cluster')
+      allow(cluster).to receive(:topology).and_return(topology)
+      allow(cluster).to receive(:app_metadata)
+      allow(cluster).to receive(:options).and_return({})
+      server = Mongo::Server.new(address, cluster, monitoring, listeners, SpecConfig.instance.test_options)
+      description = Mongo::Server::Description.new(address, ismaster, average_round_trip_time)
+      server.tap do |s|
+        allow(s).to receive(:description).and_return(description)
+      end
+    end
   end
 end
