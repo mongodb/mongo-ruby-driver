f243750acafd879ae159a5437f7d0a4cc33361f0
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2019-02-21 13:00:33 -0500

    Fix RUBY-1731 Allowed auth mechanisms are outdated and incomplete (#1264)

---
+++ spec/mongo/auth/invalid_mechanism_spec.rb
@@ -0,0 +1,11 @@
+require 'lite_spec_helper'
+
+describe Mongo::Auth::InvalidMechanism do
+  describe 'message' do
+    let(:exception) { described_class.new(:foo) }
+
+    it 'includes all built in mechanisms' do
+      expect(exception.message).to eq(':foo is invalid, please use one of the following mechanisms: :mongodb_cr, :mongodb_x509, :plain, :scram, :scram256')
+    end
+  end
+end
