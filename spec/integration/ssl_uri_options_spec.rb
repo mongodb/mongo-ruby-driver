584ba3869e6288aa5b7835bd2281bb10fd692f78
Saghm Rossi <saghmrossi@gmail.com>
2019-04-16 18:15:08 -0400

    RUBY-1767 set client key from tlsCertificateKeyFile (#1333)

---
+++ spec/integration/ssl_uri_options_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'SSL connections with URI options' do
+  # SpecConfig currently creates clients exclusively through non-URI options. Because we don't
+  # currently have a way to create what the URI would look like for a given client, it's simpler
+  # just to test the that TLS works when configured from a URI on a standalone server without auth
+  # required, since that allows us to build the URI more easily.
+  require_no_auth
+  require_topology :single
+  require_ssl
+
+  let(:hosts) do
+    SpecConfig.instance.addresses.join(',')
+  end
+
+  let(:uri) do
+    "mongodb://#{hosts}/?tls=true&tlsInsecure=true&tlsCertificateKeyFile=#{SpecConfig.instance.client_cert_key_pem}"
+  end
+
+  it 'successfully connects and runs an operation' do
+    client = Mongo::Client.new(uri)
+    expect { client[:foo].count_documents({}) }.not_to raise_error
+  end
+end
