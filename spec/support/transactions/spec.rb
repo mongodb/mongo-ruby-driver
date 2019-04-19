867b054577fe924ca81d428356b46ec38830e1b5
Oleg Pudeyev <oleg@bsdpower.com>
2019-04-19 16:46:53 -0400

    do this

---
+++ spec/support/transactions/spec.rb
@@ -0,0 +1,28 @@
+# Copyright (C) 2014-2019 MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module Mongo
+  module Transactions
+
+    class Spec < Mongo::CRUD::Spec
+      def tests
+        @tests.map do |test|
+          lambda do
+            Mongo::Transactions::TransactionsTest.new(@data, test, self)
+          end
+        end
+      end
+    end
+  end
+end
