c420931377f9fa3d0e894178a5e7d3b80800beaa
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2019-07-12 11:35:15 -0400

    Fix RUBY-1840 Legacy write retries are used when modern retryable writes are requested on some operations (#1433)
    * Fix RUBY-1840 Legacy write retries are used when modern retryable writes are requested on some operations
    
    * Return to default write retry configuration

--- lib/mongo/collection/view.rb
+++ lib/mongo/collection/view.rb
@@ -63,7 +63,7 @@ module Mongo
                      :read_with_retry,
                      :read_with_retry_cursor,
                      :write_with_retry,
-                     :legacy_write_with_retry,
+                     :nro_write_with_retry,
                      :write_concern_with_session
 
       # Delegate to the cluster for the next primary.
