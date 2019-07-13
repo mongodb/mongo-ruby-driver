54fcf617fefe8b806f923dc96a109b4f7fecf9eb
Oleg Pudeyev <oleg@bsdpower.com>
2019-07-12 20:17:41 -0400

    RUBY-1868 test cluster time equality also

--- spec/mongo/cluster_time_spec.rb
+++ spec/mongo/cluster_time_spec.rb
@@ -116,4 +116,33 @@ describe Mongo::ClusterTime do
       end
     end
   end
+
+  describe '#==' do
+    context 'equal but different objects' do
+      let(:one) { described_class.new(clusterTime: BSON::Timestamp.new(123, 456)) }
+      let(:two) { described_class.new(clusterTime: BSON::Timestamp.new(123, 456)) }
+
+      it 'is true' do
+        expect(one).to be == two
+      end
+    end
+
+    context 'first is greater' do
+      let(:one) { described_class.new(clusterTime: BSON::Timestamp.new(124, 456)) }
+      let(:two) { described_class.new(clusterTime: BSON::Timestamp.new(123, 456)) }
+
+      it 'is false' do
+        expect(one).not_to be == two
+      end
+    end
+
+    context 'second is greater' do
+      let(:one) { described_class.new(clusterTime: BSON::Timestamp.new(123, 456)) }
+      let(:two) { described_class.new(clusterTime: BSON::Timestamp.new(123, 457)) }
+
+      it 'is false' do
+        expect(one).not_to be == two
+      end
+    end
+  end
 end
