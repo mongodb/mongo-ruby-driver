565063b4965523428d36c7d0a4686108272dd792
Oleg Pudeyev <oleg@bsdpower.com>
2019-07-22 17:09:01 -0400

    Remove duplicate options assignment and attribute

--- lib/mongo/server/connection_pool/connection_pool_populator.rb
+++ lib/mongo/server/connection_pool/connection_pool_populator.rb
@@ -22,15 +22,12 @@ module Mongo
       include BackgroundThread
       include Loggable
 
-      attr_reader :options
-
       # @param [ Server::ConnectionPool ] The connection pool.
       # @param [ Hash ] options The options.
       #
       # @option options [ Logger ] :logger A custom logger to use.
       def initialize(pool, options = {})
         @pool = pool
-        @options = @pool.options
         @thread = nil
         @options = options
       end
