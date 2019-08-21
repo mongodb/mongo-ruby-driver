f795df3a46563d8b2b854de1b882dbb0b2cb8089
Emily Giurleo <e.m.giurleo@gmail.com>
2019-08-21 10:09:37 -0400

    try different formatter

--- .evergreen/run-enterprise-auth-tests.sh
+++ .evergreen/run-enterprise-auth-tests.sh
@@ -25,7 +25,7 @@ bundle install --gemfile="$BUNDLE_GEMFILE"
 
 echo "Running tests"
 bundle exec rspec spec/enterprise_auth -fd
-bundle exec rspec spec/spec_tests/uri_options_spec.rb
-bundle exec rspec spec/spec_tests/connection_string_spec.rb
-bundle exec rspec spec/mongo/uri/srv_protocol_spec.rb
-bundle exec rspec spec/mongo/uri_spec.rb
+bundle exec rspec spec/spec_tests/uri_options_spec.rb -fd
+bundle exec rspec spec/spec_tests/connection_string_spec.rb -fd
+bundle exec rspec spec/mongo/uri/srv_protocol_spec.rb -fd
+bundle exec rspec spec/mongo/uri_spec.rb -fd
