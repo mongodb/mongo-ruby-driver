e1dd2a1bf82d4b55928d7a643c40c45d08863a42
Oleg Pudeyev <oleg@bsdpower.com>
2019-08-23 12:48:18 -0400

    Improve robustness

--- spec/support/spec_config.rb
+++ spec/support/spec_config.rb
@@ -53,11 +53,14 @@ class SpecConfig
         @mongodb_uri.servers
       else
         client = Mongo::Client.new(['localhost:27017'], server_selection_timeout: 5.02)
-        client.cluster.next_primary
-        @addresses = client.cluster.servers_list.map do |server|
-          server.address.to_s
+        begin
+          client.cluster.next_primary
+          @addresses = client.cluster.servers_list.map do |server|
+            server.address.to_s
+          end
+        ensure
+          client.close
         end
-        client.close(true)
       end
     end
   end
