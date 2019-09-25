61fcb410404c09c5799b844855e75d05dff01cff
Emily Giurleo <e.m.giurleo@gmail.com>
2019-09-25 15:29:26 -0400

    wip: x509 tests

--- spec/integration/x509_spec.rb
+++ spec/integration/x509_spec.rb
@@ -1,81 +1,55 @@
 require 'spec_helper'
 
 describe 'X509 client authentication' do
-  # before(:all) do
-  #   # If auth is configured, the test suite uses the configured user
-  #   # and does not create its own users. However, the configured user may
-  #   # not have the auth mechanisms we need. Therefore we create a user
-  #   # for this test without specifying auth mechanisms, which gets us
-  #   # server default (scram for 4.0, scram & scram256 for 4.2).
-
-  #   users = ClientRegistry.instance.global_client('root_authorized').use(:admin).database.users
-  #   unless users.info('existing_user').empty?
-  #     users.remove('existing_user')
-  #   end
-  #   users.create('existing_user', password: 'password')
-  # end
-
   before(:all) do
-    client = ClientRegistry.instance.global_client('root_authorized').use(:$external)
-    users = client.database.users
-
-    unless users.info('C=US,ST=New York,L=New York City,O=MongoDB,OU=x509,CN=localhost').empty?
-      users.remove('C=US,ST=New York,L=New York City,O=MongoDB,OU=x509,CN=localhost')
-    end
-
-    users.create("C=US,ST=New York,L=New York City,O=MongoDB,OU=x509,CN=localhost")
+    user = 'C=US,ST=New York,L=New York City,O=MongoDB,OU=x509,CN=localhost'
+    users = ClientRegistry.instance.global_client('root_authorized').use(:$external).database.users
+    users.remove(user) unless users.info(user).empty?
   end
 
-  context 'attempting to connect without tls options' do
-    # TODO: write this test
-    # TODO: maybe write another test to connect with WRONG tls options?
-
-    # let!(:client) { ClientRegistry.instance.new_local_client("mongodb://localhost:27017/") }
-
-    # it 'fails on insert' do
-    #   result = client[:test].insert_one({ text: 'Hello, world!' })
-    # end
-  end
-
-  context 'with URI options' do
-    it 'successfully inserts a document' do
-      client = ClientRegistry.instance.new_local_client("mongodb://localhost:27017",
-        {
-          auth_mech: :mongodb_x509,
-          ssl: true,
-          ssl_cert: "spec/support/certificates/client-x509.pem",
-          ssl_ca_cert: "spec/support/certificates/ca.crt",
-          database: "test",
-          ssl_key: "spec/support/certificates/client-x509.pem",
-          auth_source: "$external"
-        }
-      )
-
-      client = Mongo::Client.new("mongodb://localhost:27017",
-        {
-          auth_mech: :mongodb_x509,
-          ssl: true,
-          ssl_cert: "spec/support/certificates/client-x509.pem",
-          ssl_ca_cert: "spec/support/certificates/ca.crt",
-          database: "test",
-        }
-      )
-
-      # {"database"=>"test", "auth_source"=>"$external", "auth_mech"=>:mongodb_x509, "ssl"=>true, "ssl_ca_cert"=>"spec/support/certificates/ca.crt", "ssl_cert"=>"spec/support/certificates/client-x509.pem", "ssl_key"=>"spec/support/certificates/client-x509.pem", "retry_reads"=>true, "retry_writes"=>true}
-
-      client.update_options({"database"=>"test", "auth_source" => "$external", "auth_mech"=>:mongodb_x509, "ssl"=>true, "ssl_cert"=>"spec/support/certificates/client-x509.pem", "ssl_ca_cert"=>"spec/support/certificates/ca.crt", "retry_reads"=>true, "ssl_key"=>"spec/support/certificates/client-x509.pem", "retry_writes"=>true})
-
-      result = client[:test].insert_one({ text: 'Hello, world!' })
-      expect(result).to be_ok
-    end
-  end
-
-  context 'with client options' do
-    it 'successfully inserts a document' do
+  context 'without the correct user' do
+    it 'cannot insert a document' do
       client = ClientRegistry.instance.new_local_client("mongodb://localhost:27017/?authMechanism=MONGODB-X509&tls=true&tlsCAFile=spec/support/certificates/ca.crt&tlsCertificateKeyFile=spec/support/certificates/client-x509.pem", database: 'test')
-      result = client[:test].insert_one({ text: 'Hello, world!' })
+      expect{
+        client[:test].insert_one({ text: 'Hello, world!' })
+    }.to raise_error(Mongo::Auth::Unauthorized)
+    end
+  end
 
-      expect(result).to be_ok
+  context 'with a database user in place' do
+    before(:all) do
+      user = 'C=US,ST=New York,L=New York City,O=MongoDB,OU=x509,CN=localhost'
+
+      users = ClientRegistry.instance.global_client('root_authorized').use(:$external).database.users
+
+      users.remove(user) unless users.info(user).empty?
+      users.create(user)
+    end
+
+    context 'with client options' do
+      it 'successfully inserts a document' do
+        client = ClientRegistry.instance.new_local_client("mongodb://localhost:27017",
+          {
+            auth_mech: :mongodb_x509,
+            ssl: true,
+            ssl_cert: "spec/support/certificates/client-x509.pem",
+            ssl_ca_cert: "spec/support/certificates/ca.crt",
+            database: "test",
+          }
+        )
+
+        result = client[:test].insert_one({ text: 'Hello, world!' })
+        expect(result).to be_ok
+      end
+    end
+
+    context 'with URI options' do
+      it 'successfully inserts a document' do
+        client = ClientRegistry.instance.new_local_client("mongodb://localhost:27017/?authMechanism=MONGODB-X509&tls=true&tlsCAFile=spec/support/certificates/ca.crt&tlsCertificateKeyFile=spec/support/certificates/client-x509.pem", database: 'test')
+        result = client[:test].insert_one({ text: 'Hello, world!' })
+
+        expect(result).to be_ok
+      end
     end
   end
 end
