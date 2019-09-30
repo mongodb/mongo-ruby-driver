a27ce042e07dfef06bb56db8e50f3c1ef11aae83
Oleg Pudeyev <oleg@bsdpower.com>
2019-09-30 12:56:04 -0400

    Read/write concern connection string spec runner

--- spec/runners/connection_string.rb
+++ spec/runners/connection_string.rb
@@ -54,6 +54,60 @@ def define_connection_string_spec_tests(test_paths, spec_cls = Mongo::Connection
             it 'creates a client with the correct options' do
               expect(test.client).to match_options(test)
             end
+
+            if test.read_concern_expectation
+              # Tests do not specify a read concern in the input and expect
+              # the read concern to be {); our non-specified read concern is nil.
+              # (But if a test used nil for the expectation, we wouldn't assert
+              # read concern at all.)
+              if test.read_concern_expectation == {}
+                it 'creates a client with no read concern' do
+                  actual = Utils.camelize_hash(test.client.options[:read_concern])
+                  expect(actual).to be nil
+                end
+              else
+
+                it 'creates a client with the correct read concern' do
+                  actual = Utils.camelize_hash(test.client.options[:read_concern])
+                  expect(actual).to eq(test.read_concern_expectation)
+                end
+              end
+            end
+
+            if test.write_concern_expectation
+              let(:actual_write_concern) do
+                Utils.camelize_hash(test.client.options[:write_concern])
+              end
+
+              let(:expected_write_concern) do
+                test.write_concern_expectation.dup.tap do |expected|
+                  # Spec tests have expectations on the "driver API" which is
+                  # different from what is being sent to the server. In Ruby
+                  # the "driver API" matches what we send to the server, thus
+                  # these expectations are rather awkward to work with.
+                  # Convert them all to expected server fields.
+                  j = expected.delete('journal')
+                  unless j.nil?
+                    expected['j'] = j
+                  end
+                  wtimeout = expected.delete('wtimeoutMS')
+                  unless wtimeout.nil?
+                    expected['wtimeout'] = wtimeout
+                  end
+                end
+              end
+
+              if test.write_concern_expectation == {}
+
+                it 'creates a client with no write concern' do
+                  expect(actual_write_concern).to be nil
+                end
+              else
+                it 'creates a client with the correct write concern' do
+                  expect(actual_write_concern).to eq(expected_write_concern)
+                end
+              end
+            end
           end
         end
       end
---
+++ spec/spec_tests/read_write_concern_connection_string_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+READ_WRITE_CONCERN_CONNECTION_STRING_TESTS =
+  Dir.glob("#{CURRENT_PATH}/spec_tests/data/read_write_concern/connection-string/*.yml").sort
+
+describe 'Connection String' do
+  define_connection_string_spec_tests(READ_WRITE_CONCERN_CONNECTION_STRING_TESTS)
+end
