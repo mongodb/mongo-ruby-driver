14818abba5cf093f879af615b3414b823537fa29
Emily Giurleo <e.m.giurleo@gmail.com>
2019-10-11 16:27:01 -0400

    set up tests on evergreen

---
+++ .evergreen/run-fle-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+. `dirname "$0"`/functions.sh
+
+wget -O libmongocrypt-all.tar.gz "https://s3.amazonaws.com/mciuploads/libmongocrypt/all/master/latest/libmongocrypt-all.tar.gz"
+tar xf libmongocrypt-all.tar.gz
+
+export LIBMONGOCRYPT_PATH=`pwd`/libmongocrypt-all/rhel-70-64-bit/lib/libmongocrypt.so
+
+set_env_vars
+setup_ruby
+
+install_deps
+
+bundle exec rake spec:prepare
+bundle exec rspec spec/mongo/libmongocrypt
+
+kill_jruby
