fecdb2c3d33ad4f4b01e72999f17679d6b0e8c50
Emily Giurleo <e.m.giurleo@gmail.com>
2019-10-24 17:17:41 -0400

    add some documentation

--- lib/mongo/libmongocrypt/binary.rb
+++ lib/mongo/libmongocrypt/binary.rb
@@ -13,15 +13,25 @@
 # limitations under the License.
 
 require 'ffi'
-require 'byebug' # TODO: remove
 
 module Mongo
   module Libmongocrypt
 
-    # TODO: class description
+    # A wrapper around mongocrypt_binary_t, a non-owning buffer of
+    # uint-8 byte data. Each Binary instance keeps a copy of the data
+    # passed to it in order to keep that data alive.
     #
     # @since 2.12.0
     class Binary
+
+      # Create a new Binary object that wrapps an array of bytes
+      #
+      # @example Instantiate a Binary object
+      #   Mongo::Libmongocrypt::Binary.new([73, 76, 111, 118, 101, 82, 117, 98, 121])
+      #
+      # @param [ Array<Int> ] data An array of uint-8 bytes
+      #
+      # @since 2.12.0
       def initialize(data)
         unless data
           raise MongocryptError.new('Cannot create new Binary object with no data.')
@@ -33,6 +43,11 @@ module Mongo
         @bin = Binding.mongocrypt_binary_new_from_data(@data_p, data.length)
       end
 
+      # Returns the data stored as a byte array
+      #
+      # @return [ Array<Int> ] Byte array stored in mongocrypt_binary_t
+      #
+      # @since 2.12.0
       def to_bytes
         data = Binding.mongocrypt_binary_data(@bin)
         if data == FFI::Pointer::NULL
@@ -43,6 +58,11 @@ module Mongo
         data.read_array_of_type(FFI::TYPE_UINT8, :read_uint8, len)
       end
 
+      # Releases allocated memory and cleans up resources
+      #
+      # @return [ true ] Always true.
+      #
+      # @since 2.12.0
       def close
         Binding.mongocrypt_binary_destroy(@bin) if @bin
 
@@ -50,6 +70,8 @@ module Mongo
 
         @data_p = nil
         @bin = nil
+
+        true
       end
     end
   end
