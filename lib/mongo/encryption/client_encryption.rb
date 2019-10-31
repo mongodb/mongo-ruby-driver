2bbd37f8814088a9c5726085383a1eb341a8d28e
Emily Giurleo <e.m.giurleo@gmail.com>
2019-10-31 13:03:29 -0400

    create client encryption initializer

---
+++ lib/mongo/encryption/client_encryption.rb
@@ -0,0 +1,67 @@
+# Copyright (C) 2019 MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module Mongo
+  module Encryption
+
+    # ClientEncryption encapsulates explicit operations on a key vault
+    # collection that cannot be done directly on a MongoClient. It
+    # provides an API for explicitly encrypting and decrypting values,
+    # and creating data keys.
+    class ClientEncryption
+
+      # Create a new ClientEncryption object with the provided options.
+      #
+      # @param [ Mongo::Client ] client A Mongo::Client
+      #   that is connected to the MongoDB instance where the key vault
+      #   collection is stored.
+      # @param [ Hash ] options The ClientEncryption options
+      #
+      # @option options [ String ] key_vault_namespace The name of the
+      #   key vault collection in the format "database.collection"
+      # @option options [ Hash ] kms_providers A hash of key management service
+      #   configuration information. Valid hash keys are :local or or :aws. There may be
+      #   more than one KMS provider specified.
+      def initialize(client, options = {})
+        validate_key_vault_namespace(options[:key_vault_namespace])
+
+        @client = client
+        @key_vault_db, @key_vault_coll = options[:key_vault_namespace].split('.')
+        @kms_providers = options[:kms_providers]
+
+        @crypt_handle = Crypt::Handle.new(@kms_providers)
+      end
+
+      def close
+        @crypt_handle.close if @crypt_handle
+        @crypt_handle = nil
+      end
+
+      private
+
+      def validate_key_vault_namespace(key_vault_namespace)
+        unless key_vault_namespace
+          raise ArgumentError.new('The :key_vault_namespace option cannot be nil.')
+        end
+
+        unless key_vault_namespace.split('.').length == 2
+          raise ArgumentError.new(
+            "#{key_vault_namespace} is an invalid key vault namespace;" +
+            "the :key_vault_namespace option must be in the format database.collection"
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/encryption/client_encryption_spec.rb
@@ -0,0 +1,69 @@
+require 'lite_spec_helper'
+require 'mongo'
+
+require 'base64'
+
+RSpec.configure do |config|
+  config.extend(LiteConstraints)
+end
+
+describe Mongo::Encryption::ClientEncryption do
+  describe '#initialize' do
+    let(:client) { new_local_client_nmio([SpecConfig.instance.addresses.first]) }
+    let(:client_encryption) do
+      described_class.new(client, {
+        key_vault_namespace: key_vault_namespace,
+        kms_providers: kms_providers
+      })
+    end
+
+    let(:key_vault_namespace) { 'test_db.collection' }
+    let(:kms_providers) do
+      {
+        local: { key: Base64.encode64("ru\xfe\x00" * 24) }
+      }
+    end
+
+    context 'with nil key_vault_namespace' do
+      let(:key_vault_namespace) { nil }
+
+      it 'raises an exception' do
+        expect do
+          client_encryption
+        end.to raise_error(ArgumentError, /:key_vault_namespace option cannot be nil/)
+      end
+    end
+
+    context 'with invalid key_vault_namespace' do
+      let(:key_vault_namespace) { 'three.word.namespace' }
+
+      it 'raises an exception' do
+        expect do
+          client_encryption
+        end.to raise_error(ArgumentError, /invalid key vault namespace/)
+      end
+    end
+
+    context 'with invalid KMS provider information' do
+      let(:kms_providers) { { random_key: {} } }
+
+      it 'raises an exception' do
+        expect do
+          client_encryption
+        end.to raise_error(ArgumentError, /kms_providers option must have one of the following keys/)
+      end
+    end
+
+    context 'with valid options' do
+      after do
+        client_encryption.close
+      end
+
+      it 'creates a ClientEncryption object' do
+        expect do
+          client_encryption
+        end.not_to raise_error
+      end
+    end
+  end
+end
