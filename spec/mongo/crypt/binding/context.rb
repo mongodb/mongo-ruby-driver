fe15b403b060a61f42864578e024101cab951491
Emily Giurleo <e.m.giurleo@gmail.com>
2019-11-01 12:14:09 -0400

    create binding necessary to initialize a data key context object

--- spec/mongo/crypt/binding/context.rb
+++ spec/mongo/crypt/binding/context.rb
@@ -13,12 +13,12 @@ describe 'Mongo::Crypt::Binding' do
     let(:context) { Mongo::Crypt::Binding.mongocrypt_ctx_new(mongocrypt) }
     let(:mongocrypt) { Mongo::Crypt::Binding.mongocrypt_new }
 
-    describe '#mongocrypt_ctx_new' do
-      after do
-        Mongo::Crypt::Binding.mongocrypt_destroy(mongocrypt)
-        Mongo::Crypt::Binding.mongocrypt_ctx_destroy(context)
-      end
+    after do
+      Mongo::Crypt::Binding.mongocrypt_destroy(mongocrypt)
+      Mongo::Crypt::Binding.mongocrypt_ctx_destroy(context)
+    end
 
+    describe '#mongocrypt_ctx_new' do
       it 'returns a pointer' do
         expect(context).to be_a_kind_of(FFI::Pointer)
       end
@@ -28,8 +28,6 @@ describe 'Mongo::Crypt::Binding' do
       let(:status) { Mongo::Crypt::Binding.mongocrypt_status_new }
 
       after do
-        Mongo::Crypt::Binding.mongocrypt_destroy(mongocrypt)
-        Mongo::Crypt::Binding.mongocrypt_ctx_destroy(context)
         Mongo::Crypt::Binding.mongocrypt_status_destroy(status)
       end
 
@@ -46,6 +44,7 @@ describe 'Mongo::Crypt::Binding' do
       # 16-byte binary uuid string
       let(:uuid) { "\xDEd\x00\xDC\x0E\xF8J\x99\x97\xFA\xCC\x04\xBF\xAA\x00\xF5" }
       let(:bytes) { uuid.unpack('C*') }
+
       let(:binary) do
         p = FFI::MemoryPointer
           .new(bytes.size)
@@ -63,9 +62,7 @@ describe 'Mongo::Crypt::Binding' do
       end
 
       after do
-        Mongo::Crypt::Binding.mongocrypt_destroy(mongocrypt)
         Mongo::Crypt::Binding.mongocrypt_binary_destroy(binary)
-        Mongo::Crypt::Binding.mongocrypt_ctx_destroy(context)
       end
 
       context 'with valid key id' do
@@ -86,11 +83,6 @@ describe 'Mongo::Crypt::Binding' do
 
       before do
         Mongo::Crypt::Binding.mongocrypt_init(mongocrypt)
-      end
-
-      after do
-        Mongo::Crypt::Binding.mongocrypt_destroy(mongocrypt)
-        Mongo::Crypt::Binding.mongocrypt_ctx_destroy(context)
       end
 
       context 'with deterministic algorithm' do
@@ -127,14 +119,50 @@ describe 'Mongo::Crypt::Binding' do
         Mongo::Crypt::Binding.mongocrypt_init(mongocrypt)
       end
 
-      after do
-        Mongo::Crypt::Binding.mongocrypt_destroy(mongocrypt)
-        Mongo::Crypt::Binding.mongocrypt_ctx_destroy(context)
+      it 'returns true' do
+        expect(result).to be true
+      end
+    end
+
+    describe '#mongocrypt_ctx_datakey_init' do
+      let(:result) do
+        Mongo::Crypt::Binding.mongocrypt_ctx_datakey_init(context)
       end
 
-      context 'with valid local master key' do
+      context 'a master key option and KMS provider have been set' do
+        let(:master_key) { "ru\xfe\x00" * 24 }
+        let(:bytes) { master_key.unpack('C*') }
+
+        let(:binary) do
+          p = FFI::MemoryPointer
+          .new(bytes.size)
+          .write_array_of_type(FFI::TYPE_UINT8, :put_uint8, bytes)
+
+          Mongo::Crypt::Binding.mongocrypt_binary_new_from_data(p, bytes.length)
+        end
+
+        before do
+          Mongo::Crypt::Binding.mongocrypt_setopt_kms_provider_local(mongocrypt, binary)
+          Mongo::Crypt::Binding.mongocrypt_init(mongocrypt)
+          Mongo::Crypt::Binding.mongocrypt_ctx_setopt_masterkey_local(context)
+        end
+
+        after do
+          Mongo::Crypt::Binding.mongocrypt_binary_destroy(binary)
+        end
+
         it 'returns true' do
           expect(result).to be true
+        end
+      end
+
+      context 'no master key has been set' do
+        before do
+          Mongo::Crypt::Binding.mongocrypt_init(mongocrypt)
+        end
+
+        it 'returns false' do
+          expect(result).to be false
         end
       end
     end
