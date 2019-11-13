86340434844cea8c485669c9c47cbac8fc9a292d
Oleg Pudeyev <oleg@bsdpower.com>
2019-11-12 20:52:55 -0500

    1601 verifier

--- spec/runners/sdam/verifier.rb
+++ spec/runners/sdam/verifier.rb
@@ -1,7 +1,6 @@
 module Sdam
   class Verifier
     include RSpec::Matchers
-    include Mongo::SDAMMonitoring::Matchable
 
     def initialize(test_instance)
       @test_instance = test_instance
@@ -28,7 +27,27 @@ module Sdam
     end
 
     def verify_topology_description_changed_event(expected, actual)
-      expect(topologies_match?(actual, expected)).to be true
+      verify_topology_matches(expected.data['previousDescription'], actual.previous_topology)
+      verify_topology_matches(expected.data['newDescription'], actual.new_topology)
+    end
+
+    def verify_topology_matches(expected, actual)
+      expected_type = ::Mongo::Cluster::Topology.const_get(expected['topologyType'])
+      expect(actual).to be_a(expected_type)
+
+      expect(actual.replica_set_name).to eq(expected['setName'])
+
+      expected['servers'].each do |server|
+        desc = actual.server_descriptions[server['address'].to_s]
+        expect(desc).not_to be nil
+        verify_description_matches(server, desc)
+      end
+
+      actual.server_descriptions.keys.each do |address_str|
+        expect(
+          expected['servers'].any? { |server| server['address'] == address_str }
+        ).to be true
+      end
     end
 
     def verify_server_opening_event(expected, actual)
@@ -36,7 +55,36 @@ module Sdam
     end
 
     def verify_server_description_changed_event(expected, actual)
-      expect(descriptions_match?(actual, expected)).to be true
+      verify_description_matches(expected.data['previousDescription'], actual.previous_description)
+      verify_description_matches(expected.data['newDescription'], actual.new_description)
+    end
+
+    def verify_description_matches(expected, actual)
+      case expected['type']
+      when 'Standalone'
+        expect(actual).to be_standalone
+      when 'RSPrimary'
+        expect(actual).to be_primary
+      when 'RSSecondary'
+        expect(actual).to be_secondary
+      when 'RSArbiter'
+        expect(actual).to be_arbiter
+      when 'Mongos'
+        expect(actual).to be_mongos
+      when 'Unknown', 'PossiblePrimary'
+        expect(actual).to be_unknown
+      when 'RSGhost'
+        expect(actual).to be_ghost
+      when 'RSOther'
+        expect(actual).to be_other
+      end
+
+      expect(actual.address.to_s).to eq(expected['address'])
+      expect(actual.arbiters).to eq(expected['arbiters'])
+      expect(actual.hosts).to eq(expected['hosts'])
+      expect(actual.passives).to eq(expected['passives'])
+      expect(actual.primary_host).to eq(expected['primary'])
+      expect(actual.replica_set_name).to eq(expected['setName'])
     end
 
     def verify_server_closed_event(expected, actual)
