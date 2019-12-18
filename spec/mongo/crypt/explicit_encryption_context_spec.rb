5861373f40e0ff2ae0c1986fb9acdf039c0e7781
Emily Giurleo <e.m.giurleo@gmail.com>
2019-12-18 14:24:30 -0500

    skip tests because we dont run evergreen with a verison of libmongocrypt that has logging enabled

--- spec/mongo/crypt/explicit_encryption_context_spec.rb
+++ spec/mongo/crypt/explicit_encryption_context_spec.rb
@@ -81,26 +81,28 @@ describe Mongo::Crypt::ExplicitEncryptionContext do
 
       context 'with verbose logging' do
         before(:all) do
-          ENV['MONGOCRYPT_TRACE'] = 'ON'
-        end
-
-        after(:all) do
-          ENV['MONGOCRYPT_TRACE'] = nil
-        end
-
-        let(:logger) do
-          Logger.new('/dev/null'). tap do |logger|
-            logger.level = :debug
-          end
+          # Logging from libmongocrypt requires the C library to be built with the -DENABLE_TRACE=ON
+          # option; none of the pre-built packages on Evergreen have been built with logging enabled.
+          #
+          # It is still useful to be able to run these tests locally to confirm that logging is working
+          # while debugging any problems.
+          #
+          # For now, skip this test by default and revisit once we have determined how we want to
+          # package libmongocrypt with the Ruby driver.
+          skip "These tests require libmongocrypt to be built with the '-DENABLE_TRACE=ON' cmake option." +
+            " They also require the MONGOCRYPT_TRACE environment variable to be set to 'ON'."
         end
 
         before do
-          Mongo::Logger.logger = logger
+          @original_logger_level = Mongo::Logger.level
+          Mongo::Logger.level = Logger::DEBUG
         end
 
         after do
-          Mongo::Logger.logger = nil
+          Mongo::Logger.level = @original_logger_level
         end
+
+        let(:logger) { Mongo::Logger.logger }
 
         it 'receives log messages from libmongocrypt' do
           expect(logger).to receive(:debug).with(/mongocrypt_ctx_setopt_key_id/)
