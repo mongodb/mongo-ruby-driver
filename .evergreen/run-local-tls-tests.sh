67ffb4934c23754e1e83d481cd618925f6ac3c2b
Emily Giurleo <e.m.giurleo@gmail.com>
2019-12-31 12:08:43 -0500

    throw some which pythons in there

--- .evergreen/run-local-tls-tests.sh
+++ .evergreen/run-local-tls-tests.sh
@@ -36,7 +36,9 @@ install_mlaunch_python3
 export dbdir="$MONGO_ORCHESTRATION_HOME"/db
 mkdir -p "$dbdir"
 
+which python
 source mlaunch/venv/bin/activate
+which python
 mlaunch --dir "$dbdir" --binarypath "$BINDIR" --single \
   --sslMode requireSSL \
   --sslPEMKeyFile spec/support/certificates/server-second-level-bundle.pem \
