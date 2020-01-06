7145f85d82086a9ccf5777088d6dc03bb9ad5ba7
Emily Giurleo <e.m.giurleo@gmail.com>
2020-01-06 16:01:48 -0500

    RUBY-2060 Pin mlaunch version on evergreen (#1621)
    * pin mlaunch version
    
    * remove keyword argument from spec
    
    * Re-running tests

--- .evergreen/functions.sh
+++ .evergreen/functions.sh
@@ -201,7 +201,7 @@ prepare_server_from_url() {
 
 install_mlaunch() {
   pythonpath="$MONGO_ORCHESTRATION_HOME"/python
-  pip install -t "$pythonpath" 'mtools[mlaunch]'
+  pip install -t "$pythonpath" -v 'mtools[mlaunch]==1.5.3'
   export PATH="$pythonpath/bin":$PATH
   export PYTHONPATH="$pythonpath"
 }
