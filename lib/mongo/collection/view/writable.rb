224179664b2f758ddbc0eb4074696866b4cd4fc5
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2020-01-07 00:49:20 -0500

    Fix RUBY-2054 upsert option is not sent to legacy servers with w:0 due to string/symbol key mismatch (#1610)
    * Fix RUBY-2054 upsert option is not sent to legacy servers with w:0 due to string/symbol key mismatch
    
    * Remove debug print
    
    Co-authored-by: Oleg Pudeyev <p@users.noreply.github.com>

--- lib/mongo/collection/view/writable.rb
+++ lib/mongo/collection/view/writable.rb
@@ -232,7 +232,7 @@ module Mongo
                          Operation::U => replacement,
                         }
           if opts[:upsert]
-            update_doc[:upsert] = true
+            update_doc['upsert'] = true
           end
           with_session(opts) do |session|
             write_concern = write_concern_with_session(session)
@@ -279,7 +279,7 @@ module Mongo
                          Operation::MULTI => true,
                          }
           if opts[:upsert]
-            update_doc[:upsert] = true
+            update_doc['upsert'] = true
           end
           with_session(opts) do |session|
             write_concern = write_concern_with_session(session)
@@ -323,7 +323,7 @@ module Mongo
                          Operation::U => spec,
                          }
           if opts[:upsert]
-            update_doc[:upsert] = true
+            update_doc['upsert'] = true
           end
           with_session(opts) do |session|
             write_concern = write_concern_with_session(session)
