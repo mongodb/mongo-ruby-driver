f25dfd78ec2f1d065b0435b9c82040f8032b287a
Emily Giurleo <e.m.giurleo@gmail.com>
2020-01-07 14:12:58 -0500

    specified incorrect libmongocrypt path in tests

--- .evergreen/run-fle-tests.sh
+++ .evergreen/run-fle-tests.sh
@@ -8,7 +8,7 @@ set -o errexit  # Exit the script with error if any of the commands fail
 wget "https://s3.amazonaws.com/mciuploads/libmongocrypt/all/master/latest/libmongocrypt-all.tar.gz"
 tar -xvf libmongocrypt-all.tar.gz
 
-export LIBMONGOCRYPT_PATH=`pwd`/rhel-70-64-bit/nocrypto/lib/libmongocrypt.so
+export LIBMONGOCRYPT_PATH=`pwd`/rhel-70-64-bit/nocrypto/lib64/libmongocrypt.so
 
 set_env_vars
 setup_ruby
