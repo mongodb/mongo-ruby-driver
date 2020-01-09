c0dc64b3a54b2e4f3456e39f55ee7f90728d1603
Emily Giurleo <e.m.giurleo@gmail.com>
2020-01-09 13:47:34 -0500

    delete comment

--- .evergreen/functions.sh
+++ .evergreen/functions.sh
@@ -81,7 +81,6 @@ set_env_vars() {
   # JRUBY_OPTS were initially set for Mongoid
   export JRUBY_OPTS="--server -J-Xms512m -J-Xmx2G"
 
-  echo $BSON
   if test "$BSON" = min; then
     export BUNDLE_GEMFILE=gemfiles/bson_min.gemfile
   elif test "$BSON" = master; then
