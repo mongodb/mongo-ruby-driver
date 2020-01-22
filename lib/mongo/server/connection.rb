e2e2fe7c60878944caf016e1f115e3fb5706047c
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2020-01-22 14:50:36 -0500

    RUBY-2091 Provide stack trace for ismaster failures in monitor (#1647)
    * RUBY-2091 Provide stack trace for ismaster failures in monitor
    
    * Look for required text in warnings rather than mandating complete equality
    
    Co-authored-by: Oleg Pudeyev <p@users.noreply.github.com>

--- lib/mongo/server/connection.rb
+++ lib/mongo/server/connection.rb
@@ -322,7 +322,7 @@ module Mongo
               raise exc
             end
           rescue => e
-            log_warn("Failed to handshake with #{address}: #{e.class}: #{e}")
+            log_warn("Failed to handshake with #{address}: #{e.class}: #{e}:\n#{e.backtrace[0..5].join("\n")}")
             raise
           end
         end
@@ -379,7 +379,7 @@ module Mongo
             begin
               Auth.get(user).login(pending_connection)
             rescue => e
-              log_warn("Failed to handshake with #{address}: #{e.class}: #{e}")
+              log_warn("Failed to handshake with #{address}: #{e.class}: #{e}:\n#{e.backtrace[0..5].join("\n")}")
               raise
             end
           end
