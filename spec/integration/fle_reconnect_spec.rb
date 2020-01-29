63aef9cd3c70837e2de69d837782940d86009c67
Emily Giurleo <e.m.giurleo@gmail.com>
2020-01-29 15:05:42 -0500

    add some cases to reconnect spec

--- spec/integration/fle_reconnect_spec.rb
+++ spec/integration/fle_reconnect_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe 'Client with auto encryption after reconnect' do
+  let(:client) do
+    new_local_client(
+      'mongodb://localhost:27017/test',
+      {
+        auto_encryption_options: {
+          kms_providers: { local: { key: key } },
+          key_vault_namespace: 'admin.datakeys',
+        }
+      }
+    )
+  end
 
+  context 'when reconnecting without closing' do
+
+  end
+
+  context 'when reconnecting after closing' do
+
+  end
+
+  context 'after killing monitor thread' do
+
+  end
+
+  
 end
