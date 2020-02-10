d6d736c1743a6e05b59fe95473941a429de2ea4c
Oleg Pudeyev <oleg@bsdpower.com>
2020-02-10 11:43:01 -0500

    silence some of the debug output

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -o xtrace   # Write all commands first to stderr
+#set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
 # Inspiration:
