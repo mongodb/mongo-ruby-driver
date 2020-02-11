fba7cfd5933a2abe05d895933902ecbda93be309
Oleg Pudeyev <oleg@bsdpower.com>
2020-02-11 12:54:50 -0500

    remove verbose pip output

--- .evergreen/functions.sh
+++ .evergreen/functions.sh
@@ -227,7 +227,7 @@ prepare_server_from_url() {
 
 install_mlaunch() {
   pythonpath="$MONGO_ORCHESTRATION_HOME"/python
-  pip install -t "$pythonpath" -v 'mtools[mlaunch]==1.5.3'
+  pip install -t "$pythonpath" 'mtools[mlaunch]==1.5.3'
   export PATH="$pythonpath/bin":$PATH
   export PYTHONPATH="$pythonpath"
 }
