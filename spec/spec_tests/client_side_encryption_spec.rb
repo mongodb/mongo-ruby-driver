bec5f5a4e58be93dda6021df6685f7682d6f2942
Emily Giurleo <e.m.giurleo@gmail.com>
2020-02-28 16:58:04 -0500

    add correct requirements to spec tests

--- spec/spec_tests/client_side_encryption_spec.rb
+++ spec/spec_tests/client_side_encryption_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe 'Client-Side Encryption' do
+  require_libmongocrypt
+  require_enterprise
+  min_server_fcv '4.2'
+
   define_transactions_spec_tests(CLIENT_SIDE_ENCRYPTION_TESTS)
 end
