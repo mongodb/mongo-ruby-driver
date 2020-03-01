1cd31b69a6ea0943d89e0f968ea5a735482a78bd
Oleg Pudeyev <oleg@bsdpower.com>
2020-03-01 14:17:09 -0500

    use a single rspec command to generate correct reports

--- .evergreen/run-tests-with-kerberos.sh
+++ .evergreen/run-tests-with-kerberos.sh
@@ -15,8 +15,9 @@ export MONGODB_URI='mongodb://localhost:27017'
 
 bundle exec rake spec:prepare
 
-bundle exec rspec spec/spec_tests/uri_options_spec.rb
-bundle exec rspec spec/spec_tests/connection_string_spec.rb
-bundle exec rspec spec/mongo/uri/srv_protocol_spec.rb
-bundle exec rspec spec/mongo/uri_spec.rb
-bundle exec rspec spec/integration/client_authentication_options_spec.rb
+bundle exec rspec \
+  spec/spec_tests/uri_options_spec.rb \
+  spec/spec_tests/connection_string_spec.rb \
+  spec/mongo/uri/srv_protocol_spec.rb \
+  spec/mongo/uri_spec.rb \
+  spec/integration/client_authentication_options_spec.rb
