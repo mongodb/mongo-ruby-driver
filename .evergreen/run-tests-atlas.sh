7fe91110d41c3abcf14e717ac9efcd4746b8120f
Oleg Pudeyev <oleg@bsdpower.com>
2020-03-01 17:28:53 -0500

    remove outdated comment listing supported environment variables

--- .evergreen/run-tests-atlas.sh
+++ .evergreen/run-tests-atlas.sh
@@ -3,16 +3,6 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
-# Supported/used environment variables:
-#       AUTH                    Set to enable authentication. Values are: "auth" / "noauth" (default)
-#       SSL                     Set to enable SSL. Values are "ssl" / "nossl" (default)
-#       MONGODB_URI             Set the suggested connection MONGODB_URI (including credentials and topology info)
-#       TOPOLOGY                Allows you to modify variables and the MONGODB_URI based on test topology
-#                               Supported values: "server", "replica_set", "sharded_cluster"
-#       RVM_RUBY                Define the Ruby version to test with, using its RVM identifier.
-#                               For example: "ruby-2.3" or "jruby-9.1"
-#       DRIVER_TOOLS            Path to driver tools.
-
 . `dirname "$0"`/functions.sh
 
 set_env_vars
