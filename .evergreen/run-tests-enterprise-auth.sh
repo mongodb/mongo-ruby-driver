ed72cdbad7d45929b7cf8fa69a1e468a6aee542d
Oleg Pudeyev <oleg@bsdpower.com>
2020-03-01 19:33:11 -0500

    unify enterprise auth test configuration

--- .evergreen/run-tests-enterprise-auth.sh
+++ .evergreen/run-tests-enterprise-auth.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-# Don't trace to avoid secrets showing up in the logs
-set -o errexit
+# IMPORTANT: Don't set trace (-x) to avoid secrets showing up in the logs.
+set +x
+
+set -e
 
 . `dirname "$0"`/functions.sh
 
 set_env_vars
 
 setup_ruby
+
+# TODO Find out of $OS is set here, right now we only test on Linux thus
+# it doesn't matter if it is set.
+case "$OS" in
+  cygwin*)
+    IP_ADDR=`getent hosts ${sasl_host} | head -n 1 | awk '{print $1}'`
+    ;;
+
+  darwin)
+    IP_ADDR=`dig ${sasl_host} +short | tail -1`
+    ;;
+
+  *)
+    IP_ADDR=`getent hosts ${sasl_host} | head -n 1 | awk '{print $1}'`
+esac
+
+export IP_ADDR=$IP_ADDR
+
+# Note that .env.private is supposed to be in Dotenv format which supports
+# multi-line values. Currently all values set for Kerberos tests are
+# single-line hence this isn't an issue.
+. ./.env.private
 
 echo "Setting krb5 config file"
 touch ${PROJECT_DIRECTORY}/.evergreen/krb5.conf.empty
@@ -22,6 +46,8 @@ kinit -k -t ${PROJECT_DIRECTORY}/.evergreen/drivers.keytab -p ${PRINCIPAL}
 echo "Install dependencies"
 export BUNDLE_GEMFILE=gemfiles/mongo_kerberos.gemfile
 bundle_install
+
+export KERBEROS=1
 
 echo "Running tests"
 bundle exec rspec spec/enterprise_auth
