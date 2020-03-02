b4c108fb0bb538b4c5a59ee5a0475d9dd0a4bae1
Emily Giurleo <e.m.giurleo@gmail.com>
2020-03-02 18:01:42 -0500

    fix integration test and add comment

--- spec/integration/auto_encryption_old_wire_version_spec.rb
+++ spec/integration/auto_encryption_old_wire_version_spec.rb
@@ -45,7 +45,7 @@ describe 'Auto Encryption' do
     it 'raises an exception when trying to encrypt' do
       expect do
         encryption_client[:users].find(ssn: ssn).first
-      end.to raise_error(Mongo::Error::CryptError, /Cannot perform encryption against a MongoDB server older than 4.2/)
+      end.to raise_error(Mongo::Error::CryptError, /Auto-encryption requires a minimum MongoDB version of 4.2/)
     end
 
     context 'with bypass_auto_encryption=true' do
