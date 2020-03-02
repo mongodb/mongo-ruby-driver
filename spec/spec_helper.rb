a52986466164861334644c0ad3535d6942e961a7
Oleg Pudeyev <oleg@bsdpower.com>
2020-03-02 01:29:11 -0500

    TEST_SET is no longer used

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,5 @@
 require 'lite_spec_helper'
 
-# Replica set name can be overridden via replicaSet parameter in MONGODB_URI
-# environment variable or by specifying RS_NAME environment variable when
-# not using MONGODB_URI.
-TEST_SET = 'ruby-driver-rs'
-
 require 'support/authorization'
 require 'support/primary_socket'
 require 'support/constraints'
