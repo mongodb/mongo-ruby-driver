8a0836465dae33440439bd13afe164216d5f9745
Emily Giurleo <e.m.giurleo@gmail.com>
2020-03-04 18:56:27 -0500

    test for bson size limit

---
+++ spec/integration/test_bson_size_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'BSON object size' do
+  it 'raises an exception when document size is 16MiB' do
+    client = new_local_client(SpecConfig.instance.addresses)
+    max_size = 16777216 # 16 MiB
+
+    document = { key: 'a' * (max_size - 15) }
+    expect(document.to_bson.length).to eq(max_size)
+
+    client.use(:db)[:coll].insert_one(document)
+  end
+end
