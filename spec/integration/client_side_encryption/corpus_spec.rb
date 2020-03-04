816d7241152f1d7b35a3bf8c9d63b65158e96130
Emily Giurleo <e.m.giurleo@gmail.com>
2020-03-03 23:03:50 -0500

    wip: corpus tests

---
+++ spec/integration/client_side_encryption/corpus_spec.rb
@@ -0,0 +1,108 @@
+require 'spec_helper'
+
+describe 'Client-Side Encryption' do
+  describe 'Prose tests: Corpus Test' do
+    require_libmongocrypt
+    include_context 'define shared FLE helpers'
+
+    let(:client) do
+      new_local_client(
+        SpecConfig.instance.addresses,
+        SpecConfig.instance.test_options
+      )
+    end
+
+    let(:schema_map) { BSON::ExtJSON.parse(File.read('spec/support/crypt/corpus/corpus-schema.json')) }
+    let(:local_data_key) { BSON::ExtJSON.parse(File.read('spec/support/crypt/corpus/corpus-key-local.json')) }
+    let(:aws_data_key) { BSON::ExtJSON.parse(File.read('spec/support/crypt/corpus/corpus-key-aws.json')) }
+
+    let(:client_encrypted) do
+      new_local_client(
+        SpecConfig.instance.addresses,
+        SpecConfig.instance.test_options.merge(
+          auto_encryption_options: {
+            kms_providers: {
+              local: { key: local_master_key },
+              aws: {
+                access_key_id: SpecConfig.instance.fle_aws_key,
+                secret_access_key: SpecConfig.instance.fle_aws_secret,
+              },
+            },
+            key_vault_namespace: 'admin.datakeys',
+            schema_map: test_schema_map,
+          },
+          database: :db,
+        )
+      )
+    end
+
+    let(:client_encryption) do
+      Mongo::ClientEncryption.new(
+        client,
+        {
+          kms_providers: {
+            local: { key: local_master_key },
+            aws: {
+              access_key_id: SpecConfig.instance.fle_aws_key,
+              secret_access_key: SpecConfig.instance.fle_aws_secret,
+            }
+          },
+          key_vault_namespace: 'admin.datakeys',
+        },
+      )
+    end
+
+    let(:corpus) do
+      BSON::ExtJSON.parse(File.read('spec/support/crypt/corpus/corpus.json'))
+    end
+
+    let(:corpus_copied) do
+      doc = BSON::Document.new
+      corpus.each do |key, value|
+        if ['_id', 'altname_aws', 'altname_local'].include?(key)
+          doc[key] = value
+        end
+
+        if value['method'] == 'auto'
+          doc[key] = value
+        end
+
+        if value['method'] == 'explicit'
+          options = {}
+
+          options = if value['identifier'] == 'id'
+            {
+              
+            }
+          elsif value['identifier'] == 'altname'
+            {
+              key_alt_name: value['kms']
+            }
+          end
+          doc[key] = client_encryption.encrypt(
+            value['value'],
+            {
+              key_id: ,
+              algorithm: value['algo']
+            }
+          )
+        end
+      end
+
+      doc
+    end
+
+    before do
+      client.use(:db)[:coll].drop
+      client.use(:db)[:coll,
+        {
+          'validator' => { '$jsonSchema' => schema_map }
+        }
+      ].create
+
+      client.use(:admin)[:datakeys].drop
+      client.use(:admin)[:datakeys].insert_one(local_data_key)
+      client.use(:admin)[:datakeys].insert_one(aws_data_key)
+    end
+  end
+end
