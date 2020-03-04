3f4a93149223daf6d17d2a27d0a9f4f8241a8024
Emily Giurleo <e.m.giurleo@gmail.com>
2020-03-04 16:24:29 -0500

    RUBY-2152 Add prose spec for encrypted command on views (#1757)
    * wip: views are prohibited
    
    * add a spec for encrypted commands on views

---
+++ spec/integration/client_side_encryption/views_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'Client-Side Encryption' do
+  describe 'Prose tests: Data key and double encryption' do
+    require_libmongocrypt
+    require_enterprise
+    min_server_fcv '4.2'
+
+    include_context 'define shared FLE helpers'
+
+    let(:client) do
+      new_local_client(
+        SpecConfig.instance.addresses,
+        SpecConfig.instance.test_options
+      )
+    end
+
+    let(:client_encrypted) do
+      new_local_client(
+        SpecConfig.instance.addresses,
+        SpecConfig.instance.test_options.merge(
+          auto_encryption_options: {
+            kms_providers: local_kms_providers,
+            key_vault_namespace: 'admin.datakeys',
+          },
+          database: :db,
+        )
+      )
+    end
+
+    before do
+      client.use(:db)[:view].drop
+      client.use(:db).database.command(create: "view", viewOn: "coll")
+    end
+
+    it 'does not perform encryption on views' do
+      expect do
+        client_encrypted[:view].insert_one({})
+      end.to raise_error(Mongo::Error::CryptError, /cannot auto encrypt a view/)
+    end
+  end
+end
