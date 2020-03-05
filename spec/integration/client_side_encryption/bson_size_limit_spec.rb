9e1e46b6c540258808236f96719d1ed22f187985
Emily Giurleo <e.m.giurleo@gmail.com>
2020-03-04 20:21:33 -0500

    cleaned up test descriptions and fixed issues

--- spec/integration/client_side_encryption/bson_size_limit_spec.rb
+++ spec/integration/client_side_encryption/bson_size_limit_spec.rb
@@ -18,6 +18,10 @@ describe 'Client-Side Encryption' do
       BSON::ExtJSON.parse(File.read('spec/support/crypt/limits/limits-schema.json'))
     end
 
+    let(:limits_doc) do
+      BSON::ExtJSON.parse(File.read('spec/support/crypt/limits/limits-doc.json'))
+    end
+
     let(:client_encrypted) do
       new_local_client(
         SpecConfig.instance.addresses,
@@ -35,10 +39,6 @@ describe 'Client-Side Encryption' do
       end
     end
 
-    let(:limits_doc) do
-      BSON::ExtJSON.parse(File.read('spec/support/crypt/limits/limits-doc.json'))
-    end
-
     before do
       client.use(:db)[:coll].drop
       client.use(:db)[:coll,
@@ -53,57 +53,87 @@ describe 'Client-Side Encryption' do
       )
     end
 
-    # context 'when document is under 2MiB limit' do
-    #   it 'can perform insert_one using the encrypted client' do
-    #     document = {
-    #       _id: "under_2mib",
-    #       unencrypted: 'a' * 2096980
-    #     }
+    context 'when unencrypted document is over the 2MiB limit but under the 16MiB limit' do
+      it 'can perform insert_one using the encrypted client' do
+        document = {
+          _id: "over_2mib_under_16mib",
+          unencrypted: 'a' * 2097152
+        }
 
-    #     result = client_encrypted[:coll].insert_one(document)
+        result = client_encrypted[:coll].insert_one(document)
 
-    #     expect(result).to be_ok
-    #   end
-    # end
+        expect(result).to be_ok
+      end
+    end
 
-    # context 'when document is under the 16MiB size limit' do
-    #   it 'can perform insert_one using the encrypted client' do
-    #     result = client_encrypted[:coll].insert_one(
-    #       {
-    #         _id: "encryption_exceeds_2mi",
-    #         unencrypted: 'a' * (2096980 - 2000)
-    #       }.merge(
-    #         BSON::ExtJSON.parse(File.read('spec/support/crypt/limits/limits-doc.json'))
-    #       )
-    #     )
+    context 'when encrypted content is under the 16MiB limit' do
+      it 'can perform insert_one using the encrypted client' do
+        result = client_encrypted[:coll].insert_one(
+          limits_doc.merge(
+            _id: "encryption_exceeds_2mi",
+            unencrypted: 'a' * (2097152 - 2000)
+          )
+        )
 
-    #     expect(result).to be_ok
-    #   end
-    # end
+        expect(result).to be_ok
+      end
+    end
 
-    # context 'when there are multiple documents, but neither is greater than 2MiB in size' do
-    #   it 'can perform bulk_insert using the encrypted client' do
-    #     bulk_write = Mongo::BulkWrite.new(
-    #       client_encrypted[:coll],
-    #       [
-    #         { insert_one: { _id: 'over_2mib_1', unencrypted: 'a' * 2096892 } },
-    #         { insert_one: { _id: 'over_2mib_2', unencrypted: 'a' * 2096892 } },
-    #       ]
-    #     )
+    context 'when there are multiple unencrypted documents, both under 16MiB in size' do
+      it 'can perform bulk_insert using the encrypted client' do
+        bulk_write = Mongo::BulkWrite.new(
+          client_encrypted[:coll],
+          [
+            { insert_one: { _id: 'over_2mib_1', unencrypted: 'a' * 2097152 } },
+            { insert_one: { _id: 'over_2mib_2', unencrypted: 'a' * 2097152 } },
+          ]
+        )
 
-    #     result = bulk_write.execute
-    #     expect(result.inserted_count).to eq(2)
+        result = bulk_write.execute
+        expect(result.inserted_count).to eq(2)
 
-    #     command_succeeded_events = subscriber.succeeded_events.select do |event|
-    #       event.command_name == 'insert'
-    #     end
+        command_succeeded_events = subscriber.succeeded_events.select do |event|
+          event.command_name == 'insert'
+        end
 
-    #     expect(command_succeeded_events.length).to eq(2)
-    #   end
-    # end
+        expect(command_succeeded_events.length).to eq(2)
+      end
+    end
 
-    context 'when the document falls under the maxBSONObjectSize limit' do
-      it 'inserts the document' do
+    context 'when there are multiple encrypted documents, both under 16MiB in size' do
+      it 'can perform bulk_insert using the encrypted client' do
+        bulk_write = Mongo::BulkWrite.new(
+          client_encrypted[:coll],
+          [
+            {
+              insert_one: limits_doc.merge(
+                _id: "encryption_exceeds_2mib_1",
+                unencrypted: 'a' * (2097152 - 2000)
+              )
+            },
+            {
+              insert_one: limits_doc.merge(
+                _id: 'encryption_exceeds_2mib_2',
+                unencrypted: 'a' * (2097152 - 2000)
+              )
+            },
+          ]
+        )
+
+        result = bulk_write.execute
+        expect(result.inserted_count).to eq(2)
+
+        command_succeeded_events = subscriber.succeeded_events.select do |event|
+          event.command_name == 'insert'
+        end
+
+        expect(command_succeeded_events.length).to eq(2)
+      end
+    end
+
+
+    context 'when the unencrypted document falls just under the maxBSONObjectSize limit' do
+      it 'can perform insert_one using the encrypted client' do
         result = client_encrypted[:coll].insert_one(
           _id: "under_16mib",
           unencrypted: "a" * (16777216 - 1000000)
@@ -113,7 +143,7 @@ describe 'Client-Side Encryption' do
       end
     end
 
-    context 'when the document exceeds maxBSONObjectSize limit' do
+    context 'when the encrypted document exceeds maxBSONObjectSize limit' do
       it 'raises an exception when attempting to insert the document' do
         expect do
           client_encrypted[:coll].insert_one(
