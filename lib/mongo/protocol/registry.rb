f70b3a51fe4d6a714970ec03c3d83239053d5409
Oleg Pudeyev <oleg@bsdpower.com>
2020-03-08 23:37:16 -0400

    Bump copyright year

--- lib/mongo/protocol/registry.rb
+++ lib/mongo/protocol/registry.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2009-2019 MongoDB Inc.
+# Copyright (C) 2009-2020 MongoDB Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
