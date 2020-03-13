7b4865f0fab88d0ed64ff02b5763582a8e0abfd3
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2020-03-13 15:24:44 -0400

    RUBY-2175 Change all instances of "FLE/Field-Level Encryption" to say "Client-Side Encryption" (#1785)
    Co-authored-by: Oleg Pudeyev <oleg@bsdpower.com>

--- lib/mongo/error/crypt_error.rb
+++ lib/mongo/error/crypt_error.rb
@@ -1,3 +1,7 @@
+# Copyright (C) 2020 MongoDB Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
@@ -11,7 +15,7 @@
 module Mongo
   class Error
 
-    # An error related to field-level encryption
+    # An error related to client-side encryption.
     class CryptError < Mongo::Error
       # Create a new CryptError
       #
--- lib/mongo/error/kms_error.rb
+++ lib/mongo/error/kms_error.rb
@@ -1,3 +1,7 @@
+# Copyright (C) 2020 MongoDB Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
@@ -11,7 +15,8 @@
 module Mongo
   class Error
 
-    # A KMS-related error during field-level encryption
-    class KmsError < CryptError; end
+    # A KMS-related error during client-side encryption.
+    class KmsError < CryptError
+    end
   end
 end
