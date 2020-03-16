a3a953c00232289b2ec520be12842e15adcdf405
Emily Giurleo <e.m.giurleo@gmail.com>
2020-03-16 17:15:27 -0400

    cleaned up bypass mongocryptd spawn spec

--- spec/integration/client_side_encryption/bypass_mongocryptd_spawn_spec.rb
+++ spec/integration/client_side_encryption/bypass_mongocryptd_spawn_spec.rb
@@ -27,7 +27,7 @@ describe 'Client-Side Encryption' do
                 mongocryptd_spawn_args: [ "--pidfilepath=bypass-spawning-mongocryptd.pid", "--port=27090"],
               },
             },
-            database: :db
+            database: 'db'
           ),
         )
       end
@@ -52,7 +52,7 @@ describe 'Client-Side Encryption' do
                 mongocryptd_spawn_args: [ "--pidfilepath=bypass-spawning-mongocryptd.pid", "--port=27090"],
               },
             },
-            database: :db
+            database: 'db'
           ),
         )
       end
@@ -63,7 +63,7 @@ describe 'Client-Side Encryption' do
 
       it 'does not spawn' do
         lambda do
-          client[:coll].insert_one(encrypted: 'test')
+          client['coll'].insert_one(encrypted: 'test')
         end.should_not raise_error
         lambda do
           mongocryptd_client.database.command(ismaster: 1)
