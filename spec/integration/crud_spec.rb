2874786e090b635e7aa08d8f8bcc68fceb2b8440
Emily Giurleo <e.m.giurleo@gmail.com>
2020-04-07 15:16:48 -0400

    RUBY-2119 add tests to ensure that fields are stored in the correct format in the database

--- spec/integration/crud_spec.rb
+++ spec/integration/crud_spec.rb
@@ -7,6 +7,83 @@ describe 'CRUD operations' do
     collection.delete_many
   end
 
+  describe 'insert' do
+    context 'inserting a BSON::Int64 or BSON::Int32' do
+      before do
+        collection.insert_one(int64: BSON::Int64.new(42))
+      end
+
+      it 'is stored as the correct type' do
+        result = collection.find(int64: { '$type' => 18 }).first
+        expect(result).not_to be_nil
+        expect(result['int64']).to eq(42)
+      end
+    end
+
+    context 'inserting an in32' do
+      before do
+        collection.insert_one(int32: BSON::Int32.new(42))
+      end
+
+      it 'is stored as the correct type' do
+        result = collection.find(int32: { '$type' => 16 }).first
+        expect(result).not_to be_nil
+        expect(result['int32']).to eq(42)
+      end
+    end
+
+    context 'with automatic encryption' do
+      require_libmongocrypt
+      require_enterprise
+      min_server_fcv '4.2'
+
+      include_context 'define shared FLE helpers'
+      include_context 'with local kms_providers'
+
+      let(:encrypted_collection) do
+        new_local_client(
+          SpecConfig.instance.addresses,
+          SpecConfig.instance.test_options.merge(
+            auto_encryption_options: {
+              kms_providers: kms_providers,
+              key_vault_namespace: key_vault_namespace,
+              schema_map: { 'auto_encryption.users' => schema_map },
+              # Spawn mongocryptd on non-default port for sharded cluster tests
+              extra_options: extra_options,
+            },
+            database: 'auto_encryption'
+          )
+        )['auto_encryption']
+      end
+
+      let(:collection) { authorized_client['auto_encryption'] }
+
+      context 'inserting an int64' do
+        before do
+          encrypted_collection.insert_one(ssn: '123-456-7890', int64: BSON::Int64.new(42))
+        end
+
+        it 'is stored as the correct type' do
+          result = collection.find(int64: { '$type' => 18 }).first
+          expect(result).not_to be_nil
+          expect(result['int64']).to eq(42)
+        end
+      end
+
+      context 'inserting an in32' do
+        before do
+          encrypted_collection.insert_one(ssn: '123-456-7890', int32: BSON::Int32.new(42))
+        end
+
+        it 'is stored as the correct type' do
+          result = collection.find(int32: { '$type' => 16 }).first
+          expect(result).not_to be_nil
+          expect(result['int32']).to eq(42)
+        end
+      end
+    end
+  end
+
   describe 'upsert' do
     context 'with default write concern' do
       it 'upserts' do
