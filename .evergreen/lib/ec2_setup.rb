d60517f2883594181f787b911ed96aface09d699
Oleg Pudeyev <oleg@bsdpower.com>
2020-04-08 22:52:18 -0400

    add diagnostics

--- .evergreen/lib/ec2_setup.rb
+++ .evergreen/lib/ec2_setup.rb
@@ -9,10 +9,12 @@ class Ec2Setup
       secret_access_key: ENV.fetch('MONGO_RUBY_DRIVER_AWS_AUTH_SECRET_ACCESS_KEY'),
     }
 
+    ip_arn = ENV.fetch('MONGO_RUBY_DRIVER_AWS_AUTH_INSTANCE_PROFILE_ARN')
+    puts "Setting instance profile to #{ip_arn} on #{Utils.ec2_instance_id}"
     orchestrator = AwsUtils::Orchestrator.new(**opts)
     orchestrator.set_instance_profile(Utils.ec2_instance_id,
       instance_profile_name: nil,
-      instance_profile_arn: ENV.fetch('MONGO_RUBY_DRIVER_AWS_AUTH_INSTANCE_PROFILE_ARN'),
+      instance_profile_arn: ip_arn,
     )
   end
 end
