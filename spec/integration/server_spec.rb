e987b5ca6cc8de04fb68998e11ce07f9100eab0f
Emily Giurleo <e.m.giurleo@gmail.com>
2020-04-22 17:41:13 -0400

    test that older server versions can perform read operations after being disconnected or marked unknown

--- spec/integration/server_spec.rb
+++ spec/integration/server_spec.rb
@@ -9,40 +9,56 @@ describe 'Server' do
   let(:view) { Mongo::Collection::View.new(collection) }
 
   describe 'operations when client/cluster are disconnected' do
-    # Server versions lower than 3.4 use the legacy find result
-    min_server_fcv '3.4'
+    shared_examples 'it performs read operations and receives the correct result type' do
+      context 'normal server' do
+        it 'can be used for reads' do
+          result = view.send(:send_initial_query, server)
+          expect(result).to be_a(result_class)
+        end
+      end
 
-    context 'normal server' do
-      it 'can be used for reads' do
-        result = view.send(:send_initial_query, server)
-        expect(result).to be_a(Mongo::Operation::Find::Result)
+      context 'known server in disconnected cluster' do
+        before do
+          client.close
+          expect(server).not_to be_unknown
+        end
+
+        it 'can be used for reads' do
+          result = view.send(:send_initial_query, server)
+          expect(result).to be_a(result_class)
+        end
+      end
+
+      context 'unknown server in disconnected cluster' do
+        before do
+          client.close
+          server.unknown!
+          expect(server).to be_unknown
+        end
+
+        it 'can be used for reads' do
+          result = view.send(:send_initial_query, server)
+          expect(result).to be_a(result_class)
+        end
       end
     end
 
-    context 'known server in disconnected cluster' do
-      before do
-        client.close
-        expect(server).not_to be_unknown
-      end
+    context 'for servers with FCV >= 3.4' do
+      min_server_fcv '3.2'
 
-      it 'can be used for reads' do
-        result = view.send(:send_initial_query, server)
-        expect(result).to be_a(Mongo::Operation::Find::Result)
-      end
+      let(:result_class) { Mongo::Operation::Find::Result }
+
+      it_behaves_like 'it performs read operations and receives the correct result type'
     end
 
-    context 'unknown server in disconnected cluster' do
-      before do
-        client.close
-        server.unknown!
-        expect(server).to be_unknown
-      end
+    context 'for servers with FCV < 3.4' do
+      # Find command was introduced in server version 3.2, so older versions should
+      # receive legacy result types.
+      max_server_fcv '3.0'
 
-      it 'can be used for reads' do
-        result = view.send(:send_initial_query, server)
-        # Driver falls back to the oldest MongoDB protocol
-        expect(result).to be_a(Mongo::Operation::Find::Result)
-      end
+      let(:result_class) { Mongo::Operation::Find::Legacy::Result }
+
+      it_behaves_like 'it performs read operations and receives the correct result type'
     end
   end
 end
