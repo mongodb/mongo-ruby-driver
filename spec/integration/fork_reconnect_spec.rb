04d050490dbf8b1c51d02d495b2ef069451f18e6
Oleg Pudeyev <oleg@bsdpower.com>
2020-05-19 22:04:08 -0400

    RUBY-2132 redo fork detection to not reconnect non-monitoring connections

--- spec/integration/fork_reconnect_spec.rb
+++ spec/integration/fork_reconnect_spec.rb
@@ -4,12 +4,20 @@ describe 'fork reconnect' do
   let(:client) { authorized_client }
   let(:server) { client.cluster.next_primary }
 
-  shared_examples 'reconnects the connection' do
+  describe 'monitoring connection' do
+    let(:connection) do
+      Mongo::Server::Monitor::Connection.new(server.address, server.options)
+    end
+
+    let(:operation) do
+      connection.ismaster.should be_a(Hash)
+    end
+
     it 'reconnects' do
       connection.connect!
 
       socket = connection.send(:socket).send(:socket)
-      socket.should be_a(Socket)
+      (socket.is_a?(Socket) || socket.is_a?(OpenSSL::SSL::SSLSocket)).should be true
 
       if pid = fork
         Process.wait(pid)
@@ -40,18 +48,6 @@ describe 'fork reconnect' do
     end
   end
 
-  describe 'monitoring connection' do
-    let(:connection) do
-      Mongo::Server::Monitor::Connection.new(server.address)
-    end
-
-    let(:operation) do
-      connection.ismaster.should be_a(Hash)
-    end
-
-    it_behaves_like 'reconnects the connection'
-  end
-
   describe 'non-monitoring connection' do
     let(:connection) do
       Mongo::Server::Connection.new(server)
@@ -61,7 +57,68 @@ describe 'fork reconnect' do
       connection.ping.should be true
     end
 
-    it_behaves_like 'reconnects the connection'
+    it 'does not reconnect' do
+      connection.connect!
+
+      socket = connection.send(:socket).send(:socket)
+      (socket.is_a?(Socket) || socket.is_a?(OpenSSL::SSL::SSLSocket)).should be true
+
+      if pid = fork
+        Process.wait(pid)
+        $?.exitstatus.should == 0
+      else
+        operation
+
+        child_socket = connection.send(:socket).send(:socket)
+        # fileno of child_socket may equal to fileno of socket,
+        # as socket would've been closed first and file descriptors can be
+        # reused by the kernel.
+        child_socket.object_id.should == socket.object_id
+
+        # Exec so that we do not close any clients etc. in the child.
+        exec('/bin/true')
+      end
+
+      # Connection should remain serviceable in the parent.
+      # The operation here will be invoked again, since the earlier invocation
+      # was in the child process.
+      operation
+
+      parent_socket = connection.send(:socket).send(:socket)
+      # fileno of child_socket may equal to fileno of socket,
+      # as socket would've been closed first and file descriptors can be
+      # reused by the kernel.
+      parent_socket.object_id.should == socket.object_id
+    end
+  end
+
+  describe 'connection pool' do
+
+    it 'creates a new connection in child' do
+      conn_id = server.with_connection do |connection|
+        connection.id
+      end
+
+      if pid = fork
+        Process.wait(pid)
+        $?.exitstatus.should == 0
+      else
+        new_conn_id = server.with_connection do |connection|
+          connection.id
+        end
+
+        new_conn_id.should_not == conn_id
+
+        # Exec so that we do not close any clients etc. in the child.
+        exec('/bin/true')
+      end
+
+      parent_conn_id = server.with_connection do |connection|
+        connection.id
+      end
+
+      parent_conn_id.should == conn_id
+    end
   end
 
   describe 'client' do
