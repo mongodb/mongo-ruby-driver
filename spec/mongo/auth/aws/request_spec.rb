cbbb45fd97bb505327e6d57c80e535160b826a8a
Neil Shweky <neilshweky@gmail.com>
2020-06-16 16:46:14 -0400

    RUBY-2279 fixed formatting and got rid of superfluous requires

--- spec/mongo/auth/aws/request_spec.rb
+++ spec/mongo/auth/aws/request_spec.rb
@@ -1,24 +1,23 @@
 require 'spec_helper'
-require 'json'
 
 describe Mongo::Auth::Aws::Request do 
 
-    describe "#formatted_time" do 
-        let(:request) do 
-            described_class.new(access_key_id: 'access_key_id', 
-                secret_access_key: 'secret_access_key', 
-                session_token: 'session_token', 
-                host: 'host', 
-                server_nonce: 'server_nonce'
-            )
-        end
-
-        it 'doesn\'t modify the time instance variable' do
-            original_time = request.time.dup
-            request.formatted_time
-            expect(request.time).to eq(original_time)
-            expect(request.time.strftime('%Z')).to eq(original_time.strftime('%Z'))
-
-        end
+  describe "#formatted_time" do 
+    let(:request) do 
+      described_class.new(access_key_id: 'access_key_id', 
+        secret_access_key: 'secret_access_key', 
+        session_token: 'session_token', 
+        host: 'host', 
+        server_nonce: 'server_nonce',
+        time: Time.now
+      )
     end
+
+    it 'doesn\'t modify the time instance variable' do
+      original_time = request.time.dup
+      request.formatted_time
+      expect(request.time).to eq(original_time)
+      expect(request.time.strftime('%Z')).to eq(original_time.strftime('%Z'))
+    end
+  end
 end
