037c079dc5342918eb2a3c5fbb8148113e7867bf
Neil Shweky <neilshweky@gmail.com>
2020-06-16 16:53:34 -0400

    RUBY-2279 change unit test to accept frozen time and assert no exception

--- spec/mongo/auth/aws/request_spec.rb
+++ spec/mongo/auth/aws/request_spec.rb
@@ -3,21 +3,19 @@ require 'spec_helper'
 describe Mongo::Auth::Aws::Request do 
 
   describe "#formatted_time" do 
+    original_time = Time.now.freeze
     let(:request) do 
       described_class.new(access_key_id: 'access_key_id', 
         secret_access_key: 'secret_access_key', 
         session_token: 'session_token', 
         host: 'host', 
         server_nonce: 'server_nonce',
-        time: Time.now
+        time: original_time
       )
     end
 
     it 'doesn\'t modify the time instance variable' do
-      original_time = request.time.dup
-      request.formatted_time
-      expect(request.time).to eq(original_time)
-      expect(request.time.strftime('%Z')).to eq(original_time.strftime('%Z'))
+      expect { request.formatted_time }.to_not raise_error
     end
   end
 end
