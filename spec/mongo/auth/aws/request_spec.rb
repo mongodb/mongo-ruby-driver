bcf66c4b661c7697aac20a6fc2824ed3e815e426
Neil Shweky <neilshweky@gmail.com>
2020-06-17 09:22:37 -0400

    RUBY-2279 added special/base case tests for formatted_time and signature; Added functionality test for formatted_time

--- spec/mongo/auth/aws/request_spec.rb
+++ spec/mongo/auth/aws/request_spec.rb
@@ -3,34 +3,74 @@ require 'spec_helper'
 describe Mongo::Auth::Aws::Request do 
 
   describe "#formatted_time" do 
-    original_time = Time.now.freeze
-    let(:request) do 
-      described_class.new(access_key_id: 'access_key_id', 
-        secret_access_key: 'secret_access_key', 
-        session_token: 'session_token', 
-        host: 'host', 
-        server_nonce: 'server_nonce',
-        time: original_time
-      )
+    context "when time is provided and frozen" do 
+      let(:original_time) { Time.at(1592399523).freeze }
+      let(:request) do 
+        described_class.new(access_key_id: 'access_key_id', 
+          secret_access_key: 'secret_access_key', 
+          session_token: 'session_token', 
+          host: 'host', 
+          server_nonce: 'server_nonce',
+          time: original_time
+        )
+      end
+
+      it 'doesn\'t modify the time instance variable' do
+        expect { request.formatted_time }.to_not raise_error
+      end
+
+      it 'returns the correct formatted time' do
+        expect(request.formatted_time).to eq('20200617T131203Z')
+      end
     end
 
-    it 'doesn\'t modify the time instance variable' do
-      expect { request.formatted_time }.to_not raise_error
+    context "when time is not provided" do 
+      let(:request) do 
+        described_class.new(access_key_id: 'access_key_id', 
+          secret_access_key: 'secret_access_key', 
+          session_token: 'session_token', 
+          host: 'host', 
+          server_nonce: 'server_nonce'
+        )
+      end
+
+      it 'doesn\'t raise an error on formatted_time' do
+        expect { request.formatted_time }.to_not raise_error
+      end
     end
   end
 
   describe "#signature" do 
-    let(:request) do 
-      described_class.new(access_key_id: 'access_key_id', 
-        secret_access_key: 'secret_access_key', 
-        session_token: 'session_token', 
-        host: 'host', 
-        server_nonce: 'server_nonce',
-      )
+    context "when time is provided and frozen" do 
+      let(:original_time) { Time.at(1592399523).freeze }
+      let(:request) do 
+        described_class.new(access_key_id: 'access_key_id', 
+          secret_access_key: 'secret_access_key', 
+          session_token: 'session_token', 
+          host: 'host', 
+          server_nonce: 'server_nonce',
+          time: original_time
+        )
+      end
+  
+      it 'doesn\'t raise error on signature' do 
+        expect { request.signature }.to_not raise_error
+      end
     end
 
-    it 'doesn\'t raise exception on signature' do 
-      expect { request.signature }.to_not raise_error
-    end
+    context "when time is not provided" do 
+      let(:request) do 
+        described_class.new(access_key_id: 'access_key_id', 
+          secret_access_key: 'secret_access_key', 
+          session_token: 'session_token', 
+          host: 'host', 
+          server_nonce: 'server_nonce'
+        )
+      end
+  
+      it 'doesn\'t raise error on signature' do 
+        expect { request.signature }.to_not raise_error
+      end
+    end    
   end
 end
