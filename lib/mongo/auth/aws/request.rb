dcd2347f3d2d532e5e2960b5cc9aa3d19961a2d6
Oleg Pudeyev <oleg@bsdpower.com>
2020-06-20 13:42:46 -0400

    Correctly autoload net/http

--- lib/mongo/auth/aws/request.rb
+++ lib/mongo/auth/aws/request.rb
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-autoload :Net, 'net/http'
+module Net
+  autoload :HTTP, 'net/http'
+end
 
 module Mongo
   module Auth
