cc8974a1abed73f916c4e2bebd7a206c0886cfd9
Neil Shweky <neilshweky@gmail.com>
2020-06-22 13:24:35 -0400

    RUBY-1657 added error message to timeouts

--- lib/mongo/socket/ssl.rb
+++ lib/mongo/socket/ssl.rb
@@ -137,7 +137,7 @@ module Mongo
       #
       # @since 2.0.0
       def connect!
-        Timeout.timeout(options[:connect_timeout], Error::SocketTimeoutError) do
+        Timeout.timeout(options[:connect_timeout], Error::SocketTimeoutError, 'the socket took too long to connect') do
           map_exceptions do
             @tcp_socket.connect(::Socket.pack_sockaddr_in(port, host))
           end
