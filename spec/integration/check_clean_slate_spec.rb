dd5e809c954693241f94c2fd9917f0be09a18d32
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2020-07-13 11:57:02 -0400

    RUBY-2138 background thread leak checker (#2006)
    * RUBY-2138 background thread leak checker
    
    * expand explanation
    
    Co-authored-by: Oleg Pudeyev <oleg@bsdpower.com>

---
+++ spec/integration/check_clean_slate_spec.rb
@@ -0,0 +1,16 @@
+require 'lite_spec_helper'
+
+# This test can be used to manually verify that there are no leaked
+# background threads - execute it after executing another test (in the same
+# rspec run) that is suspected to leak background threads, such as by
+# running:
+#
+# rspec your_spec.rb spec/integration/check_clean_slate_spec.rb
+
+describe 'Check clean slate' do
+  clean_slate_for_all_if_possible
+
+  it 'checks' do
+    # Nothing
+  end
+end
