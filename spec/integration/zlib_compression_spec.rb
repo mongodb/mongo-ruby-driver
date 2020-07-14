cee920f6c818f8287f915dc7cd9d8b8cd87e1716
Emily Giurleo <e.m.giurleo@gmail.com>
2020-07-14 17:16:01 -0400

    clean up integration test

--- spec/integration/zlib_compression_spec.rb
+++ spec/integration/zlib_compression_spec.rb
@@ -3,14 +3,23 @@ require 'spec_helper'
 describe 'Zlib compression' do
   require_compression
 
+  before do
+    authorized_client['test'].drop
+  end
+
   context 'when client has zlib compressor option enabled' do
     it 'compresses the message to the server' do
       # Double check that the client has zlib compression enabled
       expect(authorized_client.options[:compressors]).to include('zlib')
-      expect(Mongo::Protocol::Compressed).to receive(:new).and_call_original
-      expect_any_instance_of(Mongo::Protocol::Compressed).to receive(:serialize_fields).and_call_original
-      expect_any_instance_of(Mongo::Protocol::Compressed).to receive(:maybe_inflate).and_call_original
-      authorized_client['test'].insert_one(text: 'hello world')
+
+      expect(Mongo::Protocol::Compressed).to receive(:new).twice.and_call_original
+      expect(Zlib::Deflate).to receive(:deflate).twice.and_call_original
+      expect(Zlib::Inflate).to receive(:inflate).twice.and_call_original
+
+      authorized_client['test'].insert_one(_id: 1, text: 'hello world')
+      document = authorized_client['test'].find(_id: 1).first
+
+      expect(document['text']).to eq('hello world')
     end
   end
 end
