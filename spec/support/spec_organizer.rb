baf98839ef689519b77c77cbe40ebe32d9736cac
Oleg Pudeyev <oleg@bsdpower.com>
2020-07-13 20:58:55 -0400

    RUBY-2313 Run cursor reaping spec separately

--- spec/support/spec_organizer.rb
+++ spec/support/spec_organizer.rb
@@ -17,12 +17,16 @@ class SpecOrganizer
     [%r,^mongo,, :unit],
     [%r,^kerberos,, :unit],
     [%r,^integration/sdam_error_handling,, :sdam_integration],
+    [%r,^integration/cursor_reaping,, :cursor_reaping],
     [%r,^(atlas|integration),, :integration],
     [%r,^spec_tests,, :spec],
     [%r,^spec_tests/sdam_integration,, :spec_sdam_integration],
   ]
 
-  RUN_PRIORITY = %i(unit integration sdam_integration spec spec_sdam_integration)
+  RUN_PRIORITY = %i(unit
+    integration sdam_integration cursor_reaping
+    spec spec_sdam_integration
+  )
 
   SPEC_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
   ROOT = File.expand_path(File.join(SPEC_ROOT, '..'))
