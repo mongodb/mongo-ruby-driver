14d7ced75a4fb48d71fde20c536205567d8b19a3
Emily Giurleo <e.m.giurleo@gmail.com>
2020-07-28 17:26:24 -0400

    support the hidden option on index creation

--- spec/mongo/index/view_spec.rb
+++ spec/mongo/index/view_spec.rb
@@ -773,6 +773,76 @@ describe Mongo::Index::View do
       end
     end
 
+    context 'when providing hint option' do
+      let(:subscriber) { EventSubscriber.new }
+
+      let(:client) do
+        authorized_client.tap do |client|
+          client.subscribe(Mongo::Monitoring::COMMAND, subscriber)
+        end
+      end
+
+      let(:authorized_collection) { client['view-subscribed'] }
+
+      let(:indexes) do
+        authorized_collection.indexes.get('x_1')
+      end
+
+      let(:events) do
+        subscriber.command_started_events('createIndexes')
+      end
+
+      context 'on server versions < 4.4' do
+        max_server_fcv '4.2'
+
+        it 'raises an exception' do
+          expect do
+            view.create_one({ 'x' => 1 }, { hidden: true })
+          end.to raise_error(/The field 'hidden' is not valid for an index specification/)
+        end
+      end
+
+      context 'on server versions >= 4.4' do
+        min_server_fcv '4.4'
+
+        context 'when hidden is true' do
+          let!(:result) { view.create_one({ 'x' => 1 }, { hidden: true }) }
+
+          it 'returns ok' do
+            expect(result).to be_successful
+          end
+
+          it 'creates an index' do
+            expect(indexes).to_not be_nil
+          end
+
+          it 'passes the hint option to the server' do
+            expect(events.length).to eq(1)
+            command = events.first.command
+            expect(command['indexes'].first['hidden']).to be true
+          end
+        end
+
+        context 'when hidden is false' do
+          let!(:result) { view.create_one({ 'x' => 1 }, { hidden: false }) }
+
+          it 'returns ok' do
+            expect(result).to be_successful
+          end
+
+          it 'creates an index' do
+            expect(indexes).to_not be_nil
+          end
+
+          it 'passes the hint option to the server' do
+            expect(events.length).to eq(1)
+            command = events.first.command
+            expect(command['indexes'].first['hidden']).to be false
+          end
+        end
+      end
+    end
+
     context 'when providing commit_quorum option' do
       require_topology :replica_set, :sharded
       context 'on server versions >= 4.4' do
