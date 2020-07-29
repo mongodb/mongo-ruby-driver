63777d29d7076e94dc67785956b0617695a04ae1
Oleg Pudeyev <oleg@bsdpower.com>
2020-07-29 13:53:39 -0400

    RUBY-2342 Implement proper AWS auth region calculation

--- spec/mongo/auth/aws/request_spec.rb
+++ spec/mongo/auth/aws/request_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-describe Mongo::Auth::Aws::Request do 
+describe Mongo::Auth::Aws::Request do
 
-  describe "#formatted_time" do 
-    context "when time is provided and frozen" do 
+  describe "#formatted_time" do
+    context "when time is provided and frozen" do
       let(:original_time) { Time.at(1592399523).freeze }
-      let(:request) do 
-        described_class.new(access_key_id: 'access_key_id', 
-          secret_access_key: 'secret_access_key', 
-          session_token: 'session_token', 
-          host: 'host', 
+      let(:request) do
+        described_class.new(access_key_id: 'access_key_id',
+          secret_access_key: 'secret_access_key',
+          session_token: 'session_token',
+          host: 'host',
           server_nonce: 'server_nonce',
           time: original_time
         )
@@ -24,12 +24,12 @@ describe Mongo::Auth::Aws::Request do
       end
     end
 
-    context "when time is not provided" do 
-      let(:request) do 
-        described_class.new(access_key_id: 'access_key_id', 
-          secret_access_key: 'secret_access_key', 
-          session_token: 'session_token', 
-          host: 'host', 
+    context "when time is not provided" do
+      let(:request) do
+        described_class.new(access_key_id: 'access_key_id',
+          secret_access_key: 'secret_access_key',
+          session_token: 'session_token',
+          host: 'host',
           server_nonce: 'server_nonce'
         )
       end
@@ -40,37 +40,37 @@ describe Mongo::Auth::Aws::Request do
     end
   end
 
-  describe "#signature" do 
-    context "when time is provided and frozen" do 
+  describe "#signature" do
+    context "when time is provided and frozen" do
       let(:original_time) { Time.at(1592399523).freeze }
-      let(:request) do 
-        described_class.new(access_key_id: 'access_key_id', 
-          secret_access_key: 'secret_access_key', 
-          session_token: 'session_token', 
-          host: 'host', 
+      let(:request) do
+        described_class.new(access_key_id: 'access_key_id',
+          secret_access_key: 'secret_access_key',
+          session_token: 'session_token',
+          host: 'host',
           server_nonce: 'server_nonce',
           time: original_time
         )
       end
-  
-      it 'doesn\'t raise error on signature' do 
+
+      it 'doesn\'t raise error on signature' do
         expect { request.signature }.to_not raise_error
       end
     end
 
-    context "when time is not provided" do 
-      let(:request) do 
-        described_class.new(access_key_id: 'access_key_id', 
-          secret_access_key: 'secret_access_key', 
-          session_token: 'session_token', 
-          host: 'host', 
+    context "when time is not provided" do
+      let(:request) do
+        described_class.new(access_key_id: 'access_key_id',
+          secret_access_key: 'secret_access_key',
+          session_token: 'session_token',
+          host: 'host',
           server_nonce: 'server_nonce'
         )
       end
-  
-      it 'doesn\'t raise error on signature' do 
+
+      it 'doesn\'t raise error on signature' do
         expect { request.signature }.to_not raise_error
       end
-    end    
+    end
   end
 end
