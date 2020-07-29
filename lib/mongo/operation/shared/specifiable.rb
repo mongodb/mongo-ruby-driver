7886eca7b0841d31a960da88f9f46e2141396ab4
Emily Giurleo <e.m.giurleo@gmail.com>
2020-07-29 15:16:16 -0400

    revert unnecessary change

--- lib/mongo/operation/shared/specifiable.rb
+++ lib/mongo/operation/shared/specifiable.rb
@@ -225,10 +225,6 @@ module Mongo
         spec[DOCUMENTS]
       end
 
-      def command_documents
-        documents || deletes || updates
-      end
-
       # The name of the collection to which the operation should be sent.
       #
       # @example Get the collection name.
