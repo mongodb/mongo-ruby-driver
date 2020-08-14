eb798e26367190007d73d84920a7a390272c0a09
Emily Giurleo <e.m.giurleo@gmail.com>
2020-08-14 10:58:50 -0400

    RUBY-1131 add a test that split bulk writes dont produce command failed event

--- spec/integration/bulk_write_spec.rb
+++ spec/integration/bulk_write_spec.rb
@@ -16,4 +16,35 @@ describe 'Bulk writes' do
       end.not_to raise_error
     end
   end
+
+  context 'when bulk write needs to be split' do
+    let(:subscriber) { EventSubscriber.new }
+
+    let(:max_bson_size) { Mongo::Server::ConnectionBase::DEFAULT_MAX_BSON_OBJECT_SIZE }
+
+    let(:insert_events) do
+      subscriber.command_started_events('insert')
+    end
+
+    let(:failed_events) do
+      subscriber.failed_events
+    end
+
+    let(:operations) do
+      [{ insert_one: { text: 'a' * (max_bson_size/2) } }] * 3
+    end
+
+    before do
+      authorized_client.subscribe(Mongo::Monitoring::COMMAND, subscriber)
+      authorized_collection.bulk_write(operations) 
+    end
+
+    it 'splits the operations' do
+      expect(insert_events.length).to eq(2)
+    end
+
+    it 'does not have a command failed event' do
+      expect(failed_events).to be_empty
+    end
+  end
 end
