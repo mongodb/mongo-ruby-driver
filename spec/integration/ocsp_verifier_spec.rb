cfa2f933041866c0bc075f8e8da2b3dc1235a359
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2020-08-21 13:47:44 -0400

    RUBY-2283 Test OCSP with ECDSA certificates and delegate responders (#2071)
    * RUBY-2283 test delegate cert with rsa
    
    * RUBY-2283 test ecdsa certs
    
    Co-authored-by: Oleg Pudeyev <oleg@bsdpower.com>

--- spec/integration/ocsp_verifier_spec.rb
+++ spec/integration/ocsp_verifier_spec.rb
@@ -3,87 +3,100 @@ require 'lite_spec_helper'
 describe Mongo::Socket::OcspVerifier do
   require_ocsp
 
-  let(:cert_path) { SpecConfig.instance.ocsp_files_dir.join('rsa/server.pem') }
-  let(:ca_cert_path) { SpecConfig.instance.ocsp_files_dir.join('rsa/ca.pem') }
+  %w(rsa ecdsa).each do |algorithm|
+    context "when using #{algorithm} cert" do
+      let(:cert_path) { SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/server.pem") }
+      let(:ca_cert_path) { SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/ca.pem") }
 
-  let(:cert) { OpenSSL::X509::Certificate.new(File.read(cert_path)) }
-  let(:ca_cert) { OpenSSL::X509::Certificate.new(File.read(ca_cert_path)) }
+      let(:cert) { OpenSSL::X509::Certificate.new(File.read(cert_path)) }
+      let(:ca_cert) { OpenSSL::X509::Certificate.new(File.read(ca_cert_path)) }
 
-  let(:verifier) do
-    described_class.new('foo', cert, ca_cert, timeout: 3)
-  end
+      let(:verifier) do
+        described_class.new('foo', cert, ca_cert, timeout: 3)
+      end
 
-  context 'responder not responding' do
-    it 'returns false but does not raise' do
-      verifier.verify.should be false
-    end
+      context 'responder not responding' do
+        it 'returns false but does not raise' do
+          verifier.verify.should be false
+        end
 
-    it 'does not wait for the timeout' do
-      # Loopback interface should be refusing connections, which will make
-      # the operation complete quickly.
-      lambda do
-        verifier.verify
-      end.should take_shorter_than 3
-    end
-  end
+        it 'does not wait for the timeout' do
+          # Loopback interface should be refusing connections, which will make
+          # the operation complete quickly.
+          lambda do
+            verifier.verify
+          end.should take_shorter_than 3
+        end
+      end
 
-  context 'good response' do
-    with_ocsp_mock(
-      SpecConfig.instance.ocsp_files_dir.join('rsa/ca.pem'),
-      SpecConfig.instance.ocsp_files_dir.join('rsa/ca.crt'),
-      SpecConfig.instance.ocsp_files_dir.join('rsa/ca.key'),
-    )
+      %w(ca delegate).each do |responder_cert|
+        responder_cert_file_name = {
+          'ca' => 'ca',
+          'delegate' => 'ocsp-responder',
+        }.fetch(responder_cert)
 
-    it 'verifies' do
-      verifier.verify.should be true
-    end
+        context "when responder uses #{responder_cert} cert" do
+          context 'good response' do
+            with_ocsp_mock(
+              SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/ca.pem"),
+              SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/#{responder_cert_file_name}.crt"),
+              SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/#{responder_cert_file_name}.key"),
+            )
 
-    it 'does not wait for the timeout' do
-      lambda do
-        verifier.verify
-      end.should take_shorter_than 3
-    end
-  end
+            it 'verifies' do
+              verifier.verify.should be true
+            end
 
-  context 'revoked response' do
-    with_ocsp_mock(
-      SpecConfig.instance.ocsp_files_dir.join('rsa/ca.pem'),
-      SpecConfig.instance.ocsp_files_dir.join('rsa/ca.crt'),
-      SpecConfig.instance.ocsp_files_dir.join('rsa/ca.key'),
-      'revoked'
-    )
+            it 'does not wait for the timeout' do
+              lambda do
+                verifier.verify
+              end.should take_shorter_than 3
+            end
+          end
 
-    it 'raises an exception' do
-      lambda do
-        verifier.verify
-      end.should raise_error(Mongo::Error::ServerCertificateRevoked, %r,TLS certificate of 'foo' has been revoked according to 'http://localhost:8100/status',)
-    end
+          context 'revoked response' do
+            with_ocsp_mock(
+              SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/ca.pem"),
+              SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/#{responder_cert_file_name}.crt"),
+              SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/#{responder_cert_file_name}.key"),
+              'revoked'
+            )
 
-    it 'does not wait for the timeout' do
-      lambda do
-        lambda do
-          verifier.verify
-        end.should raise_error(Mongo::Error::ServerCertificateRevoked)
-      end.should take_shorter_than 3
-    end
-  end
+            it 'raises an exception' do
+              lambda do
+                verifier.verify
+              end.should raise_error(Mongo::Error::ServerCertificateRevoked, %r,TLS certificate of 'foo' has been revoked according to 'http://localhost:8100/status',)
+            end
 
-  context 'unknown response' do
-    with_ocsp_mock(
-      SpecConfig.instance.ocsp_files_dir.join('rsa/ca.pem'),
-      SpecConfig.instance.ocsp_files_dir.join('rsa/ca.crt'),
-      SpecConfig.instance.ocsp_files_dir.join('rsa/ca.key'),
-      'unknown',
-    )
+            it 'does not wait for the timeout' do
+              lambda do
+                lambda do
+                  verifier.verify
+                end.should raise_error(Mongo::Error::ServerCertificateRevoked)
+              end.should take_shorter_than 3
+            end
+          end
 
-    it 'does not verify and does not raise an exception' do
-      verifier.verify.should be false
-    end
+          context 'unknown response' do
+            with_ocsp_mock(
+              SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/ca.pem"),
+              SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/#{responder_cert_file_name}.crt"),
+              SpecConfig.instance.ocsp_files_dir.join("#{algorithm}/#{responder_cert_file_name}.key"),
+              'unknown',
+            )
 
-    it 'does not wait for the timeout' do
-      lambda do
-        verifier.verify
-      end.should take_shorter_than 3
+            it 'does not verify and does not raise an exception' do
+              verifier.verify.should be false
+            end
+
+            it 'does not wait for the timeout' do
+              lambda do
+                verifier.verify
+              end.should take_shorter_than 3
+            end
+          end
+        end
+      end
     end
   end
 end
