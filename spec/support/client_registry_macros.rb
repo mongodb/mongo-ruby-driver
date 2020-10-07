7ffee9bddb0162e1a9bb6d45a5c234ba5262829f
Oleg Pudeyev <oleg@bsdpower.com>
2020-10-07 12:29:14 -0400

    redo options type conversions once again

--- spec/support/client_registry_macros.rb
+++ spec/support/client_registry_macros.rb
@@ -5,24 +5,14 @@ module ClientRegistryMacros
 
   def new_local_client_nmio(address, options=nil)
     # Avoid type converting options.
-    base_options = if Hash === options
-      if options['monitoring_io']
-        {'monitoring_io' => false}
-      else
-        {monitoring_io: false}
-      end
-    else
-      {monitoring_io: false}
+    base_options = {monitoring_io: false}
+    if BSON::Document === options || options&.keys&.any? { |key| String === key }
+      base_options = Mongo::Options::Redacted.new(base_options)
     end
-    if options
-      cls = if options.keys.any? { |key| String === key }
-        Mongo::Options::Redacted
-      else
-        options.class
-      end
-      options = cls.new(base_options).update(options)
+    options = if options
+      base_options.merge(options)
     else
-      options = base_options
+      base_options
     end
     new_local_client(address, options)
   end
