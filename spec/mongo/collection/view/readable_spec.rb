80c20d8a7b19c72036024882993b3d6039d548c0
Katie Sadoff <katie.sadoff@mongodb.com>
2020-10-16 10:28:12 -0400

    estimated_document_count raises error when called with filter, added unit tests

--- spec/mongo/collection/view/readable_spec.rb
+++ spec/mongo/collection/view/readable_spec.rb
@@ -305,7 +305,6 @@ describe Mongo::Collection::View::Readable do
     end
 
     context 'when incorporating read concern' do
-
       let(:result) do
         new_view.count(options)
       end
@@ -329,7 +328,6 @@ describe Mongo::Collection::View::Readable do
     end
 
     context 'when no selector is provided' do
-
       it 'returns the count of matching documents' do
         expect(view.count).to eq(10)
       end
@@ -598,6 +596,41 @@ describe Mongo::Collection::View::Readable do
             }.to raise_exception(Mongo::Error::UnsupportedCollation)
           end
         end
+      end
+    end
+  end
+
+  describe "#estimated_document_count" do
+
+   let(:documents) do
+      (1..10).map{ |i| { field: "test#{i}" }}
+   end
+
+   before do
+    authorized_collection.delete_many
+    authorized_collection.insert_many(documents)
+   end
+
+    let(:result) do
+      view.estimated_document_count(options)
+    end
+
+    context 'when a selector is provided' do
+
+      let(:selector) do
+        { field: 'test1' }
+      end
+
+      it 'raises an error' do
+        expect {
+          result
+        }.to raise_error(ArgumentError, "Cannot call estimated_document_count when querying with a filter")
+      end
+    end
+
+    context 'when no selector is provided' do
+      it 'returns the estimated count of matching documents' do
+        expect(view.estimated_document_count).to eq(10)
       end
     end
   end
