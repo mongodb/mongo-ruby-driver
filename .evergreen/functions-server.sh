536ff81830b41eb730be66f5616d0181b819769d
Oleg Pudeyev <oleg@bsdpower.com>
2020-11-26 21:20:08 -0500

    fix virtualenv reference

--- .evergreen/functions-server.sh
+++ .evergreen/functions-server.sh
@@ -59,7 +59,7 @@ install_mlaunch_virtualenv() {
   python2 -V || true
   python -m pip install virtualenv --user
   venvpath="$MONGO_ORCHESTRATION_HOME"/venv
-  virtualenv -p python2 $venvpath
+  python2 -m virtualenv -p python2 $venvpath
   . $venvpath/bin/activate
   pip install 'mtools-legacy[mlaunch]'
 }
