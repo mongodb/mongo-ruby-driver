d4f5f9875354dbf8d0b77d75ce104e709c8f9f7d
Oleg Pudeyev <oleg@bsdpower.com>
2020-11-26 21:57:08 -0500

    install zipp?

--- .evergreen/functions-server.sh
+++ .evergreen/functions-server.sh
@@ -57,10 +57,8 @@ prepare_server_from_url() {
 
 install_mlaunch_virtualenv() {
   python2 -V || true
-  python -m pip install virtualenv --user
+  python -m pip install virtualenv zipp --user
   venvpath="$MONGO_ORCHESTRATION_HOME"/venv
-  # https://github.com/pypa/virtualenv/issues/1630
-  python2 -m virtualenv -p python2 $venvpath
   . $venvpath/bin/activate
   pip install pip --upgrade
   pip install 'mtools-legacy[mlaunch]'
