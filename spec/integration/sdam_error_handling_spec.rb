d92d14b36712ac8dc34ed2bf949dfbb6b56368a8
Oleg Pudeyev <oleg@bsdpower.com>
2020-11-27 16:59:06 -0500

    close all clients for every test case to avoid background clients racing with the intended client for ismaster fail points

--- spec/integration/sdam_error_handling_spec.rb
+++ spec/integration/sdam_error_handling_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'SDAM error handling' do
-  clean_slate_for_all
+  clean_slate
 
   after do
     # Close all clients after every test to avoid leaking expectations into
@@ -401,7 +401,6 @@ describe 'SDAM error handling' do
             closeConnection: true,
           },
         )
-        sleep 0.5
       end
 
       let(:operation) do
