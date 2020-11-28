2d11f5a453bb8a29d7848938c5123b3e1752cda8
Oleg Pudeyev <oleg@bsdpower.com>
2020-11-27 20:53:49 -0500

    close all clients created by spec setup

--- spec/support/spec_setup.rb
+++ spec/support/spec_setup.rb
@@ -8,38 +8,48 @@ class SpecSetup
       return
     end
 
-    # Create the root user administrator as the first user to be added to the
-    # database. This user will need to be authenticated in order to add any
-    # more users to any other databases.
+    admin_unauthorized_client
     begin
-      create_user(admin_unauthorized_client, SpecConfig.instance.root_user)
-    rescue Mongo::Error::OperationFailure => e
-      # When testing a cluster that requires auth, root user is already set up
-      # and it is not creatable without auth.
-      # Seems like every mongodb version has its own error message
-      # for trying to make a user when not authenticated,
-      # and prior to 4.0 or so the codes are supposedly not reliable either.
-      # In order: 4.0, 3.6, 3.4 through 2.6
-      if e.message =~ /command createUser requires authentication|there are no users authenticated|not authorized on admin to execute command.*createUser/
-        # However, if the cluster is configured to require auth but
-        # test suite has wrong credentials, then admin_authorized_test_client
-        # won't be authenticated and the following line will raise an
-        # exception
-        if admin_authorized_test_client.with(database: 'admin').database.users.info(SpecConfig.instance.root_user.name).any?
-          warn "Skipping root user creation, likely auth is enabled on cluster"
+      # Create the root user administrator as the first user to be added to the
+      # database. This user will need to be authenticated in order to add any
+      # more users to any other databases.
+      begin
+        create_user(admin_unauthorized_client, SpecConfig.instance.root_user)
+      rescue Mongo::Error::OperationFailure => e
+        # When testing a cluster that requires auth, root user is already set up
+        # and it is not creatable without auth.
+        # Seems like every mongodb version has its own error message
+        # for trying to make a user when not authenticated,
+        # and prior to 4.0 or so the codes are supposedly not reliable either.
+        # In order: 4.0, 3.6, 3.4 through 2.6
+        if e.message =~ /command createUser requires authentication|there are no users authenticated|not authorized on admin to execute command.*createUser/
+          # However, if the cluster is configured to require auth but
+          # test suite has wrong credentials, then admin_authorized_test_client
+          # won't be authenticated and the following line will raise an
+          # exception
+          if admin_authorized_test_client.use('admin').database.users.info(SpecConfig.instance.root_user.name).any?
+            warn "Skipping root user creation, likely auth is enabled on cluster"
+          else
+            raise
+          end
         else
           raise
         end
-      else
-        raise
       end
+    ensure
+      admin_unauthorized_client.close
+      ClientRegistry.instance.global_client('admin_unauthorized').close
     end
-    admin_unauthorized_client.close
 
-    # Adds the test user to the test database with permissions on all
-    # databases that will be used in the test suite.
-    create_user(admin_authorized_test_client, SpecConfig.instance.test_user)
-    admin_authorized_test_client.close
+    admin_authorized_test_client
+    begin
+      # Adds the test user to the test database with permissions on all
+      # databases that will be used in the test suite.
+      create_user(admin_authorized_test_client, SpecConfig.instance.test_user)
+    ensure
+      admin_authorized_test_client.close
+      ClientRegistry.instance.global_client('root_authorized').close
+    end
   end
 
   def create_user(client, user)
