4c2131e3af189a8ad66e4d5f1f1074ba37c28155
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2020-12-01 11:53:45 -0500

    RUBY-2446 Fix intermittent SDAM test failures (#2145)
    * remove comment
    
    * try sleeping after setting fail points
    
    * close all clients for every test case to avoid background clients racing with the intended client for ismaster fail points
    
    * relax server marked unknown event count requirements to account for our implementation
    
    * fix condition
    
    * fix conditions
    
    * close all clients created by spec setup
    
    * redo spec setup to not remove user specified in the uri
    
    Co-authored-by: Oleg Pudeyev <oleg@bsdpower.com>

--- spec/integration/sdam_events_spec.rb
+++ spec/integration/sdam_events_spec.rb
@@ -67,10 +67,8 @@ describe 'SDAM events' do
         started_events.length.should <= 10
 
         succeeded_events = subscriber.select_succeeded_events(Mongo::Monitoring::Event::ServerHeartbeatSucceeded)
-        # Since we gracefully close the client, we expect each heartbeat
-        # to complete.
         started_events.length.should > 1
-        (succeeded_events.length-1..succeeded_events.length).should include(started_events.length)
+        (succeeded_events.length..succeeded_events.length+1).should include(started_events.length)
       end
     end
 
@@ -109,9 +107,9 @@ describe 'SDAM events' do
         # There may be in-flight ismasters that don't complete, both
         # regular and awaited.
         started_awaited.length.should > 1
-        (succeeded_awaited.length-1..succeeded_awaited.length).should include(started_awaited.length)
+        (succeeded_awaited.length..succeeded_awaited.length+1).should include(started_awaited.length)
         started_regular.length.should > 1
-        (succeeded_regular.length-1..succeeded_regular.length).should include(started_regular.length)
+        (succeeded_regular.length..succeeded_regular.length+1).should include(started_regular.length)
       end
     end
   end
