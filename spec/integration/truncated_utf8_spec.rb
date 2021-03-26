6f92983f278c8ce802133a7d02b7b6f6712c34f5
Oleg Pudeyev <oleg@bsdpower.com>
2021-03-26 09:00:58 -0400

    RUBY-2560 test for truncated utf-8 being received from server

---
+++ spec/integration/truncated_utf8_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe 'truncated UTF-8 in server error messages' do
+  let(:rep) do
+    '(╯°□°)╯︵ ┻━┻'
+  end
+
+  let(:collection) do
+    authorized_client['truncated_utf8']
+  end
+
+  before(:all) do
+    ClientRegistry.instance.global_client('authorized')['truncated_utf8'].indexes.create_one(
+      {k: 1}, unique: true)
+  end
+
+  it 'works' do
+    pending 'RUBY-2560'
+
+    collection.insert_one(k: rep*20)
+    collection.insert_one(k: rep*20)
+  end
+end
