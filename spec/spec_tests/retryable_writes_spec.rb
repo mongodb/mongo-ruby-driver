9c388b6258d8f751552901660c1f226a570624c6
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2021-05-06 15:46:52 -0400

    Skip retryable writes tests when modern retryable writes are disabled globally (#2223)
    Signed-off-by: Oleg Pudeyev <oleg@bsdpower.com>
    
    Co-authored-by: Oleg Pudeyev <oleg@bsdpower.com>

--- spec/spec_tests/retryable_writes_spec.rb
+++ spec/spec_tests/retryable_writes_spec.rb
@@ -9,14 +9,14 @@ describe 'Retryable writes spec tests' do
   require_wired_tiger
   require_no_multi_shard
 
+  # Do not run these tests when write retries are disabled globally -
+  # the tests won't work in that case and testing them with retries enabled
+  # is simply redundant.
+  require_retry_writes
+
   define_crud_spec_tests(RETRYABLE_WRITES_TESTS) do |spec, req, test|
     let(:client) do
-      # Evergreen allows a RETRY_WRITES environment variable to be specified,
-      # which determines whether authorized_client has retry_writes enabled by default.
-      # The line below ensures that the retryable_write spec tests still work,
-      # even if retryable writes was initially disabled for this client, by
-      # enabling retry_writes by default, which is the standard driver behavior.
-      authorized_client.with({ retry_writes: true }.merge(test.client_options))
+      authorized_client.with(test.client_options)
     end
   end
 end
