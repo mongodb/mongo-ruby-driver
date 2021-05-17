ba43278005bb64336a8135086fee67e95640efd8
Oleg Pudeyev <oleg@bsdpower.com>
2021-05-17 12:58:22 -0400

    docker

---
+++ .evergreen/run-tests-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if echo "$AUTH" |grep -q ^aws; then
+  # Do not set -x as this will expose passwords in Evergreen logs
+  set +x
+else
+  set -x
+fi
+
+params=
+for var in MONGODB_VERSION RVM_RUBY \
+  OCSP_ALGORITHM OCSP_STATUS OCSP_DELEGATE OCSP_MUST_STAPLE \
+  OCSP_CONNECTIVITY OCSP_VERIFIER
+do
+  params="$params $var=${!var}"
+done
+
+./.evergreen/test-on-docker -d ubuntu1604 $params
