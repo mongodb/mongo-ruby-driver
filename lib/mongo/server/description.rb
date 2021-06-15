230872c5b68c131c79a8f01490d94cee297af602
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2021-06-15 12:26:30 -0400

    RUBY-2679 Stop duplicating Time instances (#2273)
    Co-authored-by: Oleg Pudeyev <code@olegp.name>

--- lib/mongo/server/description.rb
+++ lib/mongo/server/description.rb
@@ -217,7 +217,7 @@ module Mongo
         @config = config
         @features = Features.new(wire_versions, me || @address.to_s)
         @average_round_trip_time = average_round_trip_time
-        @last_update_time = Time.now.dup.freeze
+        @last_update_time = Time.now.freeze
         @last_update_monotime = Utils.monotonic_time
 
         if Mongo::Lint.enabled?
