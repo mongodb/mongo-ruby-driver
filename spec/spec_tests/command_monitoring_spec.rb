f6822318db77800fa789a1082bdcdb059dee7b7e
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2021-06-30 12:45:20 -0400

    RUBY-2220 add file name context to command monitoring spec (#2288)
    Co-authored-by: Oleg Pudeyev <code@olegp.name>

--- spec/spec_tests/command_monitoring_spec.rb
+++ spec/spec_tests/command_monitoring_spec.rb
@@ -9,40 +9,42 @@ require 'runners/command_monitoring'
 describe 'Command Monitoring Events' do
 
   COMMAND_MONITORING_TESTS.each do |file|
+    context File.basename(file) do
 
-    spec = Mongo::CommandMonitoring::Spec.new(file)
+      spec = Mongo::CommandMonitoring::Spec.new(file)
 
-    spec.tests.each do |test|
-      context(test.description) do
+      spec.tests.each do |test|
+        context(test.description) do
 
-        if test.min_server_fcv
-          min_server_fcv test.min_server_fcv
-        end
-        if test.max_server_version
-          max_server_version test.max_server_version
-        end
+          if test.min_server_fcv
+            min_server_fcv test.min_server_fcv
+          end
+          if test.max_server_version
+            max_server_version test.max_server_version
+          end
 
-        let(:subscriber) do
-          EventSubscriber.new
-        end
+          let(:subscriber) do
+            EventSubscriber.new
+          end
 
-        let(:monitoring) do
-          authorized_client.send(:monitoring)
-        end
+          let(:monitoring) do
+            authorized_client.send(:monitoring)
+          end
 
-        before do
-          authorized_collection.find.delete_many
-          authorized_client.subscribe(Mongo::Monitoring::COMMAND, subscriber)
-        end
+          before do
+            authorized_collection.find.delete_many
+            authorized_client.subscribe(Mongo::Monitoring::COMMAND, subscriber)
+          end
 
-        test.expectations.each_with_index do |expectation, index|
+          test.expectations.each_with_index do |expectation, index|
 
-          it "generates a #{expectation.event_name} for #{expectation.command_name}" do
-            begin
-              test.run(authorized_collection, subscriber)
-              check_event(subscriber, index, expectation)
-            rescue Mongo::Error::OperationFailure, Mongo::Error::BulkWriteError
-              check_event(subscriber, index, expectation)
+            it "generates a #{expectation.event_name} for #{expectation.command_name}" do
+              begin
+                test.run(authorized_collection, subscriber)
+                check_event(subscriber, index, expectation)
+              rescue Mongo::Error::OperationFailure, Mongo::Error::BulkWriteError
+                check_event(subscriber, index, expectation)
+              end
             end
           end
         end
