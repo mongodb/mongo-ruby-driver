fdd608ab034955a984399b02addded052eabaca0
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2021-09-17 13:05:58 -0400

    RUBY-2587 store errors as failures when only failure reporting is requested (#2352)
    Co-authored-by: Oleg Pudeyev <code@olegp.name>

--- spec/runners/unified/support_operations.rb
+++ spec/runners/unified/support_operations.rb
@@ -211,11 +211,17 @@ module Unified
           rescue Interrupt
             raise
           rescue => e
-            if store_errors
+            if store_failures
+              STDERR.puts "Error: #{e.class}: #{e} (reporting as failure)"
+              entities.get(:failure_list, store_failures) << {
+                error: "#{e.class}: #{e}",
+                time: Time.now.to_f,
+              }
+            elsif store_errors
               STDERR.puts "Error: #{e.class}: #{e}"
               entities.get(:error_list, store_errors) << {
                 error: "#{e.class}: #{e}",
-                observedAt: Time.now.to_f,
+                time: Time.now.to_f,
               }
             else
               raise
