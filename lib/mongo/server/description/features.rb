198b258e268e76fcd170c0b3aad93d84f0f1eaea
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2021-10-11 09:51:32 +0200

    RUBY-2747 Set minWireVersion to 6 (#2355)

--- lib/mongo/server/description/features.rb
+++ lib/mongo/server/description/features.rb
@@ -78,7 +78,7 @@ module Mongo
         # The wire protocol versions that this version of the driver supports.
         #
         # @since 2.0.0
-        DRIVER_WIRE_VERSIONS = (2..13).freeze
+        DRIVER_WIRE_VERSIONS = (6..13).freeze
 
         # Create the methods for each mapping to tell if they are supported.
         #
