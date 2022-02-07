c5ce08a4b86d805f4249299b79b71686b2a49fb7
Neil Shweky <neilshweky@gmail.com>
2022-02-07 15:26:03 -0500

    RUBY-2820 add logging for CI [UNDO ME]

--- spec/mongo/client_spec.rb
+++ spec/mongo/client_spec.rb
@@ -947,6 +947,7 @@ describe Mongo::Client do
           end
 
           it 'uses the same implicit session' do
+            puts events unless events.map { |e| e.command['lsid'] }.uniq.count == 1
             expect(
               events.map { |e| e.command['lsid'] }.uniq.count
             ).to eq 1
