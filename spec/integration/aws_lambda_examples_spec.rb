b8f5e28f405b80dcc993dd6122c6d574c426d34e
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2022-04-20 18:25:36 +0200

    RUBY-2945 Add AWS Lambda examples

---
+++ spec/integration/aws_lambda_examples_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+# encoding: utf-8
+
+require "spec_helper"
+
+describe "AWS Lambda examples in Ruby" do
+  require_aws_auth
+
+  it "shares the client" do
+    # Start AWS Lambda Example 1
+
+    # Import the dependency.
+    require "mongo"
+
+    # Create a Mongo::Client instance.
+    # CRITICAL: You must create client outside the handler so that the client
+    # can be reused across function invocations.
+    client = Mongo::Client.new(ENV["MONGODB_URI"])
+
+    def lambda_handler(event:, context:)
+      # Use the client to return the name of the connected database.
+      client.database.name
+    end
+
+    # End AWS Lambda Example 1
+  end
+
+  it "to the deployment using AWS IAM authentication" do
+    allow(ENV).to receive(:[]).and_call_original
+    allow(ENV).to receive(:[]).with("MONGODB_HOST").and_return("localhost:2017")
+    allow(ENV).to receive(:[]).with("MONGODB_DATABASE").and_return("test")
+
+    # Start AWS Lambda Example 2
+
+    # Import the dependency.
+    require "mongo"
+
+    # Create a Mongo::Client instance.
+    # CRITICAL: You must create client outside the handler so that the client
+    # can be reused across function invocations.
+    client = Mongo::Client.new([ENV["MONGODB_HOST"]],
+                               auth_mech: :aws,
+                               user: ENV["MONGO_RUBY_DRIVER_AWS_AUTH_ACCESS_KEY_ID"],
+                               password: ENV["MONGO_RUBY_DRIVER_AWS_AUTH_SECRET_ACCESS_KEY"],
+                               auth_mech_properties: {
+                                 aws_session_token: ENV["MONGO_RUBY_DRIVER_AWS_AUTH_SESSION_TOKEN"],
+                               },
+                               database: ENV["MONGODB_DATABASE"])
+
+    def lambda_handler(event:, context:)
+      # Use the client to return the name of the connected database.
+      client.database.name
+    end
+
+    # End AWS Lambda Example 2
+  end
+end
