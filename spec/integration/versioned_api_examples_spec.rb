c1342483822ad474881f608c63415a2c19f6c0af
Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
2022-04-22 16:32:54 -0400

    RUBY-2947 test on 5.2 and 6.0 standalones (#2458)
    * RUBY-2947 test on 5.2 and 6.0 standalones
    
    * RUBY-2922 do not run count example on 6.0
    
    * omit auth+ssl for now
    
    * reference ticket
    
    Co-authored-by: Oleg Pudeyev <code@olegp.name>

--- spec/integration/versioned_api_examples_spec.rb
+++ spec/integration/versioned_api_examples_spec.rb
@@ -68,48 +68,53 @@ describe 'Versioned API examples' do
     client.close
   end
 
-  it "Versioned API Strict Migration Example" do
-    client = Mongo::Client.new(uri_string, server_api: {version: "1", strict: true})
-    client[:sales].drop
+  # See also RUBY-2922 for count in versioned api v1.
+  context 'servers that exclude count from versioned api' do
+    max_server_version '5.2'
 
-    # Start Versioned API Example 5
+    it "Versioned API Strict Migration Example" do
+      client = Mongo::Client.new(uri_string, server_api: {version: "1", strict: true})
+      client[:sales].drop
 
-    client[:sales].insert_many([
-      { _id: 1, item: "abc", price: 10, quantity: 2,  date: DateTime.parse("2021-01-01T08:00:00Z") },
-      { _id: 2, item: "jkl", price: 20, quantity: 1,  date: DateTime.parse("2021-02-03T09:00:00Z") },
-      { _id: 3, item: "xyz", price: 5,  quantity: 5,  date: DateTime.parse("2021-02-03T09:05:00Z") },
-      { _id: 4, item: "abc", price: 10, quantity: 10, date: DateTime.parse("2021-02-15T08:00:00Z") },
-      { _id: 5, item: "xyz", price: 5,  quantity: 10, date: DateTime.parse("2021-02-15T09:05:00Z") },
-      { _id: 6, item: "xyz", price: 5,  quantity: 5,  date: DateTime.parse("2021-02-15T12:05:10Z") },
-      { _id: 7, item: "xyz", price: 5,  quantity: 10, date: DateTime.parse("2021-02-15T14:12:12Z") },
-      { _id: 8, item: "abc", price: 10, quantity: 5,  date: DateTime.parse("2021-03-16T20:20:13Z") }
-    ])
+      # Start Versioned API Example 5
 
-    # End Versioned API Example 5
+      client[:sales].insert_many([
+        { _id: 1, item: "abc", price: 10, quantity: 2,  date: DateTime.parse("2021-01-01T08:00:00Z") },
+        { _id: 2, item: "jkl", price: 20, quantity: 1,  date: DateTime.parse("2021-02-03T09:00:00Z") },
+        { _id: 3, item: "xyz", price: 5,  quantity: 5,  date: DateTime.parse("2021-02-03T09:05:00Z") },
+        { _id: 4, item: "abc", price: 10, quantity: 10, date: DateTime.parse("2021-02-15T08:00:00Z") },
+        { _id: 5, item: "xyz", price: 5,  quantity: 10, date: DateTime.parse("2021-02-15T09:05:00Z") },
+        { _id: 6, item: "xyz", price: 5,  quantity: 5,  date: DateTime.parse("2021-02-15T12:05:10Z") },
+        { _id: 7, item: "xyz", price: 5,  quantity: 10, date: DateTime.parse("2021-02-15T14:12:12Z") },
+        { _id: 8, item: "abc", price: 10, quantity: 5,  date: DateTime.parse("2021-03-16T20:20:13Z") }
+      ])
 
-    expect do
-      client.database.command(count: :sales)
-    end.to raise_error(Mongo::Error::OperationFailure)
+      # End Versioned API Example 5
 
-    # Start Versioned API Example 6
+      expect do
+        client.database.command(count: :sales)
+      end.to raise_error(Mongo::Error::OperationFailure)
 
-    # Mongo::Error::OperationFailure:
-    #   [323:APIStrictError]: Provided apiStrict:true, but the command count is not in API Version 1. Information on supported commands and migrations in API Version 1 can be found at https://dochub.mongodb.org/core/manual-versioned-api
+      # Start Versioned API Example 6
 
-    # End Versioned API Example 6
+      # Mongo::Error::OperationFailure:
+      #   [323:APIStrictError]: Provided apiStrict:true, but the command count is not in API Version 1. Information on supported commands and migrations in API Version 1 can be found at https://dochub.mongodb.org/core/manual-versioned-api
 
-    # Start Versioned API Example 7
+      # End Versioned API Example 6
 
-    client[:sales].count_documents
+      # Start Versioned API Example 7
 
-    # End Versioned API Example 7
+      client[:sales].count_documents
 
-    # Start Versioned API Example 8
+      # End Versioned API Example 7
 
-    # 8
+      # Start Versioned API Example 8
 
-    # End Versioned API Example 8
-    # Do not leak clients.
-    client.close
+      # 8
+
+      # End Versioned API Example 8
+      # Do not leak clients.
+      client.close
+    end
   end
 end
