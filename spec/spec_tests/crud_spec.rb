372a79b850d84592ca78ae49f6133c8681b95941
Neil Shweky <neilshweky@gmail.com>
2022-05-04 16:50:05 -0400

    RUBY-2583 remove v2 test calls

--- spec/spec_tests/crud_spec.rb
+++ spec/spec_tests/crud_spec.rb
@@ -5,18 +5,8 @@ require 'spec_helper'
 
 require 'runners/crud'
 
-describe 'CRUD v1 spec tests' do
+describe 'CRUD spec tests' do
   define_crud_spec_tests(CRUD_TESTS) do |spec, req, test|
     let(:client) { authorized_client }
-  end
-end
-
-describe 'CRUD v2 spec tests' do
-  define_crud_spec_tests(CRUD2_TESTS) do |spec, req, test|
-    let(:client) do
-      authorized_client.use(spec.database_name || 'crud-default').tap do |client|
-        client.subscribe(Mongo::Monitoring::COMMAND, event_subscriber)
-      end
-    end
   end
 end
