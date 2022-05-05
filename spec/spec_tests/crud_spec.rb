49688a69ab8fb83baca4d1cf0a818ddaabb80ed3
Neil Shweky <neilshweky@gmail.com>
2022-05-05 09:17:49 -0400

    put back v1

--- spec/spec_tests/crud_spec.rb
+++ spec/spec_tests/crud_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 
 require 'runners/crud'
 
-describe 'CRUD spec tests' do
+describe 'CRUD v1 spec tests' do
   define_crud_spec_tests(CRUD_TESTS) do |spec, req, test|
     let(:client) { authorized_client }
   end
