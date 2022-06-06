55d8f06324d31df33559e4864c0e73bf1dfe0d50
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2022-06-06 12:01:36 +0200

    2980

--- spec/support/crypt.rb
+++ spec/support/crypt.rb
@@ -111,9 +111,7 @@ module Crypt
     end
 
     let(:encrypted_fields) do
-      BSON::ExtJSON.parse(File.read('spec/support/crypt/encryptedFields.json')).tap do |ef|
-
-      end
+      BSON::ExtJSON.parse(File.read('spec/support/crypt/encryptedFields.json'))
     end
 
     let(:key1_document) do
