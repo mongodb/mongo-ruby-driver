5712bf8069019412e7c9aec68f811035a020ad47
Neil Shweky <neilshweky@gmail.com>
2022-06-09 16:43:32 -0400

    RUBY-3019 Optionally sleep before retrying tests (#2524)
    * RUBY-3019 Optionally sleep before retrying tests
    
    * RUBY-3019 fix failing tests
    
    * RUBY-3019 rename options

--- spec/mongo/client_spec.rb
+++ spec/mongo/client_spec.rb
@@ -836,7 +836,7 @@ describe Mongo::Client do
         expect(session).to be_a(Mongo::Session)
       end
 
-      retry_test 4
+      retry_test tries: 4
       it 'sets the last use field to the current time' do
         expect(session.instance_variable_get(:@server_session).last_use).to be_within(1).of(Time.now)
       end
