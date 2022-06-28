95989828bedd49f29f95304dbd40b3013df13ef9
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2022-06-28 16:32:06 +0200

    3024

--- spec/mongo/crypt/binding/context_spec.rb
+++ spec/mongo/crypt/binding/context_spec.rb
@@ -255,44 +255,12 @@ describe 'Mongo::Crypt::Binding' do
       end
     end
 
-    describe '#mongocrypt_ctx_setopt_index_type' do
-      let(:result) do
-        Mongo::Crypt::Binding.mongocrypt_ctx_setopt_index_type(
-          context,
-          index_type
-        )
-      end
-
-      before do
-        Mongo::Crypt::Binding.mongocrypt_init(mongocrypt)
-      end
-
-      context 'with equality index type' do
-        let(:index_type) do
-          :equality
-        end
-
-        it 'returns true' do
-          expect(result).to be true
-        end
-      end
-
-      context 'with none index type' do
-        let(:index_type) do
-          :none
-        end
-
-        it 'returns true' do
-          expect(result).to be true
-        end
-      end
-    end
-
     describe '#mongocrypt_ctx_setopt_query_type' do
       let(:result) do
         Mongo::Crypt::Binding.mongocrypt_ctx_setopt_query_type(
           context,
-          query_type
+          query_type,
+          -1
         )
       end
 
