6e99af97965194af8d73ddac01973488c0f973cb
Sarun Rattanasiri <midnight_w@gmx.tw>
2022-08-10 21:52:37 +0700

    RUBY-3087 add a test for insert_many with enumerable inputs

--- spec/mongo/collection_crud_spec.rb
+++ spec/mongo/collection_crud_spec.rb
@@ -362,6 +362,22 @@ describe Mongo::Collection do
       expect(result.inserted_ids.size).to eq(2)
     end
 
+    context 'when an enumerable is used instead of an array' do
+
+      let(:source_data) do
+        [{ name: 'test1' }, { name: 'test2' }]
+      end
+
+      let(:result) do
+        authorized_collection.insert_many(source_data.lazy)
+      end
+
+      it 'should accepts them without raising an error' do
+        expect { result }.to_not raise_error
+        expect(result.inserted_count).to eq(source_data.size)
+      end
+    end
+
     context 'when a session is provided' do
 
       let(:session) do
