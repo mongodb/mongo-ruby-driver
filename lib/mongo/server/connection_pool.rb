cf9687588f86c746a07f01fc24779777c02c003f
Neil Shweky <neilshweky@gmail.com>
2022-11-07 13:43:28 -0500

    Revert "get rid of debug"
    This reverts commit 42ca1b4a62f9d95dafc5b4b091af2da493445727.

--- lib/mongo/server/connection_pool.rb
+++ lib/mongo/server/connection_pool.rb
@@ -119,6 +119,7 @@ module Mongo
 
         @server = server
         @options = options.freeze
+        @options = @options.merge(backtrace: caller(0))
 
         @generation_manager = GenerationManager.new(server: server)
         @ready = false
