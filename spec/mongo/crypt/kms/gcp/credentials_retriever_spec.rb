be3b026681d0b2be29185f41283b0264c7e2b4ca
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2022-11-07 12:07:35 +0100

    Remove redundand test

--- spec/mongo/crypt/kms/gcp/credentials_retriever_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Mongo::Crypt::KMS::GCP::CredentialsRetriever do
-  it 'returns the token' do
-    expect do
-      described_class.get_access_token
-    end.not_to raise_error
-  end
-end
