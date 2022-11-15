4fb852e548217114a242c4920c480d006f801a13
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2022-11-15 09:15:19 +0100

    RUBY-3179 Fix FLE spec (#2663)

--- spec/integration/client_side_encryption/kms_tls_options_spec.rb
+++ spec/integration/client_side_encryption/kms_tls_options_spec.rb
@@ -216,7 +216,7 @@ describe 'Client-Side Encryption' do
                 master_key: master_key_template.merge({endpoint: "127.0.0.1:8002"})
               }
             )
-          end.to raise_error(Mongo::Error::KmsError, /(SocketError|ECONNRESET)/)
+          end.to raise_error(Mongo::Error::KmsError, /(certificate_required|SocketError|ECONNRESET)/)
         end
       end
 
@@ -286,7 +286,7 @@ describe 'Client-Side Encryption' do
                 master_key: master_key
              }
             )
-          end.to raise_error(Mongo::Error::KmsError, /(SocketError|ECONNRESET)/)
+          end.to raise_error(Mongo::Error::KmsError, /(certificate_required|SocketError|ECONNRESET)/)
         end
       end
 
