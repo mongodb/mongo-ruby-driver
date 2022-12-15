e19bc7e8d50de473031e3954423656cb8ecf050c
Neil Shweky <neilshweky@gmail.com>
2022-12-15 12:01:17 -0500

    RUBY-2547 Add test that ensures the error returned from being evicted from the WaitQueue is retryable (#2674)
    * RUBY-2547 add retryable read test
    
    * RUBY-2547 add retryable write test
    
    * add spec test
    
    * restrict topology
    
    * Empty-Commit
    
    * Apply suggestions from code review
    
    * add debug logging
    
    * update min server version
    
    * add to failpoint
    
    * fix min_server_version
    
    * fix race condition
    
    * RUBY-2547 fix tests
    
    * require retry writes
    
    * update description
    
    * add debug logging
    
    * fix comments
    
    * fix race condition
    
    * remove debug logging
    
    * remove more logging
    
    * only raise on non load balancer
    
    * no need for service_id
    
    * fix errors
    
    * refactor
    
    * Revert "remove more logging"
    
    This reverts commit dbab8e867ed57b96036f03e914a1822bd72b7054.
    
    * Revert "remove debug logging"
    
    This reverts commit 41d8531ab5df59bd9a4c74426a1f40bd6a90b851.
    
    * add logging
    
    * RUBY-2547 add logging
    
    * fix retryable test
    
    * attempt to dix race condition
    
    * add back logging
    
    * wait on ready
    
    * fix retryable writes as well + cleanup
    
    * remove generation bumped code
    
    * get rid of debugging
    
    * add back logging
    
    * require retry writes
    
    * remove logging
    
    * Revert "remove logging"
    
    This reverts commit 6dce1a7f517ecd12126b92cbff4e86b768539f7a.
    
    * Revert "Revert "remove logging""
    
    This reverts commit 7656394bb79a751a9b9980863fb976ce177e38a7.
    
    * remove falky test
    
    * RUBY-2547 remove debugging

--- spec/integration/retryable_writes_errors_spec.rb
+++ spec/integration/retryable_writes_errors_spec.rb
@@ -5,8 +5,10 @@ require 'spec_helper'
 
 describe 'Retryable writes errors tests' do
 
+  let(:options) { {} }
+
   let(:client) do
-    authorized_client.with(retry_writes: true)
+    authorized_client.with(options.merge(retry_writes: true))
   end
 
   let(:collection) do
@@ -34,7 +36,7 @@ describe 'Retryable writes errors tests' do
 
   context "when encountering a NoWritesPerformed error after an error with a RetryableWriteError label" do
     require_topology :replica_set
-    require_fail_command
+    require_retry_writes
     min_server_version '4.4'
 
     let(:failpoint1) do
@@ -87,6 +89,104 @@ describe 'Retryable writes errors tests' do
       expect do
         authorized_collection.insert_one(x: 1)
       end.to raise_error(Mongo::Error::OperationFailure, /\[91\]/)
+    end
+  end
+
+  context "PoolClearedError retryability test" do
+    require_topology :single, :replica_set, :sharded
+    require_no_multi_mongos
+    require_fail_command
+    require_retry_writes
+
+    let(:options) { { max_pool_size: 1 } }
+
+    let(:failpoint) do
+      {
+          configureFailPoint: "failCommand",
+          mode: { times: 1 },
+          data: {
+              failCommands: [ "insert" ],
+              errorCode: 91,
+              blockConnection: true,
+              blockTimeMS: 1000,
+              errorLabels: ["RetryableWriteError"]
+          }
+      }
+    end
+
+    let(:subscriber) { Mrss::EventSubscriber.new }
+
+    let(:threads) do
+      threads = []
+      threads << Thread.new do
+        expect(collection.insert_one(x: 2)).to be_successful
+      end
+      threads << Thread.new do
+        expect(collection.insert_one(x: 2)).to be_successful
+      end
+      threads
+    end
+
+    let(:insert_events) do
+      subscriber.started_events.select { |e| e.command_name == "insert" }
+    end
+
+    let(:cmap_events) do
+      subscriber.published_events
+    end
+
+    let(:event_types) do
+      [
+        Mongo::Monitoring::Event::Cmap::ConnectionCheckedOut,
+        Mongo::Monitoring::Event::Cmap::ConnectionCheckOutFailed,
+        Mongo::Monitoring::Event::Cmap::PoolCleared,
+      ]
+    end
+
+    let(:check_out_results) do
+      cmap_events.select do |e|
+        event_types.include?(e.class)
+      end
+    end
+
+    before do
+      authorized_client.use(:admin).command(failpoint)
+      client.subscribe(Mongo::Monitoring::COMMAND, subscriber)
+      client.subscribe(Mongo::Monitoring::CONNECTION_POOL, subscriber)
+    end
+
+    it "retries on PoolClearedError" do
+      # After the first insert fails, the pool is paused and retry is triggered.
+      # Now, a race is started between the second insert acquiring a connection,
+      # and the first retrying the read. Now, retry reads cause the cluster to
+      # be rescanned and the pool to be unpaused, allowing the second checkout
+      # to succeed (when it should fail). Therefore we want the second insert's
+      # check out to win the race. This gives the check out a little head start.
+      allow_any_instance_of(Mongo::Server::ConnectionPool).to receive(:ready).and_wrap_original do |m, *args, &block|
+        ::Utils.wait_for_condition(5) do
+          # check_out_results should contain:
+          # - insert1 connection check out successful
+          # - pool cleared
+          # - insert2 connection check out failed
+          # We wait here for the third event to happen before we ready the pool.
+          cmap_events.select do |e|
+            event_types.include?(e.class)
+          end.length >= 3
+        end
+        m.call(*args, &block)
+      end
+      threads.map(&:join)
+      expect(check_out_results[0]).to be_a(Mongo::Monitoring::Event::Cmap::ConnectionCheckedOut)
+      expect(check_out_results[1]).to be_a(Mongo::Monitoring::Event::Cmap::PoolCleared)
+      expect(check_out_results[2]).to be_a(Mongo::Monitoring::Event::Cmap::ConnectionCheckOutFailed)
+      expect(insert_events.length).to eq(3)
+    end
+
+    after do
+      authorized_client.use(:admin).command({
+        configureFailPoint: "failCommand",
+        mode: "off",
+      })
     end
   end
 end
