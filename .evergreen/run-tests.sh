8b1ec838fd15424517638cb7e20804152e80ecf0
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2023-02-10 12:21:54 +0100

    Fix crypt shared download

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -164,11 +164,15 @@ fi
 
 if test -n "$FLE"; then
   # Downloading crypt shared lib
-  crypt_shared_version=`${BINDIR}/mongod --version | grep -oP 'db version v\K.*'`
-  python3 -u .evergreen/mongodl.py --component crypt_shared -V ${crypt_shared_version} --out `pwd`/csfle_lib  --target `host_distro` || true
-  if test -f `pwd`/csfle_lib/lib/mongo_crypt_v1.so
-  then
-    export MONGO_RUBY_DRIVER_CRYPT_SHARED_LIB_PATH=`pwd`/csfle_lib/lib/mongo_crypt_v1.so
+  if [ -z $MONGO_CRYPT_SHARED_DOWNLOAD_URL ]; then
+    echo "There is no crypt_shared library for distro='$DISTRO' and version='$MONGODB_VERSION'".
+  else
+    echo "Downloading crypt_shared package from $MONGO_CRYPT_SHARED_DOWNLOAD_URL"
+    mkdir -p $(pwd)/csfle_lib
+    cd $(pwd)/csfle_lib
+    curl --retry 3 -fL $MONGO_CRYPT_SHARED_DOWNLOAD_URL | tar zxf -
+    export MONGO_RUBY_DRIVER_CRYPT_SHARED_LIB_PATH=$(pwd)/lib/mongo_crypt_v1.so
+    cd -
   fi
 
   # Start the KMS servers first so that they are launching while we are
