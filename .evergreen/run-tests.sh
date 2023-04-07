57c8a50ced69d087b4a9be8c8fbf1283bc61fa14
Jamis Buck <jamis.buck@mongodb.com>
2023-04-07 11:34:19 -0600

    lock older server versions to recent crypt_shared

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -165,7 +165,7 @@ fi
 if test -n "$FLE"; then
   # Downloading crypt shared lib
   if [ -z "$MONGO_CRYPT_SHARED_DOWNLOAD_URL" ]; then
-    crypt_shared_version=$("${BINDIR}"/mongod --version | grep -oP 'db version v\K.*')
+    crypt_shared_version=${CRYPT_SHARED_VERSION:-$("${BINDIR}"/mongod --version | grep -oP 'db version v\K.*')}
     python3 -u .evergreen/mongodl.py --component crypt_shared -V ${crypt_shared_version} --out $(pwd)/csfle_lib  --target $(host_distro) || true
     if test -f $(pwd)/csfle_lib/lib/mongo_crypt_v1.so
     then
