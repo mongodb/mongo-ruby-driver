eebfb051adff809133ed96ababb8c2a25d8eb35e
Jamis Buck <jamis.buck@mongodb.com>
2023-05-26 14:10:53 -0600

    linter appeasement

--- spec/integration/client_side_encryption/rewrap_prose_spec.rb
+++ spec/integration/client_side_encryption/rewrap_prose_spec.rb
@@ -102,7 +102,6 @@ describe 'RewrapManyDataKey' do
           expect(client_encryption2.decrypt(ciphertext)).to eq('test')
         end
 
-
         context 'when master_key is present without provider' do
           it 'raises an exception' do
             expect { client_encryption_1.rewrap_many_data_key({}, master_key: {}) }
