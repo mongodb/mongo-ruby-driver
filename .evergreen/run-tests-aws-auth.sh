d5bdf60624ae2fa3968e236a2579fbe2d83049d7
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2023-11-02 14:42:01 +0100

    3333

--- .evergreen/run-tests-aws-auth.sh
+++ .evergreen/run-tests-aws-auth.sh
@@ -95,25 +95,31 @@ case "$AUTH" in
     ;;
 
   aws-web-identity)
-    # cd `dirname "$0"`/auth_aws
-    #
-    # . ./activate-authawsvenv.sh
-    # export AWS_ACCESS_KEY_ID="`get_var IAM_AUTH_EC2_INSTANCE_ACCOUNT`"
-    # export AWS_SECRET_ACCESS_KEY="`get_var IAM_AUTH_EC2_INSTANCE_SECRET_ACCESS_KEY`"
-    # python -u lib/aws_unassign_instance_profile.py
-    # unset AWS_ACCESS_KEY_ID
-    # unset AWS_SECRET_ACCESS_KEY
-    #
-    # export IDP_ISSUER="`get_var IAM_WEB_IDENTITY_ISSUER`"
-    # export IDP_JWKS_URI="`get_var IAM_WEB_IDENTITY_JWKS_URI`"
-    # export IDP_RSA_KEY="`get_var IAM_WEB_IDENTITY_RSA_KEY`"
-    # export AWS_WEB_IDENTITY_TOKEN_FILE="`get_var IAM_WEB_IDENTITY_TOKEN_FILE`"
-    # python -u lib/aws_handle_oidc_creds.py token
-    # unset IDP_ISSUER
-    # unset IDP_JWKS_URI
-    # unset IDP_RSA_KEY
-    #
-    # cd -
+    cd `dirname "$0"`/auth_aws
+
+    echo "Activating virtual environment 'authawsvenv'..."
+    . ./activate-authawsvenv.sh
+    echo "Virtual environment 'authawsvenv' activated."
+    export AWS_ACCESS_KEY_ID="`get_var IAM_AUTH_EC2_INSTANCE_ACCOUNT`"
+    export AWS_SECRET_ACCESS_KEY="`get_var IAM_AUTH_EC2_INSTANCE_SECRET_ACCESS_KEY`"
+    echo "Unassigning instance profile..."
+    python -u lib/aws_unassign_instance_profile.py
+    echo "Instance profile unassigned."
+    unset AWS_ACCESS_KEY_ID
+    unset AWS_SECRET_ACCESS_KEY
+
+    export IDP_ISSUER="`get_var IAM_WEB_IDENTITY_ISSUER`"
+    export IDP_JWKS_URI="`get_var IAM_WEB_IDENTITY_JWKS_URI`"
+    export IDP_RSA_KEY="`get_var IAM_WEB_IDENTITY_RSA_KEY`"
+    export AWS_WEB_IDENTITY_TOKEN_FILE="`get_var IAM_WEB_IDENTITY_TOKEN_FILE`"
+    echo "Obtaining token..."
+    python -u lib/aws_handle_oidc_creds.py token
+    echo "Token obtained."
+    unset IDP_ISSUER
+    unset IDP_JWKS_URI
+    unset IDP_RSA_KEY
+
+    cd -
     export MONGO_RUBY_DRIVER_AWS_AUTH_ACCESS_KEY_ID="`get_var IAM_AUTH_EC2_INSTANCE_ACCOUNT`"
     export MONGO_RUBY_DRIVER_AWS_AUTH_SECRET_ACCESS_KEY="`get_var IAM_AUTH_EC2_INSTANCE_SECRET_ACCESS_KEY`"
     export AWS_WEB_IDENTITY_TOKEN_FILE="`get_var IAM_WEB_IDENTITY_TOKEN_FILE`"
