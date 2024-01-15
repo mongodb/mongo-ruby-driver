5debf553f86f5e2f4aee9519502af478a20aae54
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2024-01-15 17:39:17 +0100

    wip

--- spec/mongo/monitoring/open_telemetry/tracer_spec.rb
+++ spec/mongo/monitoring/open_telemetry/tracer_spec.rb
@@ -14,20 +14,28 @@ RSpec.describe Mongo::Monitoring::OpenTelemetry::Tracer do
         expect(tracer.ot_tracer).to be_nil
       end
     end
+
+    context 'when enabled' do
+      before do
+        allow(ENV).to receive(:[]).with(described_class::ENV_VARIABLE_ENABLED).and_return('true')
+      end
+
+      it 'creates the tracer' do
+        expect(tracer.ot_tracer).to be_a(OpenTelemetry::SDK::Trace::Tracer)
+      end
+    end
   end
 
   describe '#in_span' do
-    context 'when disabled' do
-      before do
-        allow(ENV).to receive(:[]).with(described_class::ENV_VARIABLE_ENABLED).and_return(nil)
-      end
-
+    context 'when not enabled' do
       it 'yields to the block' do
         expect { |b| tracer.in_span(nil, nil, nil, &b) }.to yield_control
       end
     end
 
     context 'when enabled' do
+      let(:ot_tracer) { instance_double(OpenTelemetry::SDK::Trace::Tracer) }
+
       before do
         allow(ENV).to receive(:[]).with(described_class::ENV_VARIABLE_ENABLED).and_return('true')
       end
