2ce09dfaeb25666cbed8a96e97b19edd32ca2c0e
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2024-01-15 13:21:11 +0100

    Fix code review remarks

--- lib/mongo/error/transactions_not_supported.rb
+++ lib/mongo/error/transactions_not_supported.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# rubocop:todo all
 
 # Copyright (C) 2019-2020 MongoDB Inc.
 #
@@ -17,7 +16,6 @@
 
 module Mongo
   class Error
-
     # Transactions are not supported by the cluster. There might be the
     # following reasons:
     #  - topology is standalone
