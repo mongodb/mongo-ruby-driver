303e74c24bc501daaea2b1a5b07cfbf41934ac25
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2025-07-01 13:28:15 +0200

    Make rubocop happy

--- spec/mongo/server/connection_pool/generation_manager_spec.rb
+++ spec/mongo/server/connection_pool/generation_manager_spec.rb
@@ -6,7 +6,7 @@ describe Mongo::Server::ConnectionPool::GenerationManager do
   describe '#close_all_pipes' do
     let(:service_id) { 'test_service_id' }
 
-    let(:server) { instance_double('Mongo::Server') }
+    let(:server) { instance_double(Mongo::Server) }
 
     let(:manager) { described_class.new(server: server) }
 
@@ -14,26 +14,17 @@ describe Mongo::Server::ConnectionPool::GenerationManager do
       manager.pipe_fds(service_id: service_id)
     end
 
-    it 'closes all pipes' do
+    it 'closes all pipes and removes them from the map' do
       expect(manager.pipe_fds(service_id: service_id).size).to eq(2)
 
-      manager.instance_variable_get(:@pipe_fds)[service_id].each do |gen, (r, w)|
-        expect(r).to receive(:close)
-        expect(w).to receive(:close)
+      manager.instance_variable_get(:@pipe_fds)[service_id].each do |_gen, (r, w)|
+        expect(r).to receive(:close).and_call_original
+        expect(w).to receive(:close).and_call_original
       end
 
       manager.close_all_pipes
-    end
 
-    it 'removes all pipes from the map' do
-      expect(manager.pipe_fds(service_id: service_id).size).to eq(2)
-
-      manager.instance_variable_get(:@pipe_fds)[service_id].each do |gen, (r, w)|
-        expect(r).to receive(:close)
-        expect(w).to receive(:close)
-      end
-
-      manager.close_all_pipes
+      expect(manager.instance_variable_get(:@pipe_fds)).to be_empty
     end
   end
 end
