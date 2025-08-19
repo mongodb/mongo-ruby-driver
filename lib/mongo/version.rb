ee03e8f95d7950261beec04e6fbc5e494c710eea
Jamis Buck <jamis.buck@mongodb.com>
2025-08-19 11:25:20 -0600

    Bump version to 2.20.2 (#2951)

--- lib/mongo/version.rb
+++ lib/mongo/version.rb
@@ -5,5 +5,5 @@ module Mongo
   #
   # Note that this file is automatically updated via `rake candidate:create`.
   # Manual changes to this file will be overwritten by that rake task.
-  VERSION = '2.20.1'
+  VERSION = '2.20.2'
 end
