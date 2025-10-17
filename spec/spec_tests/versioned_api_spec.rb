4a976b30fb2f54c96f84248d150d984ff18c34a7
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2025-10-17 14:23:39 +0200

    Skip some versioned api tests

--- spec/spec_tests/versioned_api_spec.rb
+++ spec/spec_tests/versioned_api_spec.rb
@@ -7,7 +7,11 @@ require 'runners/unified'
 
 base = "#{CURRENT_PATH}/spec_tests/data/versioned_api"
 UNIFIED_TESTS = Dir.glob("#{base}/**/*.yml").sort
+# https://jira.mongodb.org/browse/RUBY-3721
+SKIPPED_TESTS = 'runcommand-helper-no-api-version-declared.yml'
+
+TESTS = UNIFIED_TESTS.reject { |file| file.end_with?(SKIPPED_TESTS) }
 
 describe 'Versioned API spec tests' do
-  define_unified_spec_tests(base, UNIFIED_TESTS)
+  define_unified_spec_tests(base, TESTS)
 end
