04076fac3c85dc9b2e14b36151186087d46b8eb4
Dmitry Rybakov <dmitry@rybakov.eu>
2025-11-11 14:30:59 +0100

    Use the new toolchain

--- .evergreen/run-tests-kerberos-integration.sh
+++ .evergreen/run-tests-kerberos-integration.sh
@@ -19,7 +19,19 @@ show_local_instructions
 
 set_env_vars
 set_env_python
-set_env_ruby
+
+# Install rbenv and download the requested ruby version
+rm -rf ~/.rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+rm -rf ~/.rbenv/versions/
+curl --retry 3 -fL http://boxes.10gen.com/build/toolchain-drivers/mongo-ruby-toolchain/library/`host_distro`/$RVM_RUBY.tar.xz |tar -xC $HOME/.rbenv/ -Jf -
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init - bash)"
+export FULL_RUBY_VERSION=$(ls ~/.rbenv/versions | head -n1)
+rbenv global $FULL_RUBY_VERSION
+
+export JAVA_HOME=/opt/java/jdk21
+export JAVACMD=$JAVA_HOME/bin/java
 
 # Note that:
 #
