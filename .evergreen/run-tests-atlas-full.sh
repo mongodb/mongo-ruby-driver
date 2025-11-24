d3a9ca52d9daa0fc055595b52fd46862d3a23185
Dmitry Rybakov <160598371+comandeo-mongo@users.noreply.github.com>
2025-11-24 16:48:22 +0100

    RUBY-3726 Use new toolchain (#2958) (#2960)
    ---------
    
    Co-authored-by: Dmitry Rybakov <dmitry@rybakov.eu>

--- .evergreen/run-tests-atlas-full.sh
+++ .evergreen/run-tests-atlas-full.sh
@@ -8,14 +8,27 @@ set -ex
 
 set_env_vars
 set_env_python
-set_env_ruby
+
+# Install rbenv and download the requested ruby version
+rm -rf ~/.rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+rm -rf ~/.rbenv/versions/
+curl --retry 3 -fL http://boxes.10gen.com/build/toolchain-drivers/mongo-ruby-toolchain/library/`host_distro`/$RVM_RUBY.tar.xz |tar -xC $HOME/.rbenv/ -Jf -
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init - bash)"
+export FULL_RUBY_VERSION=$(ls ~/.rbenv/versions | head -n1)
+rbenv global $FULL_RUBY_VERSION
+
+export JAVA_HOME=/opt/java/jdk21
+export JAVACMD=$JAVA_HOME/bin/java
 
 bundle_install
 
 ATLAS_URI=$MONGODB_URI \
   SERVERLESS=1 \
   EXAMPLE_TIMEOUT=600 \
-  bundle exec rspec -fd spec/integration/search_indexes_prose_spec.rb
+  bundle exec rspec -fd spec/integration/search_indexes_prose_spec.rb \
+  --format Rfc::Riff --format RspecJunitFormatter --out tmp/rspec.xml
 
 test_status=$?
 
