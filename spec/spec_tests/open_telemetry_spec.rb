1ffd096ba5daacd06164a42891df786b184b14cd
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2025-12-10 14:06:53 +0100

    wip

--- spec/spec_tests/open_telemetry_spec.rb
+++ spec/spec_tests/open_telemetry_spec.rb
@@ -21,6 +21,8 @@ SKIPPED_OTEL_TESTS.each do |filename|
   warn "Skipping OpenTelemetry unified spec test: #{filename}"
 end
 
-describe 'OpenTelemetry unified spec tests' do
+if [ 1, '1', 'yes', 'true' ].include?(ENV['OTEL_SPEC_TESTS'])
   define_unified_spec_tests(base, TESTS_TO_RUN)
+else
+  skip 'OTel spec tests are disabled. To enable them set env variable OTEL_SPEC_TESTS to 1'
 end
