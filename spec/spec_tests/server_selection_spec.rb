7f263ebecbf31ab03a261b450989e9f10802bf8d
Dmitry Rybakov <160598371+comandeo-mongo@users.noreply.github.com>
2026-02-06 12:27:47 +0100

    RUBY-3741 Support deprioritizing for all topologies (#2984)

--- spec/spec_tests/server_selection_spec.rb
+++ spec/spec_tests/server_selection_spec.rb
@@ -7,6 +7,10 @@ require 'runners/server_selection'
 
 SERVER_SELECTION_TESTS = Dir.glob("#{CURRENT_PATH}/spec_tests/data/server_selection/**/*.yml").sort
 
+SKIPPED_TESTS = {
+  'DeprioritizedAllSecondaryPreferred.yml' => 'https://jira.mongodb.org/browse/RUBY-3768'
+}
+
 describe 'Server selection spec tests' do
-  define_server_selection_spec_tests(SERVER_SELECTION_TESTS)
+  define_server_selection_spec_tests(SERVER_SELECTION_TESTS, SKIPPED_TESTS)
 end
