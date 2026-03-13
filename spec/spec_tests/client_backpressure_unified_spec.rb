566e27f8937709fd5ef777a8eacc9d30e6ddf172
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2026-03-13 09:06:07 +0100

    Skip bulkWrite spec tests

--- spec/spec_tests/client_backpressure_unified_spec.rb
+++ spec/spec_tests/client_backpressure_unified_spec.rb
@@ -9,4 +9,11 @@ CLIENT_BACKPRESSURE_UNIFIED_TESTS = Dir.glob("#{base}/**/*.yml").sort
 
 describe 'Client backpressure unified spec tests' do
   define_unified_spec_tests(base, CLIENT_BACKPRESSURE_UNIFIED_TESTS)
+
+  around do |example|
+    if example.full_description.include?('clientBulkWrite')
+      skip 'RUBY-2964: client-level bulk write is not yet implemented'
+    end
+    example.run
+  end
 end
